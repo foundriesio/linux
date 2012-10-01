b8c1286a015fb6927f12cecb2aa8aabe383f10fe
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2012-10-01 14:38:21 +0200

    tegra: fix usb_phy build breakage if gadget as module
    Add missing symbol exports to address gcc 4.7.2 from oe-core throwing following errors if usb gadget stuff compiled as modules (cf. 328d61af72a8f59f5dc53db8cddf8c8e572ad958).

--- arch/arm/mach-tegra/usb_phy.c
+++ arch/arm/mach-tegra/usb_phy.c
@@ -381,8 +381,7 @@ fail_io:
 
 	return ERR_PTR(err);
 }
-
-
+EXPORT_SYMBOL_GPL(tegra_usb_phy_open);
 
 void tegra_usb_phy_close(struct tegra_usb_phy *phy)
 {
@@ -425,6 +424,7 @@ void tegra_usb_phy_close(struct tegra_usb_phy *phy)
 	kfree(phy->pdata);
 	kfree(phy);
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_close);
 
 irqreturn_t tegra_usb_phy_irq(struct tegra_usb_phy *phy)
 {
@@ -449,6 +449,7 @@ int tegra_usb_phy_init(struct tegra_usb_phy *phy)
 
 	return status;
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_init);
 
 int tegra_usb_phy_power_off(struct tegra_usb_phy *phy)
 {
@@ -494,6 +495,7 @@ int tegra_usb_phy_power_off(struct tegra_usb_phy *phy)
 
 	return err;
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_power_off);
 
 int tegra_usb_phy_power_on(struct tegra_usb_phy *phy)
 {
@@ -538,6 +540,7 @@ int tegra_usb_phy_power_on(struct tegra_usb_phy *phy)
 
 	return status;
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_power_on);
 
 int tegra_usb_phy_reset(struct tegra_usb_phy *phy)
 {
@@ -669,6 +672,7 @@ bool tegra_usb_phy_charger_detected(struct tegra_usb_phy *phy)
 
 	return status;
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_charger_detected);
 
 bool tegra_usb_phy_hw_accessible(struct tegra_usb_phy *phy)
 {
@@ -688,11 +692,13 @@ bool tegra_usb_phy_has_hostpc(struct tegra_usb_phy *phy)
 {
 	return phy->pdata->has_hostpc;
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_has_hostpc);
 
 bool tegra_usb_phy_otg_supported(struct tegra_usb_phy *phy)
 {
 	return phy->pdata->port_otg;
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_otg_supported);
 
 void tegra_usb_phy_memory_prefetch_on(struct tegra_usb_phy *phy)
 {
@@ -708,6 +714,7 @@ void tegra_usb_phy_memory_prefetch_on(struct tegra_usb_phy *phy)
 		writel(val, ahb_gizmo + AHB_MEM_PREFETCH_CFG2);
 	}
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_memory_prefetch_on);
 
 void tegra_usb_phy_memory_prefetch_off(struct tegra_usb_phy *phy)
 {
@@ -723,3 +730,4 @@ void tegra_usb_phy_memory_prefetch_off(struct tegra_usb_phy *phy)
 		writel(val, ahb_gizmo + AHB_MEM_PREFETCH_CFG2);
 	}
 }
+EXPORT_SYMBOL_GPL(tegra_usb_phy_memory_prefetch_off);
