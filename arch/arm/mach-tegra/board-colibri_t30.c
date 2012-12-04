bf044d093015caa16b2b5d7bb4a6ca187956ee56
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2012-12-05 00:05:01 +0100

    colibri_t30: MMC: fix building for SDMMC4B
    This fixes the following build issue:
    
    arch/arm/mach-tegra/board-colibri_t30.c:276:41: error:
    'colibri_t30_emmc_platform_data' defined but not used
    [-Werror=unused-variable]
    cc1: all warnings being treated as errors

--- arch/arm/mach-tegra/board-colibri_t30.c
+++ arch/arm/mach-tegra/board-colibri_t30.c
@@ -273,6 +273,7 @@ static void __init colibri_t30_i2c_init(void)
 
 /* MMC/SD */
 
+#ifndef COLIBRI_T30_SDMMC4B
 static struct tegra_sdhci_platform_data colibri_t30_emmc_platform_data = {
 	.cd_gpio	= -1,
 	.ddr_clk_limit	= 52000000,
@@ -285,6 +286,7 @@ static struct tegra_sdhci_platform_data colibri_t30_emmc_platform_data = {
 	.tap_delay	= 0x0f,
 	.wp_gpio	= -1,
 };
+#endif /* COLIBRI_T30_SDMMC4B */
 
 static struct tegra_sdhci_platform_data colibri_t30_sdcard_platform_data = {
 	.cd_gpio	= MMC_CD,
