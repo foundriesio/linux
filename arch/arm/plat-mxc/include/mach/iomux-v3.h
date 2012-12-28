3d8886924bce1e56a30da86797cc5868ce9dfcfb
Tapani <tapani@vmail.me>
2012-12-28 14:58:53 +0100

    iMX6: Let mxc_iomux_v3_setup_multiple_pads take a const array of pad settings.
    Since the function does not modify the array passed to it, the argument should
    be of type const.

--- arch/arm/plat-mxc/include/mach/iomux-v3.h
+++ arch/arm/plat-mxc/include/mach/iomux-v3.h
@@ -172,7 +172,7 @@ int mxc_iomux_v3_setup_pad(iomux_v3_cfg_t pad);
  * convenient way to call the above function with tables
  */
  int mxc_iomux_v3_get_multiple_pads(iomux_v3_cfg_t *pad_list, unsigned count);
-int mxc_iomux_v3_setup_multiple_pads(iomux_v3_cfg_t *pad_list, unsigned count);
+int mxc_iomux_v3_setup_multiple_pads(const iomux_v3_cfg_t *pad_list, unsigned count);
 
 /*
  * Initialise the iomux controller
