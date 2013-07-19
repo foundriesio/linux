2455e4d3d505b9d849280fa418acac32243fefa2
Peter Gielda <pgielda@antmicro.com>
2013-07-19 16:52:12 +0200

    colibri_t30: also define COLIBRI_T30_VI when VIDEO_TEGRA is a kernel module.

--- arch/arm/mach-tegra/board-colibri_t30.h
+++ arch/arm/mach-tegra/board-colibri_t30.h
@@ -28,7 +28,7 @@
 
 /* Uncomment for camera interface support on Colibri Evaluation carrier
    board */
-#if defined(CONFIG_TEGRA_CAMERA) || defined(CONFIG_VIDEO_TEGRA)
+#if defined(CONFIG_TEGRA_CAMERA) || defined(CONFIG_VIDEO_TEGRA) || defined(CONFIG_VIDEO_TEGRA_MODULE)
 #define COLIBRI_T30_VI
 #endif
 
