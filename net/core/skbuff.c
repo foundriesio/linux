af69c6eeb362b3a08634c97103662ac378e631b1
Hannes Frederic Sowa <hannes@stressinduktion.org>
2013-12-05 23:29:19 +0100

    net: clear local_df when passing skb between namespaces
    commit 239c78db9c41a8f524cce60507440d72229d73bc upstream.
    
    We must clear local_df when passing the skb between namespaces as the
    packet is not local to the new namespace any more and thus may not get
    fragmented by local rules. Fred Templin noticed that other namespaces
    do fragment IPv6 packets while forwarding. Instead they should have send
    back a PTB.
    
    The same problem should be present when forwarding DF-IPv4 packets
    between namespaces.
    
    Reported-by: Templin, Fred L <Fred.L.Templin@boeing.com>
    Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -3511,6 +3511,7 @@ void skb_scrub_packet(struct sk_buff *skb)
 	skb->tstamp.tv64 = 0;
 	skb->pkt_type = PACKET_HOST;
 	skb->skb_iif = 0;
+	skb->local_df = 0;
 	skb_dst_drop(skb);
 	skb->mark = 0;
 	secpath_reset(skb);
