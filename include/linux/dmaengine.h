44ae434051fc5b1c62290737a5c9b13508beb228
Lars-Peter Clausen <lars@metafoo.de>
2014-01-11 14:02:16 +0100

    dma: Indicate residue granularity in dma_slave_caps
    This patch adds a new field to the dma_slave_caps struct which indicates the
    granularity with which the driver is able to update the residue field of the
    dma_tx_state struct. Making this information available to dmaengine users allows
    them to make better decisions on how to operate. E.g. for audio certain features
    like wakeup less operation or timer based scheduling only make sense and work
    correctly if the reported residue is fine-grained enough.
    
    Right now four different levels of granularity are supported:
    	* DESCRIPTOR: The DMA channel is only able to tell whether a descriptor has
    	  been completed or not, which means residue reporting is not supported by
    	  this channel. The residue field of the dma_tx_state field will always be
    	  0.
    	* SEGMENT: The DMA channel updates the residue field after each successfully
    	  completed segment of the transfer (For cyclic transfers this is after each
    	  period). This is typically implemented by having the hardware generate an
    	  interrupt after each transferred segment and then the drivers updates the
    	  outstanding residue by the size of the segment. Another possibility is if
    	  the hardware supports SG and the segment descriptor has a field which gets
    	  set after the segment has been completed. The driver then counts the
    	  number of segments without the flag set to compute the residue.
    	* BURST: The DMA channel updates the residue field after each transferred
    	  burst. This is typically only supported if the hardware has a progress
    	  register of some sort (E.g. a register with the current read/write address
    	  or a register with the amount of bursts/beats/bytes that have been
    	  transferred or still need to be transferred).
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Acked-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry picked from commit 507205632dd12636cfe4af4322dace263dca0c21)
    Signed-off-by: Jon Medhurst <tixy@linaro.org>
    
    Conflicts:
    	include/linux/dmaengine.h

--- include/linux/dmaengine.h
+++ include/linux/dmaengine.h
@@ -371,6 +371,32 @@ struct dma_slave_config {
 	unsigned int slave_id;
 };
 
+/**
+ * enum dma_residue_granularity - Granularity of the reported transfer residue
+ * @DMA_RESIDUE_GRANULARITY_DESCRIPTOR: Residue reporting is not support. The
+ *  DMA channel is only able to tell whether a descriptor has been completed or
+ *  not, which means residue reporting is not supported by this channel. The
+ *  residue field of the dma_tx_state field will always be 0.
+ * @DMA_RESIDUE_GRANULARITY_SEGMENT: Residue is updated after each successfully
+ *  completed segment of the transfer (For cyclic transfers this is after each
+ *  period). This is typically implemented by having the hardware generate an
+ *  interrupt after each transferred segment and then the drivers updates the
+ *  outstanding residue by the size of the segment. Another possibility is if
+ *  the hardware supports scatter-gather and the segment descriptor has a field
+ *  which gets set after the segment has been completed. The driver then counts
+ *  the number of segments without the flag set to compute the residue.
+ * @DMA_RESIDUE_GRANULARITY_BURST: Residue is updated after each transferred
+ *  burst. This is typically only supported if the hardware has a progress
+ *  register of some sort (E.g. a register with the current read/write address
+ *  or a register with the amount of bursts/beats/bytes that have been
+ *  transferred or still need to be transferred).
+ */
+enum dma_residue_granularity {
+	DMA_RESIDUE_GRANULARITY_DESCRIPTOR = 0,
+	DMA_RESIDUE_GRANULARITY_SEGMENT = 1,
+	DMA_RESIDUE_GRANULARITY_BURST = 2,
+};
+
 /* struct dma_slave_caps - expose capabilities of a slave channel only
  *
  * @src_addr_widths: bit mask of src addr widths the channel supports
@@ -386,6 +412,7 @@ struct dma_slave_config {
  * 	0 for no maximum
  * @max_sg_len: maximum length of a SG segment supported
  * 	0 for no maximum
+ * @residue_granularity: granularity of the reported transfer residue
  */
 struct dma_slave_caps {
 	u32 src_addr_widths;
@@ -396,6 +423,7 @@ struct dma_slave_caps {
 
 	u32 max_sg_nr;
 	u32 max_sg_len;
+	enum dma_residue_granularity residue_granularity;
 };
 
 static inline const char *dma_chan_name(struct dma_chan *chan)
