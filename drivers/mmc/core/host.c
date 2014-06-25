9d719bc306859e455a78a561b08d1ae697ca75b6
Stefan Agner <stefan@agner.ch>
2014-06-25 17:58:44 +0200

    mmc: use SD/MMC host ID for block device name ID
    From: Stefan Agner <stefan@agner.ch>
    
    To get the SD/MMC host device ID, read the alias from the device
    tree.
    
    This is useful in case a SoC has multipe SD/MMC host controllers while
    the second controller should logically be the first device (e.g. if
    the second controller is connected to an internal eMMC). Combined
    with block device numbering using MMC/SD host device ID, this
    results in predictable name assignment of the internal eMMC block
    device.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -427,8 +427,8 @@ EXPORT_SYMBOL(mmc_of_parse);
  */
 struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 {
-	int err;
 	struct mmc_host *host;
+	int of_id = -1, id = -1;
 
 	host = kzalloc(sizeof(struct mmc_host) + extra, GFP_KERNEL);
 	if (!host)
@@ -438,12 +438,26 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 	host->rescan_disable = 1;
 	idr_preload(GFP_KERNEL);
 	spin_lock(&mmc_host_lock);
-	err = idr_alloc(&mmc_host_idr, host, 0, 0, GFP_NOWAIT);
-	if (err >= 0)
-		host->index = err;
+
+	if (dev->of_node)
+		of_id = of_alias_get_id(dev->of_node, "mmc");
+
+	if (of_id >= 0) {
+		id = idr_alloc(&mmc_host_idr, host, of_id, of_id + 1,
+				GFP_NOWAIT);
+		if (id < 0)
+			dev_warn(dev, "/aliases ID %d not available\n", of_id);
+	}
+
+	if (id < 0)
+		id = idr_alloc(&mmc_host_idr, host, 0, 0, GFP_NOWAIT);
+
+	if (id >= 0)
+		host->index = id;
+
 	spin_unlock(&mmc_host_lock);
 	idr_preload_end();
-	if (err < 0)
+	if (id < 0)
 		goto free;
 
 	dev_set_name(&host->class_dev, "mmc%d", host->index);
