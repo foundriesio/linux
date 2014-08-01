68efd5cb5d884d95eb16046741c14bcf465afd59
Javi Merino <javi.merino@arm.com>
2014-08-01 17:45:47 +0100

    thermal: scpi: add debugfs entry for alpha
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- drivers/thermal/scpi-thermal.c
+++ drivers/thermal/scpi-thermal.c
@@ -1,6 +1,7 @@
 #include <linux/cpu_cooling.h>
 #include <linux/cpufreq.h>
 #include <linux/cpumask.h>
+#include <linux/debugfs.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/scpi_protocol.h>
@@ -139,6 +140,24 @@ static int get_temp_value(void *data, long *temp)
 	return 0;
 }
 
+static void update_debugfs(struct scpi_sensor *sensor_data)
+{
+	struct dentry *dentry_f, *filter_d;
+
+	filter_d = debugfs_create_dir("thermal_lpf_filter", NULL);
+	if (IS_ERR_OR_NULL(filter_d)) {
+		pr_warning("unable to create debugfs directory for the LPF filter\n");
+		return;
+	}
+
+	dentry_f = debugfs_create_u32("alpha", S_IWUSR | S_IRUGO, filter_d,
+				      &sensor_data->alpha);
+	if (IS_ERR_OR_NULL(dentry_f)) {
+		pr_warn("Unable to create debugfsfile: alpha\n");
+		return;
+	}
+}
+
 static int scpi_thermal_probe(struct platform_device *pdev)
 {
 	struct scpi_sensor *sensor_data = &scpi_temp_sensor;
@@ -212,6 +231,8 @@ static int scpi_thermal_probe(struct platform_device *pdev)
 		dev_warn(&pdev->dev, "Error registering sensor: %p\n", sensor_data->tzd);
 		return PTR_ERR(sensor_data->tzd);
 	}
+
+	update_debugfs(sensor_data);
 
 	thermal_zone_device_update(sensor_data->tzd);
 
