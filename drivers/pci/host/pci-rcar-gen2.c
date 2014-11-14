f708c98a296181408d27900b7d0745678058dcb6
Michel Pollet <michel.pollet@bp.renesas.com>
2014-11-14 12:04:04 +0000

    PCI: rcar-usb: Add RZ/N1 platform
    The Renesas RZ/N1 uses a very similar PCI block instance as the
    R-Car SoC's
    
    Signed-off-by: Michel Pollet <michel.pollet@bp.renesas.com>

--- drivers/pci/host/pci-rcar-gen2.c
+++ drivers/pci/host/pci-rcar-gen2.c
@@ -434,6 +434,7 @@ static struct of_device_id rcar_pci_of_match[] = {
 	{ .compatible = "renesas,pci-r8a7790", },
 	{ .compatible = "renesas,pci-r8a7791", },
 	{ .compatible = "renesas,pci-r8a7794", },
+	{ .compatible = "renesas,pci-rzn1", },
 	{ },
 };
 
