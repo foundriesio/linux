8b63fdcb095e5face40e492433c859c46ac21299
Stefan Agner <stefan.agner@toradex.com>
2014-12-16 10:24:29 +0100

    input: touchscreen: fusion: use level triggered interrupt
    Use level triggered interrupt which makes sure that even we miss a
    rising edge (due to latencies in the kernel), the interrupt still
    will be handled later, and doesn't freeze the input device.
    
    (cherry picked from commit ef25b8a9f9c0ca7cc0f0f955d89d112c3e19f75b)

--- drivers/input/touchscreen/fusion_F0710A.c
+++ drivers/input/touchscreen/fusion_F0710A.c
@@ -17,6 +17,7 @@
 #include <asm/irq.h>
 #include <linux/gpio.h>
 #include <linux/input/fusion_F0710A.h>
+#include <linux/irq.h>
 #include <linux/slab.h>
 #include <linux/of_gpio.h>
 
@@ -369,6 +370,7 @@ static int fusion_F0710A_probe(struct i2c_client *i2c, const struct i2c_device_i
 
 	/* Use Pen Down GPIO as sampling interrupt */
 	i2c->irq = gpio_to_irq(pdata->gpio_int);
+	irq_set_irq_type(i2c->irq, IRQ_TYPE_LEVEL_HIGH);
 
 	if(!i2c->irq)
 	{
