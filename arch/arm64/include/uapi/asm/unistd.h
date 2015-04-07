08e613dbc4aead4e0649b15bcbd4a22e44fd94a7
Jorge Ramirez-Ortiz <jro@xenomai.org>
2015-04-07 12:58:04 -0400

    Revert "ARM64:ILP32: Use the same syscall names as LP64"
    This reverts commit ed722513a020c655797eb855c2d6ccf525d1c10c.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jro@xenomai.org>

--- arch/arm64/include/uapi/asm/unistd.h
+++ arch/arm64/include/uapi/asm/unistd.h
@@ -13,10 +13,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/* For ILP32, we want to use the non compat names. */
-#if defined(__ILP32__)
-#define __ARCH_WANT_64BIT_SYSCALLS
-#endif
-
 #include <asm-generic/unistd.h>
