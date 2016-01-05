9fd429f81039b05b777c6d6d3e93f5222b7042bc
Stefan Agner <stefan@agner.ch>
2016-01-04 16:34:22 -0800

    drm/fsl-dcu: do not update when modifing irq registers
    The IRQ status and mask register are not "double buffered" registers
    according to the reference manual, hence there is no transfer of the
    DCU configuration needed when modifing the interrupt registers.

--- drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_drv.c
+++ drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_drv.c
@@ -57,8 +57,6 @@ static int fsl_dcu_drm_irq_init(struct drm_device *dev)
 
 	regmap_write(fsl_dev->regmap, DCU_INT_STATUS, 0);
 	regmap_write(fsl_dev->regmap, DCU_INT_MASK, ~0);
-	regmap_write(fsl_dev->regmap, DCU_UPDATE_MODE,
-		     DCU_UPDATE_MODE_READREG);
 
 	return ret;
 }
@@ -133,8 +131,6 @@ static irqreturn_t fsl_dcu_drm_irq(int irq, void *arg)
 		drm_handle_vblank(dev, 0);
 
 	regmap_write(fsl_dev->regmap, DCU_INT_STATUS, int_status);
-	regmap_write(fsl_dev->regmap, DCU_UPDATE_MODE,
-		     DCU_UPDATE_MODE_READREG);
 
 	return IRQ_HANDLED;
 }
