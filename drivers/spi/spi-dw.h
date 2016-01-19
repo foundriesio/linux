d10caa105f1bd0dc65668d93a51c4ef51a9bc0a8
Michel Pollet <michel.pollet@bp.renesas.com>
2016-01-19 09:02:01 +0000

    spi-dw: Use u8* not void* to fix an undefined behaviour
    cppcheck doesn't like void* pointer arithmetics
    
    Signed-off-by: Michel Pollet <michel.pollet@bp.renesas.com>

--- drivers/spi/spi-dw.h
+++ drivers/spi/spi-dw.h
@@ -115,10 +115,10 @@ struct dw_spi {
 
 	/* Current message transfer state info */
 	size_t			len;
-	void			*tx;
-	void			*tx_end;
-	void			*rx;
-	void			*rx_end;
+	u8			*tx;
+	u8			*tx_end;
+	u8			*rx;
+	u8			*rx_end;
 	int			dma_mapped;
 	u8			n_bytes;	/* current is a 1/2 bytes op */
 	u32			dma_width;
