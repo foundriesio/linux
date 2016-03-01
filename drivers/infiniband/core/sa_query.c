2d6151fcd6e273fb8df4519652a1632ff843dd65
Or Gerlitz <ogerlitz@mellanox.com>
2016-03-01 18:52:23 +0200

    IB/core: Use GRH when the path hop-limit > 0
    commit 11d8d645343efba0c975aefe7c2cf3b33c836c75 upstream.
    
    According to IBTA spec v1.3 section 12.7.19, QPs should use GRH when
    the path returned by the SA has hop-limit > 0. Currently, we do that
    only for the > 1 case, fix that.
    
    Fixes: 6d969a471ba1 ('IB/sa: Add ib_init_ah_from_path()')
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Reviewed-by: Jason Gunthorpe <jgunthorpe@obsidianresearch.com>
    Signed-off-by: Doug Ledford <dledford@redhat.com>
    [ kamal: backport to 4.2-stable: context ]
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/infiniband/core/sa_query.c
+++ drivers/infiniband/core/sa_query.c
@@ -542,7 +542,7 @@ int ib_init_ah_from_path(struct ib_device *device, u8 port_num,
 
 	force_grh = rdma_cap_eth_ah(device, port_num);
 
-	if (rec->hop_limit > 1 || force_grh) {
+	if (rec->hop_limit > 0 || force_grh) {
 		ah_attr->ah_flags = IB_AH_GRH;
 		ah_attr->grh.dgid = rec->dgid;
 
