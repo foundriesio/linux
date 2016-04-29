b017e4726d4c5cfa7ca4f475e2a0d903abd330b1
Jon Medhurst <tixy@linaro.org>
2016-04-29 13:03:37 +0100

    mali: Fix following LSK's backport of OPPv2
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- drivers/gpu/arm/midgard/mali_kbase_core_linux.c
+++ drivers/gpu/arm/midgard/mali_kbase_core_linux.c
@@ -3378,7 +3378,7 @@ static int power_control_init(struct platform_device *pdev)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0))
 	err = dev_pm_opp_of_add_table(kbdev->dev);
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 7, 0))
-	err = of_init_opp_table(kbdev->dev);
+	err = dev_pm_opp_of_add_table(kbdev->dev);
 #else
 	err = 0;
 #endif /* LINUX_VERSION_CODE */
