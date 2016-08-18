473768330d42e504a89671bb47d966735c83293c
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2016-08-18 09:43:28 +0200

    colibri_t20: fix gcc-6 compilation
    arch/arm/mach-tegra/board-colibri_t20.c:668:43: error: 'i2c2_ddc'
    defined but not used [-Werror=unused-const-variable=]
     static const struct tegra_pingroup_config i2c2_ddc = {
                                               ^~~~~~~~
    
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
    Acked-by: Max Krummenacher <max.krummenacher@toradex.com>

--- arch/arm/mach-tegra/board-colibri_t20.c
+++ arch/arm/mach-tegra/board-colibri_t20.c
@@ -665,11 +665,6 @@ static struct tegra_i2c_platform_data colibri_t20_i2c1_platform_data = {
 /* GEN2_I2C: unused */
 
 /* DDC_CLOCK/DATA on X3 pin 15/16 (e.g. display EDID) */
-static const struct tegra_pingroup_config i2c2_ddc = {
-	.pingroup	= TEGRA_PINGROUP_DDC,
-	.func		= TEGRA_MUX_I2C2,
-};
-
 static struct tegra_i2c_platform_data colibri_t20_i2c2_platform_data = {
 	.adapter_nr	= 1,
 	.arb_recovery	= arb_lost_recovery,
