b8a55501994a507b176e96d746f62d300dece31f
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2017-01-23 13:52:02 +0000

    pcie-dw: make msi address registers 32 bit
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- drivers/pci/dwc/pcie-designware-host.c
+++ drivers/pci/dwc/pcie-designware-host.c
@@ -84,9 +84,8 @@ void dw_pcie_msi_init(struct pcie_port *pp)
 {
 	u64 msi_target;
 
-	pp->msi_data = __get_free_pages(GFP_KERNEL, 0);
+	pp->msi_data = __get_free_pages(GFP_KERNEL | GFP_DMA, 0);
 	msi_target = virt_to_phys((void *)pp->msi_data);
-
 	/* program the msi_data */
 	dw_pcie_wr_own_conf(pp, PCIE_MSI_ADDR_LO, 4,
 			    (u32)(msi_target & 0xffffffff));
