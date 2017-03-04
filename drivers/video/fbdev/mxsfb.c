0464d180997130645f1cffc4679c6569c26f14fa
Stefan Agner <stefan.agner@toradex.com>
2017-03-03 19:15:20 -0800

    video: fbdev: mxsfb: make sure runtime PM is suspended in system suspend
    Use force suspend/resume to trigger runtime PM when entering system
    suspend.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
    Acked-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
    (cherry picked from commit ebf45652f2e9ec2ab19831d852363deadeb6407e)

--- drivers/video/fbdev/mxsfb.c
+++ drivers/video/fbdev/mxsfb.c
@@ -2447,6 +2447,7 @@ static int mxsfb_suspend(struct device *pdev)
 	host->restore_blank = saved_blank;
 	console_unlock();
 
+	pm_runtime_force_suspend(&host->pdev->dev);
 	pinctrl_pm_select_sleep_state(pdev);
 
 	return 0;
@@ -2458,6 +2459,7 @@ static int mxsfb_resume(struct device *pdev)
 	struct fb_info *fb_info = host->fb_info;
 
 	pinctrl_pm_select_default_state(pdev);
+	pm_runtime_force_resume(&host->pdev->dev);
 
 	console_lock();
 	mxsfb_blank(host->restore_blank, fb_info);
