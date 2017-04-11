d79215768fe9eaa0052f3db3d28f14d72b3182c5
Sudeep Holla <sudeep.holla@arm.com>
2017-04-11 14:32:35 +0100

    mailbox: arm_mhu: reorder header inclusion and drop unneeded ones
    This patch just re-orders some of the headers includes and also drop
    the ones that are unnecessary.
    
    Cc: Jassi Brar <jaswinder.singh@linaro.org>
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/mailbox/arm_mhu.c
+++ drivers/mailbox/arm_mhu.c
@@ -13,16 +13,13 @@
  * GNU General Public License for more details.
  */
 
-#include <linux/interrupt.h>
-#include <linux/spinlock.h>
-#include <linux/mutex.h>
-#include <linux/delay.h>
-#include <linux/slab.h>
-#include <linux/err.h>
-#include <linux/io.h>
-#include <linux/module.h>
 #include <linux/amba/bus.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/mailbox_controller.h>
+#include <linux/module.h>
 
 #define INTR_STAT_OFS	0x0
 #define INTR_SET_OFS	0x8
