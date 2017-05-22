e6c12bc101f7b9383d1af199c76b6b2a6c6a4d73
Bhuvanchandra DV <bhuvanchandra.dv@toradex.com>
2017-05-22 14:56:06 +0530

    mxc_ipuv3_fb: Use the mode_string from command line as is
    Using the mode_str passed via device tree
    as default fall back, fb_find_mode fails
    while finding the mode, so avoid using the
    non standard mode_str passed via device tree
    and use the mode_str from command line as is.
    
    Signed-off-by: Bhuvanchandra DV <bhuvanchandra.dv@toradex.com>
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
    (cherry picked from commit 50e26af30f04d24f4783dae15206c5dba889fd2b)

--- drivers/video/fbdev/mxc/mxc_ipuv3_fb.c
+++ drivers/video/fbdev/mxc/mxc_ipuv3_fb.c
@@ -3141,8 +3141,7 @@ static int mxcfb_option_setup(struct platform_device *pdev, struct fb_info *fbi)
 			fb_mode_str = opt;
 	}
 
-	if (fb_mode_str)
-		pdata->mode_str = fb_mode_str;
+	pdata->mode_str = fb_mode_str;
 
 	return 0;
 }
