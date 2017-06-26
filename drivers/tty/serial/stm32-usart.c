4fba06809b1c43e45c69211a908251b14bbef86e
Gerald Baeza <gerald.baeza@st.com>
2017-06-26 15:30:29 +0200

    serial: stm32: less messages on dma alloc error
    Just display one info (instead of two) in the
    log buffer when there is no dma.
    
    Change-Id: Ide2437c1979322aa4d4b2876b434d2e6aaa4a56d
    Signed-off-by: Gerald Baeza <gerald.baeza@st.com>
    Signed-off-by: Bich Hemon <bich.hemon@st.com>

--- drivers/tty/serial/stm32-usart.c
+++ drivers/tty/serial/stm32-usart.c
@@ -1174,10 +1174,8 @@ static int stm32_usart_of_dma_rx_probe(struct stm32_port *stm32port,
 
 	/* Request DMA RX channel */
 	stm32port->rx_ch = dma_request_slave_channel(dev, "rx");
-	if (!stm32port->rx_ch) {
-		dev_info(dev, "rx dma alloc failed\n");
+	if (!stm32port->rx_ch)
 		return -ENODEV;
-	}
 	stm32port->rx_buf = dma_alloc_coherent(&pdev->dev, RX_BUF_L,
 					       &stm32port->rx_dma_buf,
 					       GFP_KERNEL);
@@ -1251,10 +1249,8 @@ static int stm32_usart_of_dma_tx_probe(struct stm32_port *stm32port,
 
 	/* Request DMA TX channel */
 	stm32port->tx_ch = dma_request_slave_channel(dev, "tx");
-	if (!stm32port->tx_ch) {
-		dev_info(dev, "tx dma alloc failed\n");
+	if (!stm32port->tx_ch)
 		return -ENODEV;
-	}
 	stm32port->tx_buf = dma_alloc_coherent(&pdev->dev, TX_BUF_L,
 					       &stm32port->tx_dma_buf,
 					       GFP_KERNEL);
