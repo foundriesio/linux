d54ff0d73c8dabc52688f3522106cfeb1b62eb36
Stefan Agner <stefan.agner@toradex.com>
2017-11-23 11:09:15 +0100

    rpmsg: imx: return ENOTTY if no tty connected
    Return ENOTTY if no reader is using the TTY. Note that the
    VirtIO transport used by RPmsg currently does not use the
    return value of this callback, hence currently will not
    influence operation in any way.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
    Acked-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>

--- drivers/rpmsg/imx_rpmsg_tty.c
+++ drivers/rpmsg/imx_rpmsg_tty.c
@@ -48,7 +48,7 @@ static int rpmsg_tty_cb(struct rpmsg_device *rpdev, void *data, int len,
 	if (tty == NULL) {
 		dev_dbg(&rpdev->dev, "waiting for readers, discard len %d\n",
 			len);
-		return;
+		return -ENOTTY;
 	}
 
 	/* flush the recv-ed none-zero data to tty node */
