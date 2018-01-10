3e26e697b47506613c9ae2c1111be6e4ff2b1e4a
Antonio Borneo <borneo.antonio@gmail.com>
2018-01-10 11:12:37 +0100

    spi: stm32: move spi disable out of irq handler
    The spi disable could potentially require some time to finish.
    It has to be executed at the end of a transfer, but there is
    no reason to call it in the irq handler.
    
    Simplify the irq handler by moving out the spi disable. The
    synchronization through xfer_completion is used to defer the
    execution of spi disable.
    
    Change-Id: I5ab0ceb82db1f8d2e6af6e473baf1c0f2f923a36
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>
    Reviewed-on: https://gerrit.st.com/87178
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Antonio Maria BORNEO <antonio.borneo@st.com>
    Tested-by: Antonio Maria BORNEO <antonio.borneo@st.com>
    Reviewed-by: Amelie DELAUNAY <amelie.delaunay@st.com>
    Reviewed-by: Alexandre TORGUE <alexandre.torgue@st.com>

--- drivers/spi/spi-stm32.c
+++ drivers/spi/spi-stm32.c
@@ -958,15 +958,18 @@ static irqreturn_t stm32h7_spi_irq_thread(int irq, void *dev_id)
 		if (!spi->cur_usedma && (spi->rx_buf && (spi->rx_len > 0)))
 			stm32h7_spi_read_rxfifo(spi);
 
-	writel_relaxed(ifcr, spi->base + STM32H7_SPI_IFCR);
-
-	spin_unlock_irqrestore(&spi->lock, flags);
-
 	if (end) {
-		stm32h7_spi_disable(spi);
+		/* Disable interrupts and clear status flags */
+		writel_relaxed(0, spi->base + STM32H7_SPI_IER);
+		writel_relaxed(STM32H7_SPI_IFCR_ALL,
+			       spi->base + STM32H7_SPI_IFCR);
+
 		complete(&spi->xfer_completion);
+	} else {
+		writel_relaxed(ifcr, spi->base + STM32H7_SPI_IFCR);
 	}
 
+	spin_unlock_irqrestore(&spi->lock, flags);
 	return IRQ_HANDLED;
 }
 
@@ -1692,8 +1695,9 @@ static int stm32_spi_transfer_one(struct spi_master *master,
 	if (!ret) {
 		dev_err(spi->dev, "SPI transfer timeout (%u ms)\n", xfer_time);
 		spi->xfer_status = -ETIMEDOUT;
-		spi->cfg->disable(spi);
 	}
+
+	spi->cfg->disable(spi);
 
 	spi_finalize_current_transfer(master);
 
