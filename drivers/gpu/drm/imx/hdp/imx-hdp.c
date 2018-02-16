aa594296b8fcc4275c5ee5dbba250053c0a933cd
Stefan Agner <stefan.agner@toradex.com>
2018-02-16 16:07:24 +0100

    drm/imx/hdp: add HDMI CTRL GPIO support
    Add support to control HDMI levels using a GPIO. The driver simply
    sets the GPIO high for HDMI and low for DisplayPort.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
    (cherry picked from commit d8868d38dc929cc5c4591f583489cdc57e93e9f2)
    (cherry picked from commit b7d9e3c98e2d1681e2366f9011c599003e2179d3)
    (cherry picked from commit 59004b52a9dcd08a1c1e3436851b2179f4ac7727)
    (cherry picked from commit 060dd7a275260c26e0341dce9be59ec5c49722a4)

--- drivers/gpu/drm/imx/hdp/imx-hdp.c
+++ drivers/gpu/drm/imx/hdp/imx-hdp.c
@@ -21,6 +21,7 @@
 #include <linux/of.h>
 #include <linux/irq.h>
 #include <linux/of_device.h>
+#include <linux/of_gpio.h>
 
 #include "imx-hdp.h"
 #include "imx-hdmi.h"
@@ -1320,6 +1321,7 @@ static struct hdp_ops imx8qm_hdmi_ops = {
 };
 
 static struct hdp_devtype imx8qm_dp_devtype = {
+	.is_hdmi_level = false,
 	.audio_type = CDN_DPTX,
 	.ops = &imx8qm_dp_ops,
 	.rw = &imx8qm_rw,
@@ -1327,6 +1329,7 @@ static struct hdp_devtype imx8qm_dp_devtype = {
 };
 
 static struct hdp_devtype imx8qm_hdmi_devtype = {
+	.is_hdmi_level = true,
 	.audio_type = CDN_HDMITX_TYPHOON,
 	.ops = &imx8qm_hdmi_ops,
 	.rw = &imx8qm_rw,
@@ -1353,6 +1356,7 @@ static struct hdp_ops imx8mq_ops = {
 };
 
 static struct hdp_devtype imx8mq_hdmi_devtype = {
+	.is_hdmi_level = true,
 	.audio_type = CDN_HDMITX_KIRAN,
 	.ops = &imx8mq_ops,
 	.rw = &imx8mq_rw,
@@ -1634,6 +1638,18 @@ static int imx_hdp_imx_bind(struct device *dev, struct device *master,
 
 	INIT_DELAYED_WORK(&hdp->hotplug_work, hotplug_work_func);
 
+	hdp->hdmi_ctrl_gpio = of_get_named_gpio(dev->of_node, "hdmi-ctrl-gpios", 0);
+	if (gpio_is_valid(hdp->hdmi_ctrl_gpio)) {
+		ret = gpio_request(hdp->hdmi_ctrl_gpio, "HDMI_CTRL");
+		if (ret < 0) {
+			dev_err(dev, "request HDMI CTRL GPIO failed: %d\n", ret);
+			goto err_cleanup_encoder;
+		}
+
+		/* Set signals depending on HDP device type */
+		gpio_direction_output(hdp->hdmi_ctrl_gpio, devtype->is_hdmi_level);
+	}
+
 	/* Check cable states before enable irq */
 	imx_hdp_call(hdp, get_hpd_state, &hdp->state, &hpd);
 
@@ -1647,7 +1663,7 @@ static int imx_hdp_imx_bind(struct device *dev, struct device *master,
 		if (ret) {
 			dev_err(&pdev->dev, "can't claim irq %d\n",
 							hdp->irq[HPD_IRQ_IN]);
-			goto err_irq;
+			goto err_free_hdmi_gpio;
 		}
 		/* Cable Disconnedted, enable Plug in IRQ */
 		if (hpd == 0)
@@ -1662,7 +1678,7 @@ static int imx_hdp_imx_bind(struct device *dev, struct device *master,
 		if (ret) {
 			dev_err(&pdev->dev, "can't claim irq %d\n",
 							hdp->irq[HPD_IRQ_OUT]);
-			goto err_irq;
+			goto err_free_hdmi_gpio;
 		}
 		/* Cable Connected, enable Plug out IRQ */
 		if (hpd == 1)
@@ -1678,7 +1694,11 @@ static int imx_hdp_imx_bind(struct device *dev, struct device *master,
 	imx_hdp_register_audio_driver(dev);
 
 	return 0;
-err_irq:
+
+err_free_hdmi_gpio:
+	if (gpio_is_valid(hdp->hdmi_ctrl_gpio))
+		gpio_free(hdp->hdmi_ctrl_gpio);
+err_cleanup_encoder:
 	drm_encoder_cleanup(encoder);
 	return ret;
 }
@@ -1693,6 +1713,9 @@ static void imx_hdp_imx_unbind(struct device *dev, struct device *master,
 		imx_cec_unregister(&hdp->cec);
 #endif
 	imx_hdp_call(hdp, pixel_clock_disable, &hdp->clks);
+
+	if (gpio_is_valid(hdp->hdmi_ctrl_gpio))
+		gpio_free(hdp->hdmi_ctrl_gpio);
 }
 
 static const struct component_ops imx_hdp_imx_ops = {
