25a5ccce0c6e0a229de6624427a76a0c45d13cb6
Borislav Petkov <bp@suse.de>
2018-05-11 11:30:17 +0200

    x86/bugs/AMD: Add support to disable RDS on Fam[15,16,17]h if requested (bsc#1087082 CVE-2018-3639).
    suse-commit: 09293d5f9396cee0584643ad9fd79a1f416ddb23

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -216,6 +216,7 @@
 #define X86_FEATURE_USE_IBRS_FW		( 7*32+22) /* "" Use IBRS during runtime firmware calls */
 #define X86_FEATURE_USE_IBRS		( 7*32+23) /* "" Use IBRS for Spectre v2 safety */
 #define X86_FEATURE_SPEC_STORE_BYPASS_DISABLE	( 7*32+24) /* "" Disable Speculative Store Bypass. */
+#define X86_FEATURE_AMD_RDS		(7*32+25)  /* "" AMD RDS implementation */
 
 /* Virtualization flags: Linux defined, word 8 */
 #define X86_FEATURE_TPR_SHADOW  ( 8*32+ 0) /* Intel TPR Shadow */
