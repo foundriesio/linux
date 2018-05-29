1c56ca2afb0c1b99a9a53cb4f4ed362f23da3a18
Oliver Neukum <oneukum@suse.com>
2018-05-29 11:43:54 +0200

    usb: xhci: Fix potential memory leak in xhci_disable_slot() (bsc#1085539).
    suse-commit: 2cb40353f08581cf7e9307445fc4df8252cc79c8

--- drivers/usb/host/xhci-hub.c
+++ drivers/usb/host/xhci-hub.c
@@ -624,7 +624,7 @@ static int xhci_enter_test_mode(struct xhci_hcd *xhci,
 	xhci_dbg(xhci, "Disable all slots\n");
 	spin_unlock_irqrestore(&xhci->lock, *flags);
 	for (i = 1; i <= HCS_MAX_SLOTS(xhci->hcs_params1); i++) {
-		retval = xhci_disable_slot(xhci, NULL, i);
+		retval = xhci_disable_slot(xhci, i);
 		if (retval)
 			xhci_err(xhci, "Failed to disable slot %d, %d. Enter test mode anyway\n",
 				 i, retval);
