8782af762ab64abfe52f9b6c5ad99483342c72d9
Borislav Petkov <bp@suse.de>
2018-06-05 17:06:23 +0200

    x86/speculation: Handle HT correctly on AMD (bsc#1087082 CVE-2018-3639).
    suse-commit: 5045d5a71c6c6e68eb90d0afb01972c12a6f67be

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -79,6 +79,7 @@
 #include <asm/misc.h>
 #include <asm/intel-family.h>
 #include <asm/cpu_device_id.h>
+#include <asm/spec-ctrl.h>
 
 /* Number of siblings per CPU package */
 int smp_num_siblings = 1;
@@ -242,6 +243,8 @@ static void notrace start_secondary(void *unused)
 	 * Check TSC synchronization with the BP:
 	 */
 	check_tsc_sync_target();
+
+	speculative_store_bypass_ht_init();
 
 	/*
 	 * Lock vector_lock and initialize the vectors on this cpu
@@ -1320,6 +1323,8 @@ void __init native_smp_prepare_cpus(unsigned int max_cpus)
 	set_mtrr_aps_delayed_init();
 
 	smp_quirk_init_udelay();
+
+	speculative_store_bypass_ht_init();
 }
 
 void arch_enable_nonboot_cpus_begin(void)
