be8f9c1a441f9615702b7028e1db13e820de44e7
Jiri Slaby <jslaby@suse.cz>
2018-06-06 08:45:50 +0200

    kABI: protect tap_create_cdev (kabi).
    suse-commit: d6151ad5a1b9863922dd14f92ffdf7876c53ea30

--- include/linux/if_tap.h
+++ include/linux/if_tap.h
@@ -68,8 +68,10 @@ void tap_del_queues(struct tap_dev *tap);
 int tap_get_minor(dev_t major, struct tap_dev *tap);
 void tap_free_minor(dev_t major, struct tap_dev *tap);
 int tap_queue_resize(struct tap_dev *tap);
-int tap_create_cdev(struct cdev *tap_cdev, dev_t *tap_major,
+int tap_create_cdev4(struct cdev *tap_cdev, dev_t *tap_major,
 		    const char *device_name, struct module *module);
+int tap_create_cdev(struct cdev *tap_cdev, dev_t *tap_major,
+		    const char *device_name);
 void tap_destroy_cdev(dev_t major, struct cdev *tap_cdev);
 
 #endif /*_LINUX_IF_TAP_H_*/
