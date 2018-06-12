31a00755a62d4e79f8e74bb22283f728cd575990
Coly Li <colyli@suse.de>
2018-06-12 16:15:27 +0800

    md: use TASK_IDLE instead of blocking signals (bsc#1093023).
    suse-commit: 0dcdfb9bbe4efeb26671391b0c667e20e352234d

--- drivers/md/raid1.c
+++ drivers/md/raid1.c
@@ -1320,18 +1320,14 @@ static void raid1_write_request(struct mddev *mddev, struct bio *bio,
 		 */
 		DEFINE_WAIT(w);
 		for (;;) {
-			sigset_t full, old;
 			prepare_to_wait(&conf->wait_barrier,
-					&w, TASK_INTERRUPTIBLE);
+					&w, TASK_IDLE);
 			if (!mddev_is_clustered(mddev) ||
 			    !md_cluster_ops->area_resyncing(mddev, WRITE,
 							bio->bi_iter.bi_sector,
 							bio_end_sector(bio)))
 				break;
-			sigfillset(&full);
-			sigprocmask(SIG_BLOCK, &full, &old);
 			schedule();
-			sigprocmask(SIG_SETMASK, &old, NULL);
 		}
 		finish_wait(&conf->wait_barrier, &w);
 	}
