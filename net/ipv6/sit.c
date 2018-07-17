d3a74ae4f646a87dd7b20c5608c10ac617f73777
Jiri Slaby <jslaby@suse.cz>
2018-07-17 12:51:16 +0200

    ip6_tunnel: remove magic mtu value 0xFFF8 (networking-stable-18_06_08).
    suse-commit: 3f1be65e6f3636c6476b47af764f550093984114

--- net/ipv6/sit.c
+++ net/ipv6/sit.c
@@ -1360,7 +1360,7 @@ static void ipip6_tunnel_setup(struct net_device *dev)
 	dev->hard_header_len	= LL_MAX_HEADER + t_hlen;
 	dev->mtu		= ETH_DATA_LEN - t_hlen;
 	dev->min_mtu		= IPV6_MIN_MTU;
-	dev->max_mtu		= 0xFFF8 - t_hlen;
+	dev->max_mtu		= IP6_MAX_MTU - t_hlen;
 	dev->flags		= IFF_NOARP;
 	netif_keep_dst(dev);
 	dev->addr_len		= 4;
