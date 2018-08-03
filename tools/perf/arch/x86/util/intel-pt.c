b7a9fa6d53714aac0b53f2469f2e3fbe8b5b9bd6
Tony Jones <tonyj@suse.de>
2018-08-03 02:43:49 +0200

    perf intel-pt: Always set no branch for dummy event (bsc#1087217).
    suse-commit: 62acd96f81db7c160f9b3a41a776b1a5f75d59ac

--- tools/perf/arch/x86/util/intel-pt.c
+++ tools/perf/arch/x86/util/intel-pt.c
@@ -700,6 +700,7 @@ static int intel_pt_recording_options(struct auxtrace_record *itr,
 				perf_evsel__set_sample_bit(switch_evsel, TID);
 				perf_evsel__set_sample_bit(switch_evsel, TIME);
 				perf_evsel__set_sample_bit(switch_evsel, CPU);
+				perf_evsel__reset_sample_bit(switch_evsel, BRANCH_STACK);
 
 				opts->record_switch_events = false;
 				ptr->have_sched_switch = 3;
@@ -761,6 +762,7 @@ static int intel_pt_recording_options(struct auxtrace_record *itr,
 			/* And the CPU for switch events */
 			perf_evsel__set_sample_bit(tracking_evsel, CPU);
 		}
+		perf_evsel__reset_sample_bit(tracking_evsel, BRANCH_STACK);
 	}
 
 	/*
