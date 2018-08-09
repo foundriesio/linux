57047b8779f17124371c3672ff80bb47bd63ae8f
Hannes Reinecke <hare@suse.de>
2018-08-09 10:50:29 +0200

    scsi: qla2xxx: Fix sending ADISC command for login (bsc#1086327,FATE#324903).
    suse-commit: 3c73296038d83072d23917a0062f9e0e0d4d03be

--- drivers/scsi/qla2xxx/qla_init.c
+++ drivers/scsi/qla2xxx/qla_init.c
@@ -1169,9 +1169,6 @@ int qla24xx_fcport_handle_login(struct scsi_qla_host *vha, fc_port_t *fcport)
 	    fcport->login_gen, fcport->login_retry,
 	    fcport->loop_id, fcport->scan_state);
 
-	if (fcport->login_retry == 0)
-		return 0;
-
 	if (fcport->scan_state != QLA_FCPORT_FOUND)
 		return 0;
 
@@ -1196,7 +1193,8 @@ int qla24xx_fcport_handle_login(struct scsi_qla_host *vha, fc_port_t *fcport)
 		return 0;
 	}
 
-	fcport->login_retry--;
+	if (fcport->login_retry > 0)
+		fcport->login_retry--;
 
 	switch (fcport->disc_state) {
 	case DSC_DELETED:
