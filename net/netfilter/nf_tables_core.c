c46154efd8953c421d1155567b9216508d4d867d
Jiri Slaby <jslaby@suse.cz>
2018-08-15 15:10:54 +0200

    netfilter: nf_tables: don't assume chain stats are set when jumplabel is set (netfilter-stable-18_06_27).
    suse-commit: df3ef3f51bd25c48fa08c2e405a34cd24223b33e

--- net/netfilter/nf_tables_core.c
+++ net/netfilter/nf_tables_core.c
@@ -124,6 +124,7 @@ unsigned int
 nft_do_chain(struct nft_pktinfo *pkt, void *priv)
 {
 	const struct nft_chain *chain = priv, *basechain = chain;
+	struct nft_base_chain *base_chain;
 	const struct net *net = nft_net(pkt);
 	const struct nft_rule *rule;
 	const struct nft_expr *expr, *last;
@@ -221,14 +222,20 @@ next_rule:
 	nft_trace_packet(&info, basechain, NULL, -1,
 			 NFT_TRACETYPE_POLICY);
 
-	rcu_read_lock_bh();
-	stats = this_cpu_ptr(rcu_dereference(nft_base_chain(basechain)->stats));
-	u64_stats_update_begin(&stats->syncp);
-	stats->pkts++;
-	stats->bytes += pkt->skb->len;
-	u64_stats_update_end(&stats->syncp);
-	rcu_read_unlock_bh();
+	base_chain = nft_base_chain(basechain);
+	if (!base_chain->stats)
+		goto end;
 
+	stats = this_cpu_ptr(rcu_dereference(base_chain->stats));
+	if (stats) {
+		rcu_read_lock_bh();
+		u64_stats_update_begin(&stats->syncp);
+		stats->pkts++;
+		stats->bytes += pkt->skb->len;
+		u64_stats_update_end(&stats->syncp);
+		rcu_read_unlock_bh();
+	}
+end:
 	return nft_base_chain(basechain)->policy;
 }
 EXPORT_SYMBOL_GPL(nft_do_chain);
