17c62debf9d54215e1f699932795578f377952d0
Jiri Slaby <jslaby@suse.cz>
2018-08-17 14:01:37 +0200

    - net/mlx5: E-Switch, Avoid setup attempt if not being e-switch   manager (networking-stable-18_07_19). - kABI: protect eswitch.h include (kabi).
    suse-commit: 5270a1a71751e243bfc812f5f4e01981f46c874a

--- drivers/net/ethernet/mellanox/mlx5/core/fw.c
+++ drivers/net/ethernet/mellanox/mlx5/core/fw.c
@@ -34,6 +34,9 @@
 #include <linux/mlx5/cmd.h>
 #include <linux/module.h>
 #include "mlx5_core.h"
+#ifndef __GENKSYMS__
+#include "eswitch.h"
+#endif
 #include "../../mlxfw/mlxfw.h"
 
 static int mlx5_cmd_query_adapter(struct mlx5_core_dev *dev, u32 *out,
@@ -152,13 +155,13 @@ int mlx5_query_hca_caps(struct mlx5_core_dev *dev)
 	}
 
 	if (MLX5_CAP_GEN(dev, vport_group_manager) &&
-	    MLX5_CAP_GEN(dev, eswitch_flow_table)) {
+	    MLX5_ESWITCH_MANAGER(dev)) {
 		err = mlx5_core_get_caps(dev, MLX5_CAP_ESWITCH_FLOW_TABLE);
 		if (err)
 			return err;
 	}
 
-	if (MLX5_CAP_GEN(dev, eswitch_flow_table)) {
+	if (MLX5_ESWITCH_MANAGER(dev)) {
 		err = mlx5_core_get_caps(dev, MLX5_CAP_ESWITCH);
 		if (err)
 			return err;
