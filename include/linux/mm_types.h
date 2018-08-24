ee308ed3228920e4e3756aeb1bf7fd45ebbb3ac1
Petr Tesarik <ptesarik@suse.cz>
2018-08-24 19:11:31 +0200

    kabi: move s390 mm_context_t lock to mm_struct and ignore the change (bsc#1103421).
    suse-commit: 1da3fff6c178e6c781ae526431b1da24f67f1915

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -509,6 +509,9 @@ struct mm_struct {
 	/* HMM needs to track a few things per mm */
 	struct hmm *hmm;
 #endif
+#if defined(CONFIG_S390) && !defined(__GENKSYMS__)
+	spinlock_t mm_context_lock;
+#endif
 };
 
 extern struct mm_struct init_mm;
