8f40215b43fda503a8194e2ad1a7e8e34a9d092f
Michal Hocko <mhocko@suse.com>
2018-08-27 14:35:10 +0200

    x86/speculation/l1tf: Fix off-by-one error when warning that system has too much RAM (bnc#1105536).
    suse-commit: bdf708f28a7e07e2acbc02b2dfd163dd313e3316

--- arch/x86/mm/init.c
+++ arch/x86/mm/init.c
@@ -891,7 +891,7 @@ unsigned long max_swapfile_size(void)
 
 	if (boot_cpu_has_bug(X86_BUG_L1TF)) {
 		/* Limit the swap file size to MAX_PA/2 for L1TF workaround */
-		pages = min_t(unsigned long, l1tf_pfn_limit() + 1, pages);
+		pages = min_t(unsigned long, l1tf_pfn_limit(), pages);
 	}
 	return pages;
 }
