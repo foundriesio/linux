effde451ebf0354f50f2ea2475ff0fc2795cf95c
Borislav Petkov <bp@suse.de>
2018-09-03 13:45:45 +0200

    - KVM: x86: SVM: Call x86_spec_ctrl_set_guest/host() with   interrupts disabled (git-fixes 1f50ddb4f418). - Refresh   patches.arch/36-x86-bugs-kvm-extend-speculation-control-for-virt_spec_ctrl.patch. - Refresh   patches.suse/05-x86-bugs-kvm-support-the-combination-of-guest-and-host-ibrs.patch.
    suse-commit: d04f33604b49ca5ae92b52f68f94e8afcd18b776

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -5472,8 +5472,6 @@ static void svm_vcpu_run(struct kvm_vcpu *vcpu)
 
 	clgi();
 
-	local_irq_enable();
-
 	/*
 	 * If this vCPU has touched SPEC_CTRL, restore the guest's value if
 	 * it's non-zero. Since vmentry is serialising on affected CPUs, there
@@ -5481,6 +5479,8 @@ static void svm_vcpu_run(struct kvm_vcpu *vcpu)
 	 * being speculatively taken.
 	 */
 	x86_spec_ctrl_set_guest(svm->spec_ctrl, svm->virt_spec_ctrl);
+
+	local_irq_enable();
 
 	asm volatile (
 		"push %%" _ASM_BP "; \n\t"
@@ -5609,6 +5609,9 @@ static void svm_vcpu_run(struct kvm_vcpu *vcpu)
 	reload_tss(vcpu);
 
 	local_irq_disable();
+
+	if (svm->spec_ctrl)
+		native_wrmsrl(MSR_IA32_SPEC_CTRL, 0);
 
 	vcpu->arch.cr2 = svm->vmcb->save.cr2;
 	vcpu->arch.regs[VCPU_REGS_RAX] = svm->vmcb->save.rax;
