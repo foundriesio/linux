60367116cc11358de7485b5daf81d2e8790846ff
Guo Ren <ren_guo@c-sky.com>
2018-09-05 14:25:14 +0800

    csky: VDSO and rt_sigreturn
    This patch adds files related to VDSO and our VDSO only support
    rt_sigreturn.
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

---
+++ arch/csky/include/asm/vdso.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2018 Hangzhou C-SKY Microsystems co.,ltd.
+
+#ifndef __ASM_CSKY_VDSO_H
+#define __ASM_CSKY_VDSO_H
+
+#include <abi/vdso.h>
+
+struct csky_vdso {
+	unsigned short rt_signal_retcode[4];
+};
+
+#endif /* __ASM_CSKY_VDSO_H */
