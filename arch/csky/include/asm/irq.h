fbb4e798d1588e9df2da16818932747430068baf
Guo Ren <ren_guo@c-sky.com>
2018-09-05 14:25:15 +0800

    csky: IRQ handling
    This patch adds IRQ handling files.
    
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

---
+++ arch/csky/include/asm/irq.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2018 Hangzhou C-SKY Microsystems co.,ltd.
+
+#ifndef __ASM_CSKY_IRQ_H
+#define __ASM_CSKY_IRQ_H
+
+#include <asm-generic/irq.h>
+
+#endif /* __ASM_CSKY_IRQ_H */
