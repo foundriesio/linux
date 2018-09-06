d7c9b3380ffbbe7c590ad709be0f76e596f15a5d
Olivier Moysan <olivier.moysan@st.com>
2018-09-06 17:50:47 +0200

    ASoC: stm32: sai: fix iec958 controls indexation
    Allow indexation of sai iec958 controls according
    to device id.
    
    Change-Id: I14f1ac0e1e891244e0bacdfb7af758d2b7160bf1
    Signed-off-by: Olivier Moysan <olivier.moysan@st.com>

--- sound/soc/stm/stm32_sai_sub.c
+++ sound/soc/stm/stm32_sai_sub.c
@@ -1075,11 +1075,12 @@ static int stm32_sai_pcm_new(struct snd_soc_pcm_runtime *rtd,
 			     struct snd_soc_dai *cpu_dai)
 {
 	struct stm32_sai_sub_data *sai = dev_get_drvdata(cpu_dai->dev);
+	struct snd_kcontrol_new knew = iec958_ctls;
 
 	if (STM_SAI_PROTOCOL_IS_SPDIF(sai)) {
 		dev_dbg(&sai->pdev->dev, "%s: register iec controls", __func__);
-		return snd_ctl_add(rtd->pcm->card,
-				   snd_ctl_new1(&iec958_ctls, sai));
+		knew.device = rtd->pcm->device;
+		return snd_ctl_add(rtd->pcm->card, snd_ctl_new1(&knew, sai));
 	}
 
 	return 0;
