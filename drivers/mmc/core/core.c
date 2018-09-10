9f40e9a553388f4fe73f2d50c91dfc76a2975070
Nicolas Saenz Julienne <nsaenzjulienne@suse.de>
2018-09-10 18:16:49 +0200

    mmc: core: Ensure cmd_completion is initialized (FATE#324519).
    suse-commit: 429d6267a00abb538a7e3b7069b136ab7e0cc5e7

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -351,6 +351,8 @@ int mmc_start_request(struct mmc_host *host, struct mmc_request *mrq)
 {
 	int err;
 
+	init_completion(&mrq->cmd_completion);
+
 	mmc_retune_hold(host);
 
 	if (mmc_card_removed(host->card))
@@ -421,8 +423,6 @@ static int __mmc_start_data_req(struct mmc_host *host, struct mmc_request *mrq)
 	mrq->done = mmc_wait_data_done;
 	mrq->host = host;
 
-	init_completion(&mrq->cmd_completion);
-
 	err = mmc_start_request(host, mrq);
 	if (err) {
 		mrq->cmd->error = err;
@@ -441,8 +441,6 @@ static int __mmc_start_req(struct mmc_host *host, struct mmc_request *mrq)
 
 	init_completion(&mrq->completion);
 	mrq->done = mmc_wait_done;
-
-	init_completion(&mrq->cmd_completion);
 
 	err = mmc_start_request(host, mrq);
 	if (err) {
