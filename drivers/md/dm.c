f11a80aef330b6473125b4595cc8104a2a8ab867
Jan Kara <jack@suse.cz>
2018-09-13 14:28:02 +0200

    dax: Introduce a ->copy_to_iter dax operation (bsc#1098782).
    suse-commit: dd19686bc1df44ceb82ba98b3a90b78136137e50

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1016,6 +1016,12 @@ static size_t dm_dax_copy_from_iter(struct dax_device *dax_dev, pgoff_t pgoff,
 	return ret;
 }
 
+static size_t dm_dax_copy_to_iter(struct dax_device *dax_dev, pgoff_t pgoff,
+		void *addr, size_t bytes, struct iov_iter *i)
+{
+	return copy_to_iter(addr, bytes, i);
+}
+
 /*
  * A target may call dm_accept_partial_bio only from the map routine.  It is
  * allowed for all bio types except REQ_PREFLUSH and REQ_OP_ZONE_RESET.
@@ -2994,6 +3000,7 @@ static const struct block_device_operations dm_blk_dops = {
 static const struct dax_operations dm_dax_ops = {
 	.direct_access = dm_dax_direct_access,
 	.copy_from_iter = dm_dax_copy_from_iter,
+	.copy_to_iter = dm_dax_copy_to_iter,
 };
 
 /*
