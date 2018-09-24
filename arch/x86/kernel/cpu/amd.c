7dc3e9a723044aae92be1f7e432325a4fd117afc
Joerg Roedel <jroedel@suse.de>
2018-09-24 13:49:20 +0200

    - perf/events/amd/uncore: Fix amd_uncore_llc ID to use pre-defined   cpu_llc_id (fate#324366). - perf/x86/amd/uncore: Get correct number of cores sharing last   level cache (fate#324366). - x86/CPU/AMD: Calculate last level cache ID from number of   sharing threads (fate#324366). - x86/CPU/AMD: Derive CPU topology from CPUID function 0xB when   available (fate#324366). - x86/CPU/AMD: Fix LLC ID bit-shift calculation (fate#324366). - x86/CPU/AMD: Have smp_num_siblings and cpu_llc_id always be   present (fate#324366). - x86/CPU: Modify detect_extended_topology() to return result   (fate#324366). - x86/CPU: Rename intel_cacheinfo.c to cacheinfo.c (fate#324366). - x86: irq_remapping: Move irq remapping mode enum (fate#324366). - iommu/amd: Add support for higher 64-bit IOMMU Control Register   (fate#324366). - iommu/amd: Add support for IOMMU XT mode (fate#324366). - Refresh   patches.arch/08-x86-cpu-remove-the-pointless-cpu-printout.patch. - Refresh   patches.arch/11-x86-cpu-topology-provide-detect_extended_topology_early.patch. - Refresh   patches.arch/13-x86-cpu-amd-do-not-check-cpuid-max-ext-level-before-parsing-smp-info.patch. - Refresh   patches.arch/35-x86-speculation-handle-ht-correctly-on-amd.patch. - Refresh   patches.suse/11-x86-bugs-amd-add-support-to-disable-rds-on-famh-if-requested.patch. - Refresh   patches.suse/13-x86-speculation-create-spec-ctrl-h-to-avoid-include-hell.patch.
    suse-commit: a4a3e5468ceb8089718f385f14fb32861c9f0c80

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -9,6 +9,7 @@
 #include <linux/random.h>
 #include <asm/processor.h>
 #include <asm/apic.h>
+#include <asm/cacheinfo.h>
 #include <asm/cpu.h>
 #include <asm/spec-ctrl.h>
 #include <asm/smp.h>
@@ -326,7 +327,6 @@ static void amd_get_topology_early(struct cpuinfo_x86 *c)
  *     Assumption: Number of cores in each internal node is the same.
  * (2) AMD processors supporting compute units
  */
-#ifdef CONFIG_SMP
 static void amd_get_topology(struct cpuinfo_x86 *c)
 {
 	u8 node_id;
@@ -334,6 +334,7 @@ static void amd_get_topology(struct cpuinfo_x86 *c)
 
 	/* get information required for multi-node processors */
 	if (boot_cpu_has(X86_FEATURE_TOPOEXT)) {
+		int err;
 		u32 eax, ebx, ecx, edx;
 
 		cpuid(0x8000001e, &eax, &ebx, &ecx, &edx);
@@ -351,21 +352,15 @@ static void amd_get_topology(struct cpuinfo_x86 *c)
 		}
 
 		/*
-		 * We may have multiple LLCs if L3 caches exist, so check if we
-		 * have an L3 cache by looking at the L3 cache CPUID leaf.
+		 * In case leaf B is available, use it to derive
+		 * topology information.
 		 */
-		if (cpuid_edx(0x80000006)) {
-			if (c->x86 == 0x17) {
-				/*
-				 * LLC is at the core complex level.
-				 * Core complex id is ApicId[3].
-				 */
-				per_cpu(cpu_llc_id, cpu) = c->apicid >> 3;
-			} else {
-				/* LLC is at the node level. */
-				per_cpu(cpu_llc_id, cpu) = node_id;
-			}
-		}
+		err = detect_extended_topology(c);
+		if (!err)
+			c->x86_coreid_bits = get_count_order(c->x86_max_cores);
+
+		cacheinfo_amd_init_llc_id(c, cpu, node_id);
+
 	} else if (cpu_has(c, X86_FEATURE_NODEID_MSR)) {
 		u64 value;
 
@@ -381,7 +376,6 @@ static void amd_get_topology(struct cpuinfo_x86 *c)
 		legacy_fixup_core_id(c);
 	}
 }
-#endif
 
 /*
  * On a AMD dual core setup the lower bits of the APIC id distinguish the cores.
@@ -389,7 +383,6 @@ static void amd_get_topology(struct cpuinfo_x86 *c)
  */
 static void amd_detect_cmp(struct cpuinfo_x86 *c)
 {
-#ifdef CONFIG_SMP
 	unsigned bits;
 	int cpu = smp_processor_id();
 
@@ -400,17 +393,11 @@ static void amd_detect_cmp(struct cpuinfo_x86 *c)
 	c->phys_proc_id = c->initial_apicid >> bits;
 	/* use socket ID also for last level cache */
 	per_cpu(cpu_llc_id, cpu) = c->phys_proc_id;
-	amd_get_topology(c);
-#endif
 }
 
 u16 amd_get_nb_id(int cpu)
 {
-	u16 id = 0;
-#ifdef CONFIG_SMP
-	id = per_cpu(cpu_llc_id, cpu);
-#endif
-	return id;
+	return per_cpu(cpu_llc_id, cpu);
 }
 EXPORT_SYMBOL_GPL(amd_get_nb_id);
 
@@ -865,6 +852,7 @@ static void init_amd(struct cpuinfo_x86 *c)
 	cpu_detect_cache_sizes(c);
 
 	amd_detect_cmp(c);
+	amd_get_topology(c);
 	srat_detect_node(c);
 	init_amd_cacheinfo(c);
 
