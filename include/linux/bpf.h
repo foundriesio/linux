bc600a598a2ae539ac2715b6535b1f1cf7f01a69
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2018-09-28 16:03:07 +0200

    - net: fix build error in devmap helper calls (bsc#1109837). - Refresh   patches.fixes/netfilter-xt_bpf-Fix-XT_BPF_MODE_FD_PINNED-mode-of-x.patch.
    suse-commit: 7c58943d0e5565685f4bd14b3c21cbdca632a58e

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -312,6 +312,12 @@ static inline void bpf_long_memcpy(void *dst, const void *src, u32 size)
 
 /* verify correctness of eBPF program */
 int bpf_check(struct bpf_prog **fp, union bpf_attr *attr);
+
+/* Map specifics */
+struct net_device  *__dev_map_lookup_elem(struct bpf_map *map, u32 key);
+void __dev_map_insert_ctx(struct bpf_map *map, u32 index);
+void __dev_map_flush(struct bpf_map *map);
+
 #else
 static inline struct bpf_prog *bpf_prog_get(u32 ufd)
 {
@@ -357,6 +363,20 @@ static inline void __bpf_prog_uncharge(struct user_struct *user, u32 pages)
 {
 }
 
+static inline struct net_device  *__dev_map_lookup_elem(struct bpf_map *map,
+						       u32 key)
+{
+	return NULL;
+}
+
+static inline void __dev_map_insert_ctx(struct bpf_map *map, u32 index)
+{
+}
+
+static inline void __dev_map_flush(struct bpf_map *map)
+{
+}
+
 static inline int bpf_obj_get_user(const char __user *pathname)
 {
 	return -EOPNOTSUPP;
@@ -383,10 +403,5 @@ extern const struct bpf_func_proto bpf_get_stackid_proto;
 /* Shared helpers among cBPF and eBPF. */
 void bpf_user_rnd_init_once(void);
 u64 bpf_user_rnd_u32(u64 r1, u64 r2, u64 r3, u64 r4, u64 r5);
-
-/* Map specifics */
-struct net_device  *__dev_map_lookup_elem(struct bpf_map *map, u32 key);
-void __dev_map_insert_ctx(struct bpf_map *map, u32 index);
-void __dev_map_flush(struct bpf_map *map);
 
 #endif /* _LINUX_BPF_H */
