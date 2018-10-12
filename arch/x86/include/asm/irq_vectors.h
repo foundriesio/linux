6d82b6b23230b9a05428a67e87554cacc24eaa95
Olaf Hering <ohering@suse.de>
2018-10-12 10:49:35 +0000

    x86/hyperv: Reenlightenment notifications support (bsc#1107207).
    suse-commit: fe275eb567f6fc9a55f8115c688fd491af4e2b2e

--- arch/x86/include/asm/irq_vectors.h
+++ arch/x86/include/asm/irq_vectors.h
@@ -106,7 +106,12 @@
  * to work around the 'lost local interrupt if more than 2 IRQ
  * sources per level' errata.
  */
-#define LOCAL_TIMER_VECTOR		0xef
+
+#if IS_ENABLED(CONFIG_HYPERV)
+#define HYPERV_REENLIGHTENMENT_VECTOR	0xee
+#endif
+
+#define LOCAL_TIMER_VECTOR		0xed
 
 #define NR_VECTORS			 256
 
--- arch/x86/kernel/cpu/mshyperv.c
+++ arch/x86/kernel/cpu/mshyperv.c
@@ -256,6 +256,12 @@ static void __init ms_hyperv_init_platform(void)
 	if (!test_bit(HYPERVISOR_CALLBACK_VECTOR, used_vectors))
 		alloc_intr_gate(HYPERVISOR_CALLBACK_VECTOR,
 				hyperv_callback_vector);
+
+	/* Setup the IDT for reenlightenment notifications */
+	if (ms_hyperv.features & HV_X64_ACCESS_REENLIGHTENMENT && !test_bit(HYPERVISOR_CALLBACK_VECTOR, used_vectors))
+		alloc_intr_gate(HYPERV_REENLIGHTENMENT_VECTOR,
+				hyperv_reenlightenment_vector);
+
 #endif
 }
 
