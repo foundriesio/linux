5ae84d1486e405dad51f77d229712713d08526b8
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2018-10-15 14:01:39 +0200

    bpf: report offload info to user space (bsc#1109837).
    suse-commit: 49ac00b21beb405746dd91a9b431fafa76af2649

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -894,6 +894,10 @@ enum sk_action {
 
 #define BPF_TAG_SIZE	8
 
+enum bpf_prog_status {
+	BPF_PROG_STATUS_DEV_BOUND	= (1 << 0),
+};
+
 struct bpf_prog_info {
 	__u32 type;
 	__u32 id;
@@ -907,6 +911,8 @@ struct bpf_prog_info {
 	__u32 nr_map_ids;
 	__aligned_u64 map_ids;
 	char name[BPF_OBJ_NAME_LEN];
+	__u32 ifindex;
+	__u32 status;
 } __attribute__((aligned(8)));
 
 struct bpf_map_info {
