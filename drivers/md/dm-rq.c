771ffbdc89d9388082eff106d4e5ee5057e97119
Hannes Reinecke <hare@suse.de>
2018-10-17 16:34:50 +0200

    dm: various cleanups to md->queue initialization code (bsc#1104967,FATE#325924).
    suse-commit: 6eaa484ee8b19499b7f3f8ac18bc50015b64c1d9

--- drivers/md/dm-rq.c
+++ drivers/md/dm-rq.c
@@ -717,7 +717,6 @@ int dm_old_init_request_queue(struct mapped_device *md, struct dm_table *t)
 	/* disable dm_old_request_fn's merge heuristic by default */
 	md->seq_rq_merge_deadline_usecs = 0;
 
-	dm_init_normal_md_queue(md);
 	blk_queue_softirq_done(md->queue, dm_softirq_done);
 
 	/* Initialize the request-based DM worker thread */
@@ -824,7 +823,6 @@ int dm_mq_init_request_queue(struct mapped_device *md, struct dm_table *t)
 		err = PTR_ERR(q);
 		goto out_tag_set;
 	}
-	dm_init_md_queue(md);
 
 	return 0;
 
