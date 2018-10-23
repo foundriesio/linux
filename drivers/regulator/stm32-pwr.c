312a57cbcf238997fb2d545b696e49def48327c6
Pascal Paillet <p.paillet@st.com>
2018-10-23 11:24:55 +0200

    regulator: Add support for stm32 power regulators
    Add support for 1V1 1V8 USB3V3 power regulators.
    
    Change-Id: I4b1323016d3297e8441787f6debf72e2522d6af7
    Reviewed-on: https://gerrit.st.com/79029
    Reviewed-by: Ludovic BARRE <ludovic.barre@st.com>
    Signed-off-by: Pascal Paillet <p.paillet@st.com>

---
+++ drivers/regulator/stm32-pwr.c
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) STMicroelectronics SA 2017
+ * Author: Gabriel Fernandez <gabriel.fernandez@st.com>
+ *
+ * License terms: GPL V2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/err.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/mfd/syscon.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/platform_device.h>
+#include <linux/regulator/driver.h>
+#include <linux/regulator/machine.h>
+#include <linux/regulator/of_regulator.h>
+#include <linux/regmap.h>
+#include <linux/slab.h>
+
+/*
+ * Registers
+ */
+#define REG_PWR_CR3 0x0C
+
+/*
+ * SYSTEM_PARAMETER
+ */
+#define REG_1_1_EN  BIT(30)
+#define REG_1_8_EN  BIT(28)
+#define USB_3_3_EN  BIT(24)
+
+static const struct regulator_ops stm32_pwr_reg_ops = {
+	.list_voltage	= regulator_list_voltage_linear,
+	.enable		= regulator_enable_regmap,
+	.disable	= regulator_disable_regmap,
+	.is_enabled	= regulator_is_enabled_regmap,
+};
+
+static const struct regulator_desc stm32_pwr_reg11 = {
+	.name = "REG11",
+	.of_match = of_match_ptr("reg11"),
+	.n_voltages = 1,
+	.type = REGULATOR_VOLTAGE,
+	.min_uV = 1100000,
+	.fixed_uV = 1100000,
+	.ops = &stm32_pwr_reg_ops,
+	.enable_reg = REG_PWR_CR3,
+	.enable_mask = REG_1_1_EN,
+	.owner = THIS_MODULE,
+};
+
+static const struct regulator_desc stm32_pwr_reg18 = {
+	.name = "REG18",
+	.of_match = of_match_ptr("reg18"),
+	.n_voltages = 1,
+	.type = REGULATOR_VOLTAGE,
+	.min_uV = 1800000,
+	.fixed_uV = 1800000,
+	.ops = &stm32_pwr_reg_ops,
+	.enable_reg = REG_PWR_CR3,
+	.enable_mask = REG_1_8_EN,
+	.owner = THIS_MODULE,
+};
+
+static const struct regulator_desc stm32_pwr_usb33 = {
+	.name = "USB33",
+	.of_match = of_match_ptr("usb33"),
+	.n_voltages = 1,
+	.type = REGULATOR_VOLTAGE,
+	.min_uV = 3300000,
+	.fixed_uV = 3300000,
+	.ops = &stm32_pwr_reg_ops,
+	.enable_reg = REG_PWR_CR3,
+	.enable_mask = USB_3_3_EN,
+	.owner = THIS_MODULE,
+};
+
+static struct of_regulator_match stm32_pwr_reg_matches[] = {
+	{ .name = "reg11", .driver_data = (void *)&stm32_pwr_reg11 },
+	{ .name = "reg18", .driver_data = (void *)&stm32_pwr_reg18 },
+	{ .name = "usb33", .driver_data = (void *)&stm32_pwr_usb33 },
+};
+
+#define STM32PWR_REG_NUM_REGS ARRAY_SIZE(stm32_pwr_reg_matches)
+
+static int stm32_power_regulator_probe(struct platform_device *pdev)
+{
+	struct device_node *np = pdev->dev.of_node;
+	struct regulator_dev *rdev;
+	struct regulator_config config = { };
+	struct regmap *regmap;
+	int i, ret = 0;
+
+	of_regulator_match(&pdev->dev, np, stm32_pwr_reg_matches,
+			   STM32PWR_REG_NUM_REGS);
+
+	regmap = syscon_node_to_regmap(pdev->dev.parent->of_node);
+	if (IS_ERR(regmap))
+		return PTR_ERR(regmap);
+
+	config.regmap = regmap;
+	config.dev = &pdev->dev;
+
+	for (i = 0; i < STM32PWR_REG_NUM_REGS; i++) {
+		struct of_regulator_match *match = &stm32_pwr_reg_matches[i];
+
+		if (!match->init_data ||
+		    !match->of_node)
+			continue;
+
+		config.init_data = match->init_data;
+		config.driver_data = match->driver_data;
+		config.of_node = match->of_node;
+
+		rdev = devm_regulator_register(&pdev->dev,
+					       match->driver_data,
+					       &config);
+		if (IS_ERR(rdev)) {
+			ret = PTR_ERR(rdev);
+			dev_err(&pdev->dev,
+				"Failed to register regulator: %d\n", ret);
+			break;
+		}
+	}
+	return ret;
+}
+
+static const struct of_device_id stm32_pwr_reg_of_match[] = {
+	{ .compatible = "st,stm32mp1,pwr-reg", },
+	{},
+};
+
+static struct platform_driver stm32_pwr_reg_driver = {
+	.probe = stm32_power_regulator_probe,
+	.driver = {
+		.name = "stm32-pwr-regulator",
+		.of_match_table = of_match_ptr(stm32_pwr_reg_of_match),
+	},
+};
+
+static int __init stm32_pwr_regulator_init(void)
+{
+	return platform_driver_register(&stm32_pwr_reg_driver);
+}
+subsys_initcall(stm32_pwr_regulator_init);
