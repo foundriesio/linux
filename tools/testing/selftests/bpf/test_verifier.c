4695ea13cee6d021a7b0441f259efe1bdae0862b
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2018-10-25 14:45:14 +0200

    bpf: fix null pointer deref in bpf_prog_test_run_xdp (bsc#1109837).
    suse-commit: 822b2982b58508b327cb591cae2a2537937bf61d

--- tools/testing/selftests/bpf/test_verifier.c
+++ tools/testing/selftests/bpf/test_verifier.c
@@ -7007,6 +7007,20 @@ static struct bpf_test tests[] = {
 		.result = REJECT,
 	},
 	{
+		"XDP, using ifindex from netdev",
+		.insns = {
+			BPF_MOV64_IMM(BPF_REG_0, 0),
+			BPF_LDX_MEM(BPF_W, BPF_REG_2, BPF_REG_1,
+				    offsetof(struct xdp_md, ingress_ifindex)),
+			BPF_JMP_IMM(BPF_JLT, BPF_REG_2, 1, 1),
+			BPF_MOV64_IMM(BPF_REG_0, 1),
+			BPF_EXIT_INSN(),
+		},
+		.result = ACCEPT,
+		.prog_type = BPF_PROG_TYPE_XDP,
+		.retval = 1,
+	},
+	{
 		"search pruning: all branches should be verified (invalid stack access)",
 		.insns = {
 			BPF_MOV64_REG(BPF_REG_2, BPF_REG_10),
