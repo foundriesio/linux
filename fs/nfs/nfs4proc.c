4a7c41f81429c3d9b797c20b86efb3f9a7ca95c4
NeilBrown <neilb@suse.com>
2018-11-13 16:12:28 +1100

    KABI fix for "NFSv4.1: Fix up replays of interrupted requests" (git-fixes).
    suse-commit: 7ec87ef98d58f7cce6cb514f34f649dc322d2359

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -926,11 +926,12 @@ void nfs4_sequence_attach_slot(struct nfs4_sequence_args *args,
 
 }
 
-int nfs4_setup_sequence(struct nfs_client *client,
+int nfs4_setup_sequence(const struct nfs_client *client,
 			struct nfs4_sequence_args *args,
 			struct nfs4_sequence_res *res,
 			struct rpc_task *task)
 {
+	struct nfs_client *client_non_const = (void*)client;
 	struct nfs4_session *session = nfs4_get_session(client);
 	struct nfs4_slot_table *tbl  = client->cl_slot_tbl;
 	struct nfs4_slot *slot;
@@ -961,7 +962,7 @@ int nfs4_setup_sequence(struct nfs_client *client,
 
 		if (likely(!slot->interrupted))
 			break;
-		nfs4_sequence_process_interrupted(client,
+		nfs4_sequence_process_interrupted(client_non_const,
 				slot, task->tk_msg.rpc_cred);
 	}
 
