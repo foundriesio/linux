c9b4dca49251ac7fec8848a50902fb572107516f
TH Jeong [B150163] <thjeong@telechips.com>
2018-11-14 12:26:49 +0900

    mmc: pwrseq_simple_ext_clk: update for simple external clock pwrseq
    This power sequencer supports only for enabling external clock.
    Please refer to the document in Documentation for more information.
    * "Documentation/devicetree/bindings/mmc/mmc-pwrseq-simple-ext-clk.txt"
    
    Change-Id: I6819a1c4eeedebf60f717c1f40a2280cabb67ea5

---
+++ drivers/mmc/core/pwrseq_simple_ext_clk.c
@@ -0,0 +1,128 @@
+/*
+ * Author: Telechips <linux@telechips.com>
+ *
+ * Based on pwrseq_simple.c
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+ 
+#include <linux/clk.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/gpio/consumer.h>
+#include <linux/delay.h>
+#include <linux/property.h>
+
+#include <linux/mmc/host.h>
+
+#include "pwrseq.h"
+
+struct mmc_pwrseq_simple_ext_clk {
+	struct mmc_pwrseq pwrseq;
+	bool clk_enabled;
+	u32 ext_clk_speed;
+	struct clk *ext_clk;
+};
+
+#define to_pwrseq_simple(p) container_of(p, struct mmc_pwrseq_simple_ext_clk, pwrseq)
+
+static void mmc_pwrseq_simple_ext_clk_pre_power_on(struct mmc_host *host)
+{
+	struct mmc_pwrseq_simple_ext_clk *pwrseq = to_pwrseq_simple(host->pwrseq);
+
+	if (!IS_ERR(pwrseq->ext_clk) && !pwrseq->clk_enabled) {
+		clk_prepare_enable(pwrseq->ext_clk);
+		pwrseq->clk_enabled = true;
+	}
+
+	if (pwrseq->ext_clk_speed != 0)
+		clk_set_rate(pwrseq->ext_clk, pwrseq->ext_clk_speed);
+}
+static void mmc_pwrseq_simple_ext_clk_power_off(struct mmc_host *host)
+{
+	struct mmc_pwrseq_simple_ext_clk *pwrseq = to_pwrseq_simple(host->pwrseq);
+
+	if (!IS_ERR(pwrseq->ext_clk) && pwrseq->clk_enabled) {
+		clk_disable_unprepare(pwrseq->ext_clk);
+		pwrseq->clk_enabled = false;
+	}
+}
+
+static const struct mmc_pwrseq_ops mmc_pwrseq_simple_ext_clk_ops = {
+	.pre_power_on = mmc_pwrseq_simple_ext_clk_pre_power_on,
+	.power_off = mmc_pwrseq_simple_ext_clk_power_off,
+};
+
+static const struct of_device_id mmc_pwrseq_simple_ext_clk_of_match[] = {
+	{ .compatible = "mmc-pwrseq-simple-ext-clock",},
+	{/* sentinel */},
+};
+MODULE_DEVICE_TABLE(of, mmc_pwrseq_simple_ext_clk_of_match);
+
+static int mmc_pwrseq_simple_ext_clk_probe(struct platform_device *pdev)
+{
+	struct mmc_pwrseq_simple_ext_clk *pwrseq;
+	struct device *dev = &pdev->dev;
+
+	pwrseq = devm_kzalloc(dev, sizeof(*pwrseq), GFP_KERNEL);
+	if (!pwrseq)
+		return -ENOMEM;
+
+	pwrseq->ext_clk = devm_clk_get(dev, "ext_clock");
+	if (IS_ERR(pwrseq->ext_clk) && PTR_ERR(pwrseq->ext_clk) != -ENOENT) {
+		return PTR_ERR(pwrseq->ext_clk);
+	}
+
+	pwrseq->ext_clk_speed = 0;
+	device_property_read_u32(dev, "ext_clock_speed",
+				 &pwrseq->ext_clk_speed);
+
+	if(pwrseq->ext_clk_speed == 0) {
+		dev_err(&pdev->dev, "err! clock speed is zero\n");
+		return -EINVAL;
+	}
+
+	pwrseq->pwrseq.dev = dev;
+	pwrseq->pwrseq.ops = &mmc_pwrseq_simple_ext_clk_ops;
+	pwrseq->pwrseq.owner = THIS_MODULE;
+	platform_set_drvdata(pdev, pwrseq);
+
+	return mmc_pwrseq_register(&pwrseq->pwrseq);
+}
+
+static int mmc_pwrseq_simple_ext_clk_remove(struct platform_device *pdev)
+{
+	struct mmc_pwrseq_simple_ext_clk *pwrseq = platform_get_drvdata(pdev);
+
+	mmc_pwrseq_unregister(&pwrseq->pwrseq);
+
+	return 0;
+}
+
+static struct platform_driver mmc_pwrseq_simple_ext_clk_driver = {
+	.probe = mmc_pwrseq_simple_ext_clk_probe,
+	.remove = mmc_pwrseq_simple_ext_clk_remove,
+	.driver = {
+		.name = "pwrseq_simple_ext_clk",
+		.of_match_table = mmc_pwrseq_simple_ext_clk_of_match,
+	},
+};
+
+module_platform_driver(mmc_pwrseq_simple_ext_clk_driver);
+MODULE_LICENSE("GPL v2");
