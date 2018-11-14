ee3a9167c172406bdd8cd0c6fd05e017d013a685
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2018-11-14 17:34:29 +0100

    - net: introduce helper dev_change_tx_queue_len() (bsc#1109837). - Refresh   patches.drivers/net_sched-plug-in-qdisc-ops-change_tx_queue_len.patch.
    suse-commit: 0c0d2aca8a6646eee118fcc689b5ba71057b89ef

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -3346,6 +3346,7 @@ int dev_set_alias(struct net_device *, const char *, size_t);
 int dev_change_net_namespace(struct net_device *, struct net *, const char *);
 int __dev_set_mtu(struct net_device *, int);
 int dev_set_mtu(struct net_device *, int);
+int dev_change_tx_queue_len(struct net_device *, unsigned long);
 void dev_set_group(struct net_device *, int);
 int dev_set_mac_address(struct net_device *, struct sockaddr *);
 int dev_change_carrier(struct net_device *, bool new_carrier);
