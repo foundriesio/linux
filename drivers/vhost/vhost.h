dc6bcf991e8d1374b7b83dd16d1e2799f04b7f3c
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2018-12-01 14:26:19 +0100

    vhost: switch to use new message format (bsc#1109837).
    suse-commit: fc003a8aa98c66fda61ced2c7e4893e08f11ccc8

--- drivers/vhost/vhost.h
+++ drivers/vhost/vhost.h
@@ -140,6 +140,7 @@ struct vhost_virtqueue {
 	struct vhost_umem *iotlb;
 	void *private_data;
 	u64 acked_features;
+	u64 acked_backend_features;
 	/* Log write descriptors */
 	void __user *log_base;
 	struct vhost_log *log;
@@ -155,7 +156,10 @@ struct vhost_virtqueue {
 };
 
 struct vhost_msg_node {
-  struct vhost_msg msg;
+  union {
+	  struct vhost_msg msg;
+	  struct vhost_msg_v2 msg_v2;
+  };
   struct vhost_virtqueue *vq;
   struct list_head node;
 };
@@ -245,6 +249,11 @@ enum {
 static inline bool vhost_has_feature(struct vhost_virtqueue *vq, int bit)
 {
 	return vq->acked_features & (1ULL << bit);
+}
+
+static inline bool vhost_backend_has_feature(struct vhost_virtqueue *vq, int bit)
+{
+	return vq->acked_backend_features & (1ULL << bit);
 }
 
 #ifdef CONFIG_VHOST_CROSS_ENDIAN_LEGACY
