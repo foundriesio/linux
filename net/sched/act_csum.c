b969f58709897002c3599fafdb79701bdfacad17
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2018-12-03 17:53:46 +0100

    - net: sched: change type of reference and bind counters   (bsc#1109837). - Refresh   patches.suse/net-sched-act_pedit-fix-dump-of-extended-layered-op.patch.
    suse-commit: bdda93067260d14fc3114c710460d3e870d245c8

--- net/sched/act_csum.c
+++ net/sched/act_csum.c
@@ -597,9 +597,8 @@ static int tcf_csum_dump(struct sk_buff *skb, struct tc_action *a, int bind,
 	struct tcf_csum_params *params;
 	struct tc_csum opt = {
 		.index   = p->tcf_index,
-		.refcnt  = p->tcf_refcnt - ref,
-		.bindcnt = p->tcf_bindcnt - bind,
-		.action  = p->tcf_action,
+		.refcnt  = refcount_read(&p->tcf_refcnt) - ref,
+		.bindcnt = atomic_read(&p->tcf_bindcnt) - bind,
 	};
 	struct tcf_t t;
 
