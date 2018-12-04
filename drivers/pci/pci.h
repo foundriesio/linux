422584ba0f3b4cbe20a9f3ae61168962f7b198ef
Takashi Iwai <tiwai@suse.de>
2018-12-04 16:23:28 +0100

    PCI/PM: Move pcie_clear_root_pme_status() to core (FATE#326145).
    suse-commit: 1e35dbf90c06f2b6f7ebaf3570ea85251c4859a1

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -73,6 +73,7 @@ void pci_update_current_state(struct pci_dev *dev, pci_power_t state);
 void pci_power_up(struct pci_dev *dev);
 void pci_disable_enabled_device(struct pci_dev *dev);
 int pci_finish_runtime_suspend(struct pci_dev *dev);
+void pcie_clear_root_pme_status(struct pci_dev *dev);
 int __pci_pme_wakeup(struct pci_dev *dev, void *ign);
 bool pci_dev_keep_suspended(struct pci_dev *dev);
 void pci_dev_complete_resume(struct pci_dev *pci_dev);
