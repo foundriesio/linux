f095b2ba41ac0424bc8739b1d352bee28b9780b5
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2018-12-04 18:08:27 +0100

    net_sched: remove unused parameter for tcf_action_delete() (bsc#1109837).
    suse-commit: e668caceec3325f31ac701d11c8d28130ef66a54

--- net/sched/act_api.c
+++ net/sched/act_api.c
@@ -1178,8 +1178,7 @@ err_out:
 	return err;
 }
 
-static int tcf_action_delete(struct net *net, struct tc_action *actions[],
-			     struct netlink_ext_ack *extack)
+static int tcf_action_delete(struct net *net, struct tc_action *actions[])
 {
 	int i;
 
@@ -1228,7 +1227,7 @@ tcf_del_notify(struct net *net, struct nlmsghdr *n, struct tc_action *actions[],
 	}
 
 	/* now do the delete */
-	ret = tcf_action_delete(net, actions, extack);
+	ret = tcf_action_delete(net, actions);
 	if (ret < 0) {
 		NL_SET_ERR_MSG(extack, "Failed to delete TC action");
 		kfree_skb(skb);
