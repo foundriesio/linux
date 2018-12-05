54f107c3c1b5f31ea3fcd6683be53c5301a6e2d7
Takashi Iwai <tiwai@suse.de>
2018-12-05 11:56:07 +0100

    Correct wrongly applied pciehp patches
    Refresh
      patches.drivers/0001-PCI-pciehp-Deduplicate-presence-check-on-probe-resum.patch
      patches.drivers/0001-PCI-pciehp-Disable-hotplug-interrupt-during-suspend.patch
      patches.drivers/0001-PCI-pciehp-Unify-controller-and-slot-structs.patch
    
    suse-commit: cdd3802f99fd5ac985c2fe96600f9dde6f67d757

--- drivers/pci/hotplug/pciehp_core.c
+++ drivers/pci/hotplug/pciehp_core.c
@@ -301,31 +301,21 @@ static int pciehp_resume_noirq(struct pcie_device *dev)
 {
 	struct controller *ctrl = get_service_data(dev);
 
-	pciehp_check_presence(ctrl);
+	/* clear spurious events from rediscovery of inserted card */
+	if (ctrl->state == ON_STATE || ctrl->state == BLINKINGOFF_STATE)
+              pcie_clear_hotplug_events(ctrl);
 
 	return 0;
 }
 
 static int pciehp_resume(struct pcie_device *dev)
 {
-	struct controller *ctrl;
-	u8 status;
+	struct controller *ctrl = get_service_data(dev);
 
 	if (pme_is_native(dev))
 		pcie_enable_interrupt(ctrl);
 
-	ctrl = get_service_data(dev);
-
-	/* Check if slot is occupied */
-	pciehp_get_adapter_status(ctrl, &status);
-
-	mutex_lock(&ctrl->lock);
-	if ((status && (ctrl->state == OFF_STATE ||
-			ctrl->state == BLINKINGON_STATE)) ||
-	     (!status && (ctrl->state == ON_STATE ||
-			ctrl->state == BLINKINGOFF_STATE)))
-		pciehp_request(ctrl, PCI_EXP_SLTSTA_PDC);
-	mutex_unlock(&ctrl->lock);
+	pciehp_check_presence(ctrl);
 
 	return 0;
 }
