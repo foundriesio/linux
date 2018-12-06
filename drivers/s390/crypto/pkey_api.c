c8b30e79f2b13264b8e5a3a767976afbe57cf272
Petr Tesarik <ptesarik@suse.cz>
2018-12-06 10:49:58 +0100

    s390/pkey: Load pkey kernel module automatically (FATE#326366, LTC#169192, bsc#1113523).
    suse-commit: a0c11b15db2d11cce8fdad07a8e0ee932323081e

--- drivers/s390/crypto/pkey_api.c
+++ drivers/s390/crypto/pkey_api.c
@@ -21,6 +21,7 @@
 #include <linux/kallsyms.h>
 #include <linux/debugfs.h>
 #include <linux/random.h>
+#include <linux/cpufeature.h>
 #include <asm/zcrypt.h>
 #include <asm/cpacf.h>
 #include <asm/pkey.h>
@@ -1709,5 +1710,5 @@ static void __exit pkey_exit(void)
 	pkey_debug_exit();
 }
 
-module_init(pkey_init);
+module_cpu_feature_match(MSA, pkey_init);
 module_exit(pkey_exit);
