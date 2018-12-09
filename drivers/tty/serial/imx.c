af47ed57d3cf5eeaf3d208464ca941567003ae62
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2018-12-10 00:00:37 +0100

    tty: serial: imx: fix merge inconsistency
    Missing commit 688f897ddd96 ("Merge remote-tracking branch
    'linux-fslc/4.9-1.0.x-imx' into toradex_4.9-1.0.x-imx-next").
    
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
    Acked-by: Max Krummenacher <max.krummenacher@toradex.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -1305,13 +1305,6 @@ static int imx_startup(struct uart_port *port)
 
 	writel(temp & ~UCR4_DREN, sport->port.membase + UCR4);
 
-	/* Disable DCDDELT/RIDELT interrupts */
-	if (!is_imx1_uart(sport) && sport->dte_mode) {
-		temp = readl(sport->port.membase + UCR3);
-		temp &= ~(UCR3_DCD | UCR3_RI);
-		writel(temp, sport->port.membase + UCR3);
-	}
-
 	/* Reset fifo's and state machines */
 	i = 100;
 
@@ -2278,6 +2271,18 @@ static int serial_imx_probe(struct platform_device *pdev)
 	}
 
 	clk_disable_unprepare(sport->clk_ipg);
+
+	/* if DTE mode is requested, make sure DTE mode is selected
+	   and then disable DCDDELT/RIDELT interrupts */
+	if (!is_imx1_uart(sport) && sport->dte_mode) {
+		unsigned long temp;
+		temp = readl(sport->port.membase + UFCR);
+		temp |= UFCR_DCEDTE;
+		writel(temp, sport->port.membase + UFCR);
+		temp = readl(sport->port.membase + UCR3);
+		temp &= ~(UCR3_DCD | UCR3_RI);
+		writel(temp, sport->port.membase + UCR3);
+	}
 
 	/*
 	 * Allocate the IRQ(s) i.MX1 has three interrupts whereas later
