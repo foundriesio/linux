fd3114c30a1b8f1e1ae2242d3189c788fd02ccef
Michal Hocko <mhocko@suse.com>
2018-12-12 16:28:14 +0100

    mm, memory_hotplug: be more verbose for memory offline failures (generic hotplug debugability).
    suse-commit: 501eed80867434e0af95a94f97108a55c8bd8306

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7449,7 +7449,7 @@ bool has_unmovable_pages(struct zone *zone, struct page *page, int count,
 		if (PageHuge(page)) {
 
 			if (!hugepage_migration_supported(page_hstate(page)))
-				return true;
+				goto unmovable;
 
 			iter = round_up(iter + 1, 1<<compound_order(page)) - 1;
 			continue;
@@ -7493,9 +7493,12 @@ bool has_unmovable_pages(struct zone *zone, struct page *page, int count,
 		 * page at boot.
 		 */
 		if (found > count)
-			return true;
+			goto unmovable;
 	}
 	return false;
+unmovable:
+	dump_page(pfn_to_page(pfn+iter), "unmovable page");
+	return true;
 }
 
 bool is_pageblock_removable_nolock(struct page *page)
