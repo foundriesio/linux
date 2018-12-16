05cb1d44504d537a4bba75b29f9b115b6d0cffa0
Borislav Petkov <bp@suse.de>
2018-12-16 02:56:34 +0100

    Refresh patches.arch/locking-atomics-asm-generic-move-some-macros-from-linux-bitops-h-to-a-new-linux-bits-h-file.patch.
    suse-commit: 35b3a9fc2e5ef5015f3c8dd24ed18cfc5a49c933

--- include/linux/bits.h
+++ include/linux/bits.h
@@ -17,7 +17,7 @@
  * GENMASK_ULL(39, 21) gives us the 64bit vector 0x000000ffffe00000.
  */
 #define GENMASK(h, l) \
-	(((~0UL) - (1UL << (l)) + 1) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
+	(((~0UL) << (l)) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
 
 #define GENMASK_ULL(h, l) \
 	(((~0ULL) - (1ULL << (l)) + 1) & \
