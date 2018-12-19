9d3ebb37b25f5bd32ca61c510a63f91c5dbf881f
Johannes Thumshirn <jthumshirn@suse.de>
2018-12-19 10:13:36 +0100

    - dax: Introduce a ->copy_to_iter dax operation (FATE#326060). - Delete   patches.fixes/dax-Introduce-a-copy_to_iter-dax-operation.patch.
    suse-commit: 64d74642d68e26fbbb17c6b7111d9ad8b0063ca2

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1101,7 +1101,25 @@ static size_t dm_dax_copy_from_iter(struct dax_device *dax_dev, pgoff_t pgoff,
 static size_t dm_dax_copy_to_iter(struct dax_device *dax_dev, pgoff_t pgoff,
 		void *addr, size_t bytes, struct iov_iter *i)
 {
-	return copy_to_iter(addr, bytes, i);
+	struct mapped_device *md = dax_get_private(dax_dev);
+	sector_t sector = pgoff * PAGE_SECTORS;
+	struct dm_target *ti;
+	long ret = 0;
+	int srcu_idx;
+
+	ti = dm_dax_get_live_target(md, sector, &srcu_idx);
+
+	if (!ti)
+		goto out;
+	if (!ti->type->dax_copy_to_iter) {
+		ret = copy_to_iter(addr, bytes, i);
+		goto out;
+	}
+	ret = ti->type->dax_copy_to_iter(ti, pgoff, addr, bytes, i);
+ out:
+	dm_put_live_table(md, srcu_idx);
+
+	return ret;
 }
 
 /*
