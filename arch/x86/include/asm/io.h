41633fbca6a6b733b4e4e31dfbb78c61505d2026
Hannes Reinecke <hare@suse.de>
2018-12-21 12:06:29 +0100

    xen: don't include <xen/xen.h> from <asm/io.h> and <asm/dma-mapping.h> (bsc#1104967,FATE#325924).
    suse-commit: 61390e0128c376905e9014ec15511b18d9d63d40

--- arch/x86/include/asm/io.h
+++ arch/x86/include/asm/io.h
@@ -386,10 +386,6 @@ extern void __iomem *ioremap_wt(resource_size_t offset, unsigned long size);
 
 extern bool is_early_ioremap_ptep(pte_t *ptep);
 
-#ifdef CONFIG_XEN
-#include <xen/xen.h>
-#endif	/* CONFIG_XEN */
-
 #define IO_SPACE_LIMIT 0xffff
 
 #ifdef CONFIG_MTRR
