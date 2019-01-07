f9f4a51c29d64bfa5d84f3d3d447e7554eec488b
Petr Tesarik <ptesarik@suse.cz>
2019-01-07 10:13:36 +0100

    drm/nouveau/gr/gf100-gf119: modify max_ways_evict where required (FATE#326289 FATE#326079 FATE#326049 FATE#322398 FATE#326166).
    suse-commit: d97c0cf0a3e912951e4814abfee522ac6dcf7fc0

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/ctxgf100.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/ctxgf100.h
@@ -55,6 +55,7 @@ struct gf100_grctx_func {
 	void (*r4060a8)(struct gf100_gr *);
 	void (*rop_mapping)(struct gf100_gr *);
 	void (*alpha_beta_tables)(struct gf100_gr *);
+	void (*max_ways_evict)(struct gf100_gr *);
 };
 
 extern const struct gf100_grctx_func gf100_grctx;
@@ -70,6 +71,7 @@ void gf100_grctx_generate_tpc_nr(struct gf100_gr *, int);
 void gf100_grctx_generate_r4060a8(struct gf100_gr *);
 void gf100_grctx_generate_rop_mapping(struct gf100_gr *);
 void gf100_grctx_generate_alpha_beta_tables(struct gf100_gr *);
+void gf100_grctx_generate_max_ways_evict(struct gf100_gr *);
 
 extern const struct gf100_grctx_func gf108_grctx;
 void gf108_grctx_generate_attrib(struct gf100_grctx *);
