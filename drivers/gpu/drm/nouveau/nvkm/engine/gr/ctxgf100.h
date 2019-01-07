33732b52f89d196dffe9a4ebc6f2cd947f858223
Petr Tesarik <ptesarik@suse.cz>
2019-01-07 10:13:36 +0100

    drm/nouveau/gr/gf100-: update r408840 where required (FATE#326289 FATE#326079 FATE#326049 FATE#322398 FATE#326166).
    suse-commit: b32765c12e38a3e4cb238e6f5a1e03d7eb0125ba

--- drivers/gpu/drm/nouveau/nvkm/engine/gr/ctxgf100.h
+++ drivers/gpu/drm/nouveau/nvkm/engine/gr/ctxgf100.h
@@ -69,6 +69,7 @@ struct gf100_grctx_func {
 	void (*r419e00)(struct gf100_gr *);
 	void (*r418e94)(struct gf100_gr *);
 	void (*r419a3c)(struct gf100_gr *);
+	void (*r408840)(struct gf100_gr *);
 };
 
 extern const struct gf100_grctx_func gf100_grctx;
@@ -141,6 +142,8 @@ void gp100_grctx_generate_smid_config(struct gf100_gr *);
 
 extern const struct gf100_grctx_func gp102_grctx;
 void gp102_grctx_generate_attrib(struct gf100_grctx *);
+
+extern const struct gf100_grctx_func gp104_grctx;
 
 extern const struct gf100_grctx_func gp107_grctx;
 
