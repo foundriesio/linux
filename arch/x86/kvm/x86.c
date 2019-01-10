e1543bafd08e9aacc7a1abdedbf378f5dc3fb359
Olaf Hering <ohering@suse.de>
2019-01-10 16:37:31 +0000

    KVM: x86: hyperv: implement PV IPI send hypercalls (fate#323887).
    suse-commit: 22a3fb1ed844f345b5dea5393f51e4445d11fc29

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -2780,6 +2780,7 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_HYPERV_SYNIC2:
 	case KVM_CAP_HYPERV_VP_INDEX:
 	case KVM_CAP_HYPERV_EVENTFD:
+	case KVM_CAP_HYPERV_SEND_IPI:
 	case KVM_CAP_PCI_SEGMENT:
 	case KVM_CAP_DEBUGREGS:
 	case KVM_CAP_X86_ROBUST_SINGLESTEP:
