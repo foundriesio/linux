5692f66732a81d13c62552977ff8d6d9ea0f9a8d
JJ PARK(B120171) <jingoust@telechips.com>
2019-01-14 11:00:52 +0900

    Description : TCC897x LVDS setting function add 	lcd_fld0800 panel add power on/off function.
    Change-Id: I47a18b0da2927006fced35398c81de159afa8d1d

--- include/video/tcc/tcc897x/vioc_lvds.h
+++ include/video/tcc/tcc897x/vioc_lvds.h
@@ -1,5 +1,5 @@
 /*
- * linux/include/video/tcc/vioc_lvds.h
+ * linux/include/video/tcc/tcc897x/vioc_lvds.h
  * Author:  <linux@telechips.com>
  * Created: June 10, 2008
  * Description: TCC VIOC h/w block 
@@ -22,9 +22,24 @@
  * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifdef CONFIG_ARCH_TCC803X
-#include "tcc803x/vioc_lvds.h"
-#elif defined(CONFIG_ARCH_TCC897X)
-#include "tcc897x/vioc_lvds.h"
-#endif
+#ifndef __VIOC_LVDS_H__
+#define	__VIOC_LVDS_H__
 
+
+extern void tcc_set_ddi_lvds_reset(unsigned int reset);
+extern void tcc_set_ddi_lvds_pms(unsigned int lcdc_n, unsigned int pclk, unsigned int enable);
+
+#define LVDS_DUMMY			(0x1F)
+#define LVDS_DE				(24)
+#define LVDS_HS				(25)
+#define LVDS_VS				(26)
+#define LVDS_R_D(x)			(x + 0x10)
+#define LVDS_G_D(x)			(x + 0x8)
+#define LVDS_B_D(x)			(x )
+
+#define LVDS_MAX_LINE			4
+#define LVDS_DATA_PER_LINE		7
+extern void tcc_set_ddi_lvds_data_arrary(unsigned int data[LVDS_MAX_LINE][LVDS_DATA_PER_LINE]);
+extern void tcc_set_ddi_lvds_config(void);
+
+#endif
