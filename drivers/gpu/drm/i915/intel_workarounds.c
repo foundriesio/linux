6b31a3e5999435843150737663bd71a006631a45
Thomas Zimmermann <tzimmermann@suse.de>
2019-01-22 15:14:35 +0100

    drm/i915/icl: Add Wa_1406609255 (fate#326289)
    suse-commit: 000b9706cc1ea7d107961ffa688703a23ca8349e

--- drivers/gpu/drm/i915/intel_workarounds.c
+++ drivers/gpu/drm/i915/intel_workarounds.c
@@ -843,6 +843,7 @@ static void cnl_gt_workarounds_init(struct drm_i915_private *i915)
 static void icl_gt_workarounds_init(struct drm_i915_private *i915)
 {
 	struct i915_wa_list *wal = &i915->gt_wa_list;
+	struct drm_i915_private *dev_priv = i915; /* for I915_WRITE */
 
 	wa_init_mcr(i915);
 
@@ -895,6 +896,12 @@ static void icl_gt_workarounds_init(struct drm_i915_private *i915)
 	wa_write_or(wal,
 		    GAMT_CHKN_BIT_REG,
 		    GAMT_CHKN_DISABLE_L3_COH_PIPE);
+
+	/* Wa_1406609255:icl (pre-prod) */
+	if (IS_ICL_REVID(i915, ICL_REVID_A0, ICL_REVID_B0))
+		I915_WRITE(GEN7_SARCHKMD,
+			   I915_READ(GEN7_SARCHKMD) |
+			   GEN7_DISABLE_DEMAND_PREFETCH);
 }
 
 void intel_gt_init_workarounds(struct drm_i915_private *i915)
