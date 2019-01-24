ba5124c324e196c6478b8f93f9c26a6f9ee569d4
Petr Tesarik <ptesarik@suse.cz>
2019-01-24 13:06:05 +0100

    Add product identifying information to VMCOREINFO (bsc#1123015).
    suse-commit: b361141dbaa8c96b2135f974ecfcecbe886d8442

--- kernel/crash_core.c
+++ kernel/crash_core.c
@@ -9,6 +9,7 @@
 #include <linux/crash_core.h>
 #include <linux/utsname.h>
 #include <linux/vmalloc.h>
+#include <linux/suse_version.h>
 
 #include <asm/page.h>
 #include <asm/sections.h>
@@ -395,6 +396,7 @@ static int __init crash_save_vmcoreinfo_init(void)
 	}
 
 	VMCOREINFO_OSRELEASE(init_uts_ns.name.release);
+	VMCOREINFO_NUMBER(SUSE_PRODUCT_CODE);
 	VMCOREINFO_PAGESIZE(PAGE_SIZE);
 
 	VMCOREINFO_SYMBOL(init_uts_ns);
