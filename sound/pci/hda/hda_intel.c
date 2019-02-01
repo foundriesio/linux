71f433f84335602d12eac715a65385abbd1bb355
Takashi Iwai <tiwai@suse.de>
2019-02-01 15:14:53 +0100

    ALSA: hda - Serialize codec registrations (bsc#1122944).
    suse-commit: 83088133d0d179640b0b72ee1daa9093097c6a5d

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -2248,6 +2248,7 @@ static int azx_probe_continue(struct azx *chip)
 	int val;
 	int err;
 
+	to_hda_bus(bus)->bus_probing = 1;
 	hda->probe_continued = 1;
 
 	/* bind with i915 if needed */
@@ -2353,6 +2354,7 @@ i915_power_fail:
 	if (err < 0)
 		hda->init_failed = 1;
 	complete_all(&hda->probe_wait);
+	to_hda_bus(bus)->bus_probing = 0;
 	return err;
 }
 
