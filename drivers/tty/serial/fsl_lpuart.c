427a8cdcb1bb59b705125af0ffe3385422ac833d
Takashi Iwai <tiwai@suse.de>
2019-02-13 12:42:29 +0100

    serial: fsl_lpuart: clear parity enable bit when disable parity (bsc#1051510).
    suse-commit: ae34696831fde4f79ebb0aac40046eaa3ef3696c

--- drivers/tty/serial/fsl_lpuart.c
+++ drivers/tty/serial/fsl_lpuart.c
@@ -1403,6 +1403,8 @@ lpuart_set_termios(struct uart_port *port, struct ktermios *termios,
 			else
 				cr1 &= ~UARTCR1_PT;
 		}
+	} else {
+		cr1 &= ~UARTCR1_PE;
 	}
 
 	/* ask the core to calculate the divisor */
@@ -1547,6 +1549,8 @@ lpuart32_set_termios(struct uart_port *port, struct ktermios *termios,
 			else
 				ctrl &= ~UARTCTRL_PT;
 		}
+	} else {
+		ctrl &= ~UARTCTRL_PE;
 	}
 
 	/* ask the core to calculate the divisor */
