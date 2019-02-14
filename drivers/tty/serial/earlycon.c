f073cd85541c1c1f6af0ea52f1d5920936bc45c5
Takashi Iwai <tiwai@suse.de>
2019-02-14 11:33:46 +0100

    earlycon: Initialize port->uartclk based on clock-frequency property (bsc#1051510).
    suse-commit: d29416c07aa2b5fdabb8d5e0ce23480a80920a96

--- drivers/tty/serial/earlycon.c
+++ drivers/tty/serial/earlycon.c
@@ -285,6 +285,10 @@ int __init of_setup_earlycon(const struct earlycon_id *match,
 		}
 	}
 
+	val = of_get_flat_dt_prop(node, "clock-frequency", NULL);
+	if (val)
+		port->uartclk = be32_to_cpu(*val);
+
 	if (options) {
 		strlcpy(early_console_dev.options, options,
 			sizeof(early_console_dev.options));
