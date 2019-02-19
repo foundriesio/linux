f5fdd2c6e30fc526cb1405bd40bfb24f39746fa5
Paul Walmsley <paul.walmsley@sifive.com>
2019-02-18 16:20:29 -0800

    [FIO extras] clk: sifive: add a driver for the SiFive FU540 PRCI IP block
    Add driver code for the SiFive FU540 PRCI IP block.  This IP block
    handles reset and clock control for the SiFive FU540 device and
    implements SoC-level clock tree controls and dividers.
    
    Based on code written by Wesley Terpstra <wesley@sifive.com>:
    https://github.com/riscv/riscv-linux/commit/999529edf517ed75b56659d456d221b2ee56bb60
    
    Boot and PLL rate change were tested on a SiFive HiFive Unleashed
    board.
    
    This third version updates the compatible string to drop the IP block
    version number, per discussion with Rob Herring <robh@kernel.org>.
    
    Cc: Michael Turquette <mturquette@baylibre.com>
    Cc: Stephen Boyd <sboyd@kernel.org>
    Cc: Albert Ou <aou@eecs.berkeley.edu>
    Cc: Wesley W. Terpstra <wesley@sifive.com>
    Cc: Palmer Dabbelt <palmer@sifive.com>
    Cc: Megan Wachs <megan@sifive.com>
    Cc: Rob Herring <robh@kernel.org>
    Cc: linux-riscv@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-clk@vger.kernel.org
    Signed-off-by: Paul Walmsley <paul.walmsley@sifive.com>
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Michael Scott <mike@foundries.io>

---
+++ include/dt-bindings/clock/sifive-fu540-prci.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2018 SiFive, Inc.
+ * Wesley Terpstra
+ * Paul Walmsley
+ */
+
+#ifndef __LINUX_CLK_SIFIVE_FU540_PRCI_H
+#define __LINUX_CLK_SIFIVE_FU540_PRCI_H
+
+/* Clock indexes for use by Device Tree data */
+
+#define PRCI_CLK_COREPLL		0
+#define PRCI_CLK_DDRPLL			1
+#define PRCI_CLK_GEMGXLPLL		2
+#define PRCI_CLK_TLCLK			3
+
+#endif
