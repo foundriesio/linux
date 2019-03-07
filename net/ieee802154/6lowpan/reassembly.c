06aeeb614ef37831df8390a6391c688ac145a6ca
Michal Kubecek <mkubecek@suse.cz>
2019-03-07 20:00:30 +0100

    net: ieee802154: 6lowpan: remove redundant pointers 'fq' and 'net' (CVE-2018-5391 bsc#1103097).
    suse-commit: 432fd33daef8c9f66832de5dd003981f4d23156b

--- net/ieee802154/6lowpan/reassembly.c
+++ net/ieee802154/6lowpan/reassembly.c
@@ -40,9 +40,6 @@ static int lowpan_frag_reasm(struct lowpan_frag_queue *fq,
 static void lowpan_frag_init(struct inet_frag_queue *q, const void *a)
 {
 	const struct frag_lowpan_compare_key *key = a;
-	struct lowpan_frag_queue *fq;
-
-	fq = container_of(q, struct lowpan_frag_queue, q);
 
 	BUILD_BUG_ON(sizeof(*key) > sizeof(q->key));
 	memcpy(&q->key, key, sizeof(*key));
@@ -51,10 +48,8 @@ static void lowpan_frag_init(struct inet_frag_queue *q, const void *a)
 static void lowpan_frag_expire(unsigned long data)
 {
 	struct frag_queue *fq;
-	struct net *net;
 
 	fq = container_of((struct inet_frag_queue *)data, struct frag_queue, q);
-	net = container_of(fq->q.net, struct net, ieee802154_lowpan.frags);
 
 	spin_lock(&fq->q.lock);
 
