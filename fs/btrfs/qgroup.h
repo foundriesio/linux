a4256c79d41d1be723bc15bc5612a53c954a7b2b
Qu Wenruo <wqu@suse.com>
2019-03-15 13:08:00 +0800

    - btrfs: qgroup: Use delayed subtree rescan for balance   (bsc#1063638). - Refresh   patches.suse/btrfs-suspend-qgroups-during-relocation-recovery.patch.
    suse-commit: e6256d67c98ff2783e07c6355637e94923bc5431

--- fs/btrfs/qgroup.h
+++ fs/btrfs/qgroup.h
@@ -412,6 +412,8 @@ int btrfs_qgroup_add_swapped_blocks(struct btrfs_trans_handle *trans,
 		struct extent_buffer *subvol_parent, int subvol_slot,
 		struct extent_buffer *reloc_parent, int reloc_slot,
 		u64 last_snapshot);
+int btrfs_qgroup_trace_subtree_after_cow(struct btrfs_trans_handle *trans,
+		struct btrfs_root *root, struct extent_buffer *eb);
 /*
  * These are only used as a workaround for relocation recovery since it
  * degrades into accounting the entire tree at once.
