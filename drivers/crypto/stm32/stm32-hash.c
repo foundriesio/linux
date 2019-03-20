dce9c0458f44367637d5333df8356063e7b0e3ca
Lionel Debieve <lionel.debieve@st.com>
2019-03-20 20:38:18 +0100

    crypto: stm32/hash - Fix self test issue during export
    Change the wait condition to check if the hash is busy.
    Context can be saved as soon as hash has finishing processing
    data.
    
    Change-Id: Id9b80fcc80905f4eec9ceeae034467e9f316363c
    Signed-off-by: Lionel Debieve <lionel.debieve@st.com>
    Reviewed-on: https://gerrit.st.com/128873
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Fabien DESSENNE <fabien.dessenne@st.com>

--- drivers/crypto/stm32/stm32-hash.c
+++ drivers/crypto/stm32/stm32-hash.c
@@ -977,7 +977,7 @@ static int stm32_hash_export(struct ahash_request *req, void *out)
 
 	pm_runtime_get_sync(hdev->dev);
 
-	while (!(stm32_hash_read(hdev, HASH_SR) & HASH_SR_DATA_INPUT_READY))
+	while ((stm32_hash_read(hdev, HASH_SR) & HASH_SR_BUSY))
 		cpu_relax();
 
 	rctx->hw_context = kmalloc_array(3 + HASH_CSR_REGISTER_NUMBER,
