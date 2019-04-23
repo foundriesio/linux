d7eab47c4f92d9e75de1d01139f9ff2bdc884913
Michal Suchanek <msuchanek@suse.de>
2019-04-23 09:08:26 +0200

    intel_idle: add support for Jacobsville (jsc#SLE-5394).
    suse-commit: 653c52e701889f781287e3113caa0d77d931327b

--- drivers/idle/intel_idle.c
+++ drivers/idle/intel_idle.c
@@ -1099,6 +1099,7 @@ static const struct x86_cpu_id intel_idle_ids[] __initconst = {
 	ICPU(INTEL_FAM6_ATOM_GOLDMONT,		idle_cpu_bxt),
 	ICPU(INTEL_FAM6_ATOM_GEMINI_LAKE,	idle_cpu_bxt),
 	ICPU(INTEL_FAM6_ATOM_DENVERTON,		idle_cpu_dnv),
+	ICPU(INTEL_FAM6_ATOM_TREMONT_X,		idle_cpu_dnv),
 	{}
 };
 
