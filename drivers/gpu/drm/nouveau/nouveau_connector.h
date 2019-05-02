6356c1d5c5fcf09301a0e57e641962bd88285996
Takashi Iwai <tiwai@suse.de>
2019-05-02 14:16:13 +0200

    drm/nouveau/drm/nouveau: Don't forget to label dp_aux devices (bsc#1133593).
    suse-commit: 6d61a099b25e813dc6eedea922810c599bf12af5

--- drivers/gpu/drm/nouveau/nouveau_connector.h
+++ drivers/gpu/drm/nouveau/nouveau_connector.h
@@ -36,6 +36,7 @@
 #include "nouveau_encoder.h"
 
 struct nvkm_i2c_port;
+struct dcb_output;
 
 #ifdef CONFIG_DRM_NOUVEAU_BACKLIGHT
 struct nouveau_backlight;
@@ -111,7 +112,7 @@ nouveau_crtc_connector_get(struct nouveau_crtc *nv_crtc)
 }
 
 struct drm_connector *
-nouveau_connector_create(struct drm_device *, int index);
+nouveau_connector_create(struct drm_device *, const struct dcb_output *);
 
 extern int nouveau_tv_disable;
 extern int nouveau_ignorelid;
