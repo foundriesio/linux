378750fff0d98dd96d79553ebf24afcfd5d3f475
Coly Li <colyli@suse.de>
2019-05-08 00:27:53 +0800

    bcache: remove useless parameter of bch_debug_init() (bsc#1130972).
    suse-commit: 9efb647fb3bab6b2576234d2c7093607ebc89f4b

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -2392,7 +2392,7 @@ static int __init bcache_init(void)
 	    sysfs_create_files(bcache_kobj, files))
 		goto err;
 
-	bch_debug_init(bcache_kobj);
+	bch_debug_init();
 	closure_debug_init();
 
 	return 0;
