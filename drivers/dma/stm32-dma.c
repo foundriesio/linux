8cc148adfae3437c591f57e4a3da64f0833eb247
Amelie Delaunay <amelie.delaunay@st.com>
2019-06-24 11:09:36 +0200

    dmaengine: stm32-dma: use vchan_terminate_vdesc() in .terminate_all
    To avoid race with vchan_complete, use the race free way to terminate
    running transfer.
    
    Move vdesc->node list_del in stm32_dma_start_transfer instead of in
    stm32_mdma_chan_complete to avoid another race in vchan_dma_desc_free_list.
    
    Change-Id: Ia1b466fbf8930364cf047faf9b5e4fd28b470c61
    Signed-off-by: Amelie Delaunay <amelie.delaunay@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/136773
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Hugues FRUCHET <hugues.fruchet@st.com>
    Reviewed-by: Alexandre TORGUE <alexandre.torgue@st.com>
    Tested-by: Hugues FRUCHET <hugues.fruchet@st.com>

--- drivers/dma/stm32-dma.c
+++ drivers/dma/stm32-dma.c
@@ -525,8 +525,10 @@ static int stm32_dma_terminate_all(struct dma_chan *c)
 		spin_lock_irqsave(&chan->vchan.lock, flags);
 	}
 
-	if (chan->busy) {
-		stm32_dma_stop(chan);
+	if (chan->desc) {
+		vchan_terminate_vdesc(&chan->desc->vdesc);
+		if (chan->busy)
+			stm32_dma_stop(chan);
 		chan->desc = NULL;
 	}
 
@@ -808,7 +810,6 @@ static void stm32_mdma_chan_complete(void *param,
 		}
 
 		if (chan->next_sg == chan->desc->num_sgs) {
-			list_del(&chan->desc->vdesc.node);
 			vchan_cookie_complete(&chan->desc->vdesc);
 			chan->desc = NULL;
 		}
@@ -897,6 +898,8 @@ static void stm32_dma_start_transfer(struct stm32_dma_chan *chan)
 		vdesc = vchan_next_desc(&chan->vchan);
 		if (!vdesc)
 			return;
+
+		list_del(&vdesc->node);
 
 		chan->desc = to_stm32_dma_desc(vdesc);
 		chan->next_sg = 0;
@@ -1000,7 +1003,6 @@ static void stm32_dma_handle_chan_done(struct stm32_dma_chan *chan)
 		if (chan->use_mdma && chan->mchan.dir != DMA_MEM_TO_DEV)
 			return;
 		if (chan->next_sg == chan->desc->num_sgs) {
-			list_del(&chan->desc->vdesc.node);
 			vchan_cookie_complete(&chan->desc->vdesc);
 			chan->desc = NULL;
 		}
