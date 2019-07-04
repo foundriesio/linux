39ff7aa26bd458f2a4bcc0661753acf8d02dec8e
Tony Jones <tonyj@suse.de>
2019-07-04 14:36:14 -0700

    perf/x86: Move Nehalem PEBS code to flag (jsc#SLE-5373 (dependent patch)).
    suse-commit: 289a223367f38739a6c4f7f8ff7b04502b0955c6

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -3999,6 +3999,7 @@ __init int intel_pmu_init(void)
 
 		intel_pmu_pebs_data_source_nhm();
 		x86_add_quirk(intel_nehalem_quirk);
+		x86_pmu.pebs_no_tlb = 1;
 
 		pr_cont("Nehalem events, ");
 		break;
