f1236c7f74d7e77457111eabddbbc88d2b7164c2
Michal Suchanek <msuchanek@suse.de>
2019-07-04 23:49:44 +0200

    Add back sibling paca poiter to paca (bsc#1055117).
    suse-commit: a873261d38d43e81ee1eb91148a2ccff8026d360

--- arch/powerpc/include/asm/paca.h
+++ arch/powerpc/include/asm/paca.h
@@ -198,6 +198,7 @@ struct paca_struct {
 	u8 subcore_sibling_mask;
 	/* Flag to request this thread not to stop */
 	atomic_t dont_stop;
+	struct paca_struct **thread_sibling_pacas;
 	/* The PSSCR value that the kernel requested before going to stop */
 	u64 requested_psscr;
 
