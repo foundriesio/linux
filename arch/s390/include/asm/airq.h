0ea6eb2c0858b159cd169363da7c74fd29386ac7
Petr Tesarik <ptesarik@suse.cz>
2019-07-19 11:31:07 +0200

    kABI: mask changes made by use of DMA memory for adapter interrupts (jsc#SLE-6197 FATE#327012 bsc#1140559 LTC#173150).
    suse-commit: f33c33608dd6c2418da26fe515aa8dca17edd18f

--- arch/s390/include/asm/airq.h
+++ arch/s390/include/asm/airq.h
@@ -10,7 +10,9 @@
 #define _ASM_S390_AIRQ_H
 
 #include <linux/bit_spinlock.h>
+#ifndef __GENKSYMS__
 #include <linux/dma-mapping.h>
+#endif
 
 struct airq_struct {
 	struct hlist_node list;		/* Handler queueing. */
@@ -29,7 +31,6 @@ void unregister_adapter_interrupt(struct airq_struct *airq);
 /* Adapter interrupt bit vector */
 struct airq_iv {
 	unsigned long *vector;	/* Adapter interrupt bit vector */
-	dma_addr_t vector_dma; /* Adapter interrupt bit vector dma */
 	unsigned long *avail;	/* Allocation bit mask for the bit vector */
 	unsigned long *bitlock;	/* Lock bit mask for the bit vector */
 	unsigned long *ptr;	/* Pointer associated with each bit */
@@ -38,6 +39,9 @@ struct airq_iv {
 	unsigned long end;	/* Number of highest allocated bit + 1 */
 	unsigned long flags;	/* Allocation flags */
 	spinlock_t lock;	/* Lock to protect alloc & free */
+#ifndef __GENKSYMS__
+	dma_addr_t vector_dma; /* Adapter interrupt bit vector dma */
+#endif
 };
 
 #define AIRQ_IV_ALLOC		1	/* Use an allocation bit mask */
