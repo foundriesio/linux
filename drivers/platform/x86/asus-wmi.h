ca2bf20249f1ed5ce184fe83b512709fc2c15324
Takashi Iwai <tiwai@suse.de>
2019-07-24 17:32:19 +0200

    Fix kABI for asus-wmi quirk_entry field addition (bsc#1051510).
    suse-commit: 955a98c88c98f16edbbaabfbebefd4dd40d3773c

--- drivers/platform/x86/asus-wmi.h
+++ drivers/platform/x86/asus-wmi.h
@@ -44,8 +44,10 @@ struct quirk_entry {
 	bool store_backlight_power;
 	bool wmi_backlight_power;
 	bool wmi_backlight_native;
-	bool wmi_backlight_set_devstate;
 	bool wmi_force_als_set;
+#ifndef __GENKSYMS__
+	bool wmi_backlight_set_devstate;
+#endif
 	int wapf;
 	/*
 	 * For machines with AMD graphic chips, it will send out WMI event
