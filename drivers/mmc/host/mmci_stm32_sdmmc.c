fefa25b8afcd07a7035a7082cae352cb38cd90a7
Ludovic Barre <ludovic.barre@st.com>
2019-08-02 12:08:26 +0200

    mmc: mmci: sdmmc: add busy_complete callback
    This patch adds a specific busy_complete callback for sdmmc variant.
    
    sdmmc has 2 status flags:
    -busyd0: This is a hardware status flag (inverted value of d0 line).
    it does not generate an interrupt.
    -busyd0end: This indicates only end of busy following a CMD response.
    On busy to Not busy changes, an interrupt is generated (if unmask)
    and BUSYD0END status flag is set. Status flag is cleared by writing
    corresponding interrupt clear bit in MMCICLEAR.
    
    The legacy busy completion monitors step by step the busy progression
    start/in-progress/end. On sdmmc variant, the monitoring of busy steps
    is difficult and not adapted (the software can miss a step and locks
    the monitoring), the sdmmc has just need to wait the busyd0end bit
    without monitoring all the changes.
    
    Signed-off-by: Ludovic Barre <ludovic.barre@st.com>

--- drivers/mmc/host/mmci_stm32_sdmmc.c
+++ drivers/mmc/host/mmci_stm32_sdmmc.c
@@ -282,6 +282,43 @@ static u32 sdmmc_get_dctrl_cfg(struct mmci_host *host)
 	return datactrl;
 }
 
+bool sdmmc_busy_complete(struct mmci_host *host, u32 status, u32 err_msk)
+{
+	void __iomem *base = host->base;
+	u32 busy_d0, busy_d0end, mask;
+
+	mask = readl_relaxed(base + MMCIMASK0);
+	busy_d0end = readl_relaxed(base + MMCISTATUS) & MCI_STM32_BUSYD0END;
+	busy_d0 = readl_relaxed(base + MMCISTATUS) & MCI_STM32_BUSYD0;
+
+	/* complete if there is an error or busy_d0end */
+	if ((status & err_msk) || busy_d0end)
+		goto complete;
+
+	/*
+	 * On response the busy signaling is reflected in the BUSYD0 flag.
+	 * if busy_d0 is in-progress we must activate busyd0end interrupt
+	 * to wait this completion. Else this request has no busy step.
+	 */
+	if (busy_d0) {
+		if (!host->busy_status) {
+			writel_relaxed(mask | host->variant->busy_detect_mask,
+				       base + MMCIMASK0);
+			host->busy_status = status &
+				(MCI_CMDSENT | MCI_CMDRESPEND);
+		}
+		return false;
+	}
+
+complete:
+	writel_relaxed(mask & ~host->variant->busy_detect_mask,
+		       base + MMCIMASK0);
+	writel_relaxed(host->variant->busy_detect_mask, base + MMCICLEAR);
+	host->busy_status = 0;
+
+	return true;
+}
+
 static struct mmci_host_ops sdmmc_variant_ops = {
 	.validate_data = sdmmc_idma_validate_data,
 	.prep_data = sdmmc_idma_prep_data,
@@ -292,6 +329,7 @@ static struct mmci_host_ops sdmmc_variant_ops = {
 	.dma_finalize = sdmmc_idma_finalize,
 	.set_clkreg = mmci_sdmmc_set_clkreg,
 	.set_pwrreg = mmci_sdmmc_set_pwrreg,
+	.busy_complete = sdmmc_busy_complete,
 };
 
 void sdmmc_variant_init(struct mmci_host *host)
