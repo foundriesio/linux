c71a94e5b05a8f956f01a3b1f73f2605b117ac88
Nikolay Borisov <nborisov@suse.com>
2019-08-09 14:17:33 +0300

    btrfs: Split btrfs_del_delalloc_inode into 2 functions (bsc#1050911).
    suse-commit: 096de1943b937154a9ddd33c932605df2e6fe51f

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -3229,6 +3229,8 @@ static inline void btrfs_force_ra(struct address_space *mapping,
 	page_cache_sync_readahead(mapping, ra, file, offset, req_size);
 }
 
+void __btrfs_del_delalloc_inode(struct btrfs_root *root,
+				struct btrfs_inode *inode);
 struct inode *btrfs_lookup_dentry(struct inode *dir, struct dentry *dentry);
 int btrfs_set_inode_index(struct btrfs_inode *dir, u64 *index);
 int btrfs_unlink_inode(struct btrfs_trans_handle *trans,
