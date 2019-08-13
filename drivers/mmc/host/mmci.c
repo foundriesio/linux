1e35b7426c96e0f47df19f496273b5193db19662
Ludovic Barre <ludovic.barre@st.com>
2019-08-13 14:32:38 +0200

    mmc: mmci: allow sdio transfer not aligned on blocksz
    SDIO cards may transfer data in either a multi-byte (1 to 512 bytes)
    or an optional block format, while the SD memory cards are fixed in
    the block transfer mode. The Physical Layer Specification limits
    the block size for data transfer to powers of 2 (i.e. 512, 1024, 2048)
    unless using partial read and write.
    
    Signed-off-by: Ludovic Barre <ludovic.barre@st.com>

--- drivers/mmc/host/mmci.c
+++ drivers/mmc/host/mmci.c
@@ -499,7 +499,8 @@ static int mmci_validate_data(struct mmci_host *host,
 	if (!data)
 		return 0;
 
-	if (!is_power_of_2(data->blksz)) {
+	if ((host->mmc->card && !mmc_card_sdio(host->mmc->card)) &&
+	    !is_power_of_2(data->blksz)) {
 		dev_err(mmc_dev(host->mmc),
 			"unsupported block size (%d bytes)\n", data->blksz);
 		return -EINVAL;
