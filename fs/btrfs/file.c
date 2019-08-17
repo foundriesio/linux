e51c907ad5e5c8dde46b06fc352f70738dc2d5c4
Filipe Manana <fdmanana@suse.com>
2019-08-17 09:32:13 +0100

    Refresh patches.suse/btrfs-add-missing-inode-version-ctime-and-mtime-upda.patch.
    suse-commit: caa1588856e42ff92ecf46ae45979546521684c3

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -2783,7 +2783,7 @@ out_only_mutex:
 		 * for detecting, at fsync time, if the inode isn't yet in the
 		 * log tree or it's there but not up to date.
 		 */
-		struct timespec64 now = current_time(inode);
+		struct timespec now = current_time(inode);
 
 		inode_inc_iversion(inode);
 		inode->i_mtime = now;
