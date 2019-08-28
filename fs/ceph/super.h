b223edd3b6dfa5c243f81e0f521f82699ce7550f
Luis Henriques <lhenriques@suse.com>
2019-08-28 17:17:04 +0100

    ceph: handle btime in cap messages (bsc#1148133 bsc#1136682).
    suse-commit: a2af5f74ad16803fbe4ab6bb5f2282ef59d39602

--- fs/ceph/super.h
+++ fs/ceph/super.h
@@ -193,7 +193,7 @@ struct ceph_cap_snap {
 	u64 xattr_version;
 
 	u64 size;
-	struct timespec mtime, atime, ctime;
+	struct timespec mtime, atime, ctime, btime;
 	u64 time_warp_seq;
 	u64 truncate_size;
 	u32 truncate_seq;
