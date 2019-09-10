9e76b3d11462bbfdcadcf93460c9700647b73392
Hannes Reinecke <hare@suse.de>
2019-09-10 14:10:57 +0200

    blk-mq: backport fixes for blk_mq_complete_e_request_sync() (bsc#1145661).
    suse-commit: a78db034e899cc3000d01d06c05b83e201521c6d

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -590,8 +590,8 @@ EXPORT_SYMBOL(blk_mq_complete_request);
 
 void blk_mq_complete_request_sync(struct request *rq)
 {
-	WRITE_ONCE(rq->state, MQ_RQ_COMPLETE);
-	rq->q->mq_ops->complete(rq);
+	if (!blk_mark_rq_complete(rq))
+		rq->q->mq_ops->complete(rq);
 }
 EXPORT_SYMBOL_GPL(blk_mq_complete_request_sync);
 
