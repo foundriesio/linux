365b2848d8c468999205d0e51ae49eb4d6a92c0e
Erwan Le Ray <erwan.leray@st.com>
2019-09-18 16:47:56 +0200

    serial: stm32: fix a deadlock condition with wakeup event
    Deadlock issue is seen when enabling:
    - CONFIG_PROVE_LOCKING=Y
    - uart console as wakeup source
      echo enabled > /sys/class/tty/ttySTM0/power/wakeup
    - go to suspend:
      echo mem > /sys/power/state
    - wake via usart console (rx)
    
    Above conditions lead to trig bellow message:
    ======================================================
    WARNING: possible circular locking dependency detected
    ...
    d4a90b1e (&(&dev->power.lock)->rlock){-.-.}, at: pm_wakeup_dev_event...
    ...
       49.363406] -> #4 (&port_lock_key){-.-.}:
    [   49.363441]        _raw_spin_lock+0x38/0x48
    [   49.363455]        stm32_console_write+0xe4/0x100
    [   49.363469]        console_unlock+0x29c/0x620
    [   49.363480]        register_console+0x220/0x3d8
    [   49.363492]        uart_add_one_port+0x438/0x4fc
    [   49.363502]        stm32_serial_probe+0x284/0xa90
    ...
    [   49.364108] -> #0 (&(&dev->power.lock)->rlock){-.-.}:
    [   49.364140]        lock_acquire+0xd8/0x268
    [   49.364151]        _raw_spin_lock_irqsave+0x44/0x58
    [   49.364162]        pm_wakeup_dev_event+0x20/0x4c
    [   49.364172]        stm32_receive_chars+0x33c/0x354
    [   49.364182]        stm32_interrupt+0xbc/0xcc
    
    Change-Id: I58570d74d1bace80bcc9965bb42e4d12cbbc5fc5
    
    ---
    The deadlock is triggered 100% when also disabling console suspend prior
    to go to suspend:
    - echo N > /sys/module/printk/parameters/console_suspend
    
    Simplified call stack with no console suspend, deadlock condition:
    - stm32_console_write <-- spin_lock already held
    - print_circular_bug
    - pm_wakeup_dev_event <-- triggers lockdep as seen above
    - stm32_receive_chars
    - stm32_interrupt <-- wakeup via uart console, takes the lock
    
    So, revisit spin_lock in stm32-usart driver:
    - there is no need to hold the lock to access ICR (atomic clear of status
      flags)
    - only hold the lock inside stm32_receive_chars() routine (no need to
      call pm_wakeup_dev_event with lock held)
    - keep stm32_transmit_chars() routine called with lock held
    
    Change-Id: I300462a423fe398756fe52ad7487fe5064cbce58
    Signed-off-by: Erwan Le Ray <erwan.leray@st.com>
    Signed-off-by: Fabrice Gasnier <fabrice.gasnier@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/138346
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Erwan LE RAY <erwan.leray@st.com>
    Tested-by: Erwan LE RAY <erwan.leray@st.com>

--- drivers/tty/serial/stm32-usart.c
+++ drivers/tty/serial/stm32-usart.c
@@ -228,12 +228,17 @@ static void stm32_usart_receive_chars(struct uart_port *port, bool threaded)
 	struct tty_port *tport = &port->state->port;
 	struct stm32_port *stm32_port = to_stm32_port(port);
 	struct stm32_usart_offsets *ofs = &stm32_port->info->ofs;
-	unsigned long c;
+	unsigned long c, flags;
 	u32 sr;
 	char flag;
 
 	if (irqd_is_wakeup_set(irq_get_irq_data(port->irq)))
 		pm_wakeup_event(tport->tty->dev, 0);
+
+	if (threaded)
+		spin_lock_irqsave(&port->lock, flags);
+	else
+		spin_lock(&port->lock);
 
 	while (stm32_usart_pending_rx(port, &sr, &stm32_port->last_res,
 				      threaded)) {
@@ -290,9 +295,12 @@ static void stm32_usart_receive_chars(struct uart_port *port, bool threaded)
 		uart_insert_char(port, sr, USART_SR_ORE, c, flag);
 	}
 
-	spin_unlock(&port->lock);
+	if (threaded)
+		spin_unlock_irqrestore(&port->lock, flags);
+	else
+		spin_unlock(&port->lock);
+
 	tty_flip_buffer_push(tport);
-	spin_lock(&port->lock);
 }
 
 static void stm32_usart_tx_dma_complete(void *arg)
@@ -339,7 +347,9 @@ static void stm32_usart_rx_dma_complete(void *arg)
 
 	if (stm32port->rx_dma_cb == CALLBACK_NOT_CALLED) {
 		stm32port->rx_dma_cb = CALLBACK_CALLED;
+		spin_unlock_irqrestore(&port->lock, flags);
 		stm32_usart_receive_chars(port, true);
+		return;
 	}
 
 	spin_unlock_irqrestore(&port->lock, flags);
@@ -498,8 +508,6 @@ static irqreturn_t stm32_usart_interrupt(int irq, void *ptr)
 	struct stm32_usart_offsets *ofs = &stm32_port->info->ofs;
 	u32 sr;
 
-	spin_lock(&port->lock);
-
 	sr = readl_relaxed(port->membase + ofs->isr);
 
 	if ((sr & USART_SR_RTOF) && ofs->icr != UNDEF_REG)
@@ -513,10 +521,11 @@ static irqreturn_t stm32_usart_interrupt(int irq, void *ptr)
 	if ((sr & USART_SR_RXNE) && !(stm32_port->rx_ch))
 		stm32_usart_receive_chars(port, false);
 
-	if ((sr & USART_SR_TXE) && !(stm32_port->tx_ch))
+	if ((sr & USART_SR_TXE) && !(stm32_port->tx_ch)) {
+		spin_lock(&port->lock);
 		stm32_usart_transmit_chars(port);
-
-	spin_unlock(&port->lock);
+		spin_unlock(&port->lock);
+	}
 
 	if (stm32_port->rx_ch)
 		return IRQ_WAKE_THREAD;
@@ -529,11 +538,12 @@ static irqreturn_t stm32_usart_threaded_interrupt(int irq, void *ptr)
 	struct uart_port *port = ptr;
 	struct stm32_port *stm32_port = to_stm32_port(port);
 	struct stm32_usart_offsets *ofs = &stm32_port->info->ofs;
-
-	spin_lock(&port->lock);
+	unsigned long flags;
 
 	if (stm32_port->rx_ch) {
+		spin_lock_irqsave(&port->lock, flags);
 		stm32_usart_clr_bits(port, ofs->cr3, USART_CR3_DMAR);
+		spin_unlock_irqrestore(&port->lock, flags);
 		dma_sync_single_for_cpu(port->dev,
 					stm32_port->rx_dma_buf,
 					RX_BUF_L, DMA_FROM_DEVICE);
@@ -541,10 +551,10 @@ static irqreturn_t stm32_usart_threaded_interrupt(int irq, void *ptr)
 		dma_sync_single_for_device(port->dev,
 					   stm32_port->rx_dma_buf,
 					   RX_BUF_L, DMA_FROM_DEVICE);
+		spin_lock_irqsave(&port->lock, flags);
 		stm32_usart_set_bits(port, ofs->cr3, USART_CR3_DMAR);
+		spin_unlock_irqrestore(&port->lock, flags);
 	}
-
-	spin_unlock(&port->lock);
 
 	return IRQ_HANDLED;
 }
