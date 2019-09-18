aa33d949ce482b28684dddf004bdd3f559e4241d
Takashi Iwai <tiwai@suse.de>
2019-09-18 16:11:54 +0200

    ACPI / property: Fix acpi_graph_get_remote_endpoint() name in kerneldoc (bsc#1051510).
    suse-commit: cf741d50cc454315acbeae0f4ad6dfbfbdce70f2

--- drivers/acpi/property.c
+++ drivers/acpi/property.c
@@ -1066,7 +1066,7 @@ static struct fwnode_handle *acpi_graph_get_child_prop_value(
 
 
 /**
- * acpi_graph_get_remote_enpoint - Parses and returns remote end of an endpoint
+ * acpi_graph_get_remote_endpoint - Parses and returns remote end of an endpoint
  * @fwnode: Endpoint firmware node pointing to a remote device
  * @parent: Firmware node of remote port parent is filled here if not %NULL
  * @port: Firmware node of remote port is filled here if not %NULL
