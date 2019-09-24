20579b6b0f94e3345256dd2a0f5992690c751c4b
Daniel Wagner <dwagner@suse.de>
2019-09-24 13:29:21 +0200

    scsi: qla2xxx: Change abort wait_loop from msleep to wait_event_timeout (bsc#1123034 bsc#1131304 bsc#1127988).
    suse-commit: 011b1a7409b4902cc53032c75aa7ead261534e05

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -546,6 +546,7 @@ typedef struct srb {
 	int rc;
 	int retry_count;
 	struct completion comp;
+	wait_queue_head_t *cwaitq;
 	union {
 		struct srb_iocb iocb_cmd;
 		struct bsg_job *bsg_job;
@@ -4792,5 +4793,4 @@ struct sff_8247_a0 {
 #include "qla_gbl.h"
 #include "qla_dbg.h"
 #include "qla_inline.h"
-
 #endif
