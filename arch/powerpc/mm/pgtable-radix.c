a506a1cf93bae32f6f31cace21dc5dc6b173c78c
Michal Suchanek <msuchanek@suse.de>
2019-10-07 16:13:19 +0200

    powerpc/64s/pseries: radix flush translations before MMU is enabled at boot (bsc#1055186).
    suse-commit: 6dd6649b75b8356527a9291c41fb026a6789becc

--- arch/powerpc/mm/pgtable-radix.c
+++ arch/powerpc/mm/pgtable-radix.c
@@ -536,8 +536,7 @@ void __init radix__early_init_mmu(void)
 	radix_init_pgtable();
 	/* Switch to the guard PID before turning on MMU */
 	radix__switch_mmu_context(NULL, &init_mm);
-	if (cpu_has_feature(CPU_FTR_HVMODE))
-		tlbiel_all();
+	tlbiel_all();
 }
 
 void radix__early_init_mmu_secondary(void)
@@ -557,8 +556,7 @@ void radix__early_init_mmu_secondary(void)
 	radix_init_iamr();
 
 	radix__switch_mmu_context(NULL, &init_mm);
-	if (cpu_has_feature(CPU_FTR_HVMODE))
-		tlbiel_all();
+	tlbiel_all();
 }
 
 void radix__mmu_cleanup_all(void)
