f5c5a691e181cc34fb6ecd0a4f67558f4340c4bf
Joerg Roedel <jroedel@suse.de>
2019-10-16 13:16:11 +0200

    kvm: x86, powerpc: do not allow clearing largepages debugfs entry (bsc#1117665).
    suse-commit: a7fcd25bf7c51d34a51ad963f7c26ff72cc10209

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -1019,6 +1019,7 @@ enum kvm_stat_kind {
 
 struct kvm_stat_data {
 	int offset;
+	int mode;
 	struct kvm *kvm;
 };
 
@@ -1026,6 +1027,7 @@ struct kvm_stats_debugfs_item {
 	const char *name;
 	int offset;
 	enum kvm_stat_kind kind;
+	int mode;
 };
 extern struct kvm_stats_debugfs_item debugfs_entries[];
 extern struct dentry *kvm_debugfs_dir;
