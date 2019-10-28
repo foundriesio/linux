971545ebdb24c904fee6c24b174dd2999c9823d1
Joerg Roedel <jroedel@suse.de>
2019-10-28 10:36:15 +0100

    - x86: Add ITLB_MULTIHIT bug infrastructure (bsc#1117665,   CVE-2018-12207). - x86/cpu: Add Tremont to the cpu vulnerability (bsc#1117665,   CVE-2018-12207). - kvm: mmu: ITLB_MULTIHIT mitigation (bsc#1117665,   CVE-2018-12207). - kvm: Add helper function for creating VM worker (bsc#1117665,   CVE-2018-12207). - kvm: x86: mmu: Recovery of shattered NX large pages   (bsc#1117665, CVE-2018-12207). - KVM: vmx, svm: always run with EFER.NXE=1 when shadow paging   is active (bsc#1117665). - Refresh patches.kabi/Fix-KVM-kABI-after-x86-mmu-backports.patch.
    suse-commit: 71771f2cc25eedea03398f1eb23bc16766d15326

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -277,6 +277,7 @@ struct kvm_rmap_head {
 struct kvm_mmu_page {
 	struct list_head link;
 	struct hlist_node hash_link;
+	struct list_head lpage_disallowed_link;
 
 	/*
 	 * The following two entries are used to key the shadow page in the
@@ -295,6 +296,7 @@ struct kvm_mmu_page {
 
 	/* The page is obsolete if mmu_valid_gen != kvm->arch.mmu_valid_gen.  */
 	unsigned long mmu_valid_gen;
+	bool lpage_disallowed; /* Can't be replaced by an equiv large page */
 
 	DECLARE_BITMAP(unsync_child_bitmap, 512);
 
@@ -798,6 +800,7 @@ struct kvm_arch {
 	 */
 	struct list_head active_mmu_pages;
 	struct list_head zapped_obsolete_pages;
+	struct list_head lpage_disallowed_mmu_pages;
 	struct kvm_page_track_notifier_node mmu_sp_tracker;
 	struct kvm_page_track_notifier_head track_notifier_head;
 
@@ -875,6 +878,8 @@ struct kvm_arch {
 	bool x2apic_broadcast_quirk_disabled;
 
 	struct kvm_sev_info sev_info;
+
+	struct task_struct *nx_lpage_recovery_thread;
 };
 
 struct kvm_vm_stat {
@@ -888,6 +893,7 @@ struct kvm_vm_stat {
 	ulong mmu_unsync;
 	ulong remote_tlb_flush;
 	ulong lpages;
+	ulong nx_lpage_splits;
 	ulong max_mmu_page_hash_collisions;
 };
 
