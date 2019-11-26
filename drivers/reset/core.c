49e5f3d498448d872e8e34fab490a4ea6762c22a
Takashi Iwai <tiwai@suse.de>
2019-11-26 14:40:14 +0100

    reset: fix of_reset_simple_xlate kerneldoc comment (bsc#1051510).
    suse-commit: 7ccea2976ac9056adce0dbcf7589bdadcf424969

--- drivers/reset/core.c
+++ drivers/reset/core.c
@@ -50,7 +50,6 @@ struct reset_control {
  * of_reset_simple_xlate - translate reset_spec to the reset line number
  * @rcdev: a pointer to the reset controller device
  * @reset_spec: reset line specifier as found in the device tree
- * @flags: a flags pointer to fill in (optional)
  *
  * This simple translation function should be used for reset controllers
  * with 1:1 mapping, where reset lines can be indexed by number without gaps.
