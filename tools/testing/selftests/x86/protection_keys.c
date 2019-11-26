57173e70fef057f9dd95639f376f9780e7358ee1
Michal Suchanek <msuchanek@suse.de>
2019-11-26 19:08:27 +0100

    docs: move protection-keys.rst to the core-api book (FATE#322447, bsc#1078248).
    suse-commit: a5594373329f862e34d14a0afc9801715da75c90

--- tools/testing/selftests/x86/protection_keys.c
+++ tools/testing/selftests/x86/protection_keys.c
@@ -1,5 +1,5 @@
 /*
- * Tests x86 Memory Protection Keys (see Documentation/x86/protection-keys.txt)
+ * Tests x86 Memory Protection Keys (see Documentation/core-api/protection-keys.rst)
  *
  * There are examples in here of:
  *  * how to set protection keys on memory
