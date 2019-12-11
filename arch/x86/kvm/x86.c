e9a47432dd25035fc78a17c3794926467fccc241
Joerg Roedel <jroedel@suse.de>
2019-12-11 14:23:29 +0100

    KVM: x86: Remove a spurious export of a static function (bsc#1158954).
    suse-commit: 51a5f461b15977addeab225086de2e5393fee759

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1105,8 +1105,6 @@ u64 kvm_get_arch_capabilities(void)
 	return data;
 }
 
-EXPORT_SYMBOL_GPL(kvm_get_arch_capabilities);
-
 static int kvm_get_msr_feature(struct kvm_msr_entry *msr)
 {
 	switch (msr->index) {
