0cfe1071b3f687bcdc88fed66b66bd4bc7f14beb
Yannick Fertre <yannick.fertre@st.com>
2019-12-13 16:13:06 +0100

    drm/panel: rm68200: allow using non-continuous dsi clock
    The panel is able to work when dsi clock is non-continuous, thus
    the system power consumption can be reduced using such feature.
    
    Add MIPI_DSI_CLOCK_NON_CONTINUOUS to panel's mode_flags.
    
    Signed-off-by: Antonio Borneo <antonio.borneo@st.com>
    Change-Id: I71064ca1fc76f4d9647f1fc519cf2a803e78382a
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/153240
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Yannick FERTRE <yannick.fertre@st.com>
    Reviewed-by: Philippe CORNU <philippe.cornu@st.com>
    Tested-by: Yannick FERTRE <yannick.fertre@st.com>

--- drivers/gpu/drm/panel/panel-raydium-rm68200.c
+++ drivers/gpu/drm/panel/panel-raydium-rm68200.c
@@ -402,7 +402,7 @@ static int rm68200_probe(struct mipi_dsi_device *dsi)
 	dsi->lanes = 2;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
-			  MIPI_DSI_MODE_LPM;
+			  MIPI_DSI_MODE_LPM | MIPI_DSI_CLOCK_NON_CONTINUOUS;
 
 	drm_panel_init(&ctx->panel);
 	ctx->panel.dev = dev;
