9a5d185c0a98a921623e6affe82c4c91223fec29
Michal Suchanek <msuchanek@suse.de>
2019-12-18 22:31:06 +0100

    Refresh patches.kabi/kABI-add-_q-suffix-to-exports-that-take-struct-dh.patch.
    Remove the fips=1 check in dh.c
    
    dh.c is not fips-specific and should perform the same regardless of this
    setting.
    
    suse-commit: b58e024a02befe980baf7b34d992c49bc019387e

--- crypto/dh.c
+++ crypto/dh.c
@@ -13,7 +13,6 @@
 #include <crypto/internal/kpp.h>
 #include <crypto/kpp.h>
 #include <crypto/dh.h>
-#include <linux/fips.h>
 #include <linux/mpi.h>
 
 struct dh_ctx {
@@ -176,11 +175,9 @@ static int dh_compute_value(struct kpp_request *req)
 			ret = -EINVAL;
 			goto err_free_val;
 		}
-		if (fips_enabled) {
-			ret = dh_is_pubkey_valid(ctx, base);
-			if (ret)
-				goto err_free_base;
-		}
+		ret = dh_is_pubkey_valid(ctx, base);
+		if (ret)
+			goto err_free_base;
 	} else {
 		base = ctx->g;
 	}
