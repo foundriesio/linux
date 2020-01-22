77fc2935d306a957f0bf3c78e2eb1216de586a3b
정태훈 (Ted Jeong) <ted.jeong@telechips.com>
2020-01-22 18:05:08 +0900

    [bug] mmc: sdhci-tcc: fix the compilation error
    This patch fixes the incompatible pointer type error.
    
    Linked Commit:
    ITS:
    
    Change-Id: Id57fa83a7414cf798c08c83cd679d8b70317e9b4
    (cherry picked from commit 3d0ab37ee7a0e572cbc3426271f6d0fda94a84df)

--- drivers/mmc/host/sdhci-tcc.c
+++ drivers/mmc/host/sdhci-tcc.c
@@ -842,7 +842,7 @@ static int sdhci_tcc_tap_dly_show(struct seq_file *sf, void *data)
 	return 0;
 }
 
-static int sdhci_tcc_tap_dly_store(struct file *file,
+static ssize_t sdhci_tcc_tap_dly_store(struct file *file,
 					const char __user *ubuf, size_t count, loff_t *ppos)
 {
 	struct seq_file *sf = file->private_data;
@@ -882,7 +882,7 @@ static int sdhci_tcc_clk_dly_show(struct seq_file *sf, void *data)
 	return 0;
 }
 
-static int sdhci_tcc_clk_dly_store(struct file *file,
+static ssize_t sdhci_tcc_clk_dly_store(struct file *file,
 					const char __user *ubuf, size_t count, loff_t *ppos)
 {
 	struct seq_file *sf = file->private_data;
