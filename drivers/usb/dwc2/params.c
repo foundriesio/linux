c839116551e1437abfec6aa7ba319d76e99c1839
Amelie Delaunay <amelie.delaunay@st.com>
2020-01-31 14:29:00 +0100

    usb: dwc2: set ahbcfg parameter for STM32MP1 OTG HS and FS
    STM32MP1 ahbcfg register default value sets Burst length/type (HBSTLEN)
    to Single (32-bit accesses on AHB), which is not recommended, according
    to STM32MP157 Reference manual [1].
    This patch sets Burst length/type (HBSTLEN) so that bus transactions
    target 16x32 bit accesses. This improves OTG controller performance.
    
    [1] https://www.st.com/resource/en/reference_manual/dm00327659.pdf, p.3147
    
    Change-Id: I989c588f76996a41f78cfdb591005f2fd54aaad6
    Signed-off-by: Amelie Delaunay <amelie.delaunay@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/156688
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Fabrice GASNIER <fabrice.gasnier@st.com>

--- drivers/usb/dwc2/params.c
+++ drivers/usb/dwc2/params.c
@@ -166,6 +166,7 @@ static void dwc2_set_stm32mp1_fsotg_params(struct dwc2_hsotg *hsotg)
 	p->i2c_enable = false;
 	p->activate_stm_fs_transceiver = true;
 	p->activate_stm_id_vb_detection = true;
+	p->ahbcfg = GAHBCFG_HBSTLEN_INCR16 << GAHBCFG_HBSTLEN_SHIFT;
 	p->power_down = DWC2_POWER_DOWN_PARAM_NONE;
 	p->host_support_fs_ls_low_power = true;
 	p->host_ls_low_power_phy_clk = true;
@@ -181,6 +182,7 @@ static void dwc2_set_stm32mp1_hsotg_params(struct dwc2_hsotg *hsotg)
 	p->host_rx_fifo_size = 440;
 	p->host_nperio_tx_fifo_size = 256;
 	p->host_perio_tx_fifo_size = 256;
+	p->ahbcfg = GAHBCFG_HBSTLEN_INCR16 << GAHBCFG_HBSTLEN_SHIFT;
 	p->power_down = DWC2_POWER_DOWN_PARAM_NONE;
 }
 
