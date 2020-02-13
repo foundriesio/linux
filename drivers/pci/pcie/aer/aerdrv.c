742e307cfa139d79540df0fd5e842adbe9f4b3b1
Takashi Iwai <tiwai@suse.de>
2020-02-13 13:59:05 +0100

    PCI/ERR: Use slot reset if available (bsc#1161561).
    suse-commit: b0ede7b02b0fe71a53c744a8a5bc60941f803008

--- drivers/pci/pcie/aer/aerdrv.c
+++ drivers/pci/pcie/aer/aerdrv.c
@@ -324,7 +324,7 @@ static pci_ers_result_t aer_root_reset(struct pci_dev *dev)
 	reg32 &= ~ROOT_PORT_INTR_ON_MESG_MASK;
 	pci_write_config_dword(dev, pos + PCI_ERR_ROOT_COMMAND, reg32);
 
-	pci_reset_bridge_secondary_bus(dev);
+	pci_bus_error_reset(dev);
 	pci_printk(KERN_DEBUG, dev, "Root Port link has been reset\n");
 
 	/* Clear Root Error Status */
