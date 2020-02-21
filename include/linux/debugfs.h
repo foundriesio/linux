c44ec38d1c4b08ee1785385a4209c01685409c36
Nicolai Stange <nstange@suse.de>
2020-02-21 12:02:38 +0100

    simple_recursive_removal(): kernel-side rm -rf for ramfs-style filesystems (bsc#1159198 CVE-2019-19770).
    - Import upstream commit.
    - Refresh patches.kabi/debugfs-kabi-fix-d_fsdata-semantics.patch.
    - Refresh
      patches.kabi/debugfs-kabi-provide-removal-prot-comp-wrappers.patch.
    
    suse-commit: 894cecb4a760ec06db6cbcade0fd570e69e4d8d2

--- include/linux/debugfs.h
+++ include/linux/debugfs.h
@@ -85,7 +85,7 @@ struct dentry *debugfs_create_automount(const char *name,
 					void *data);
 
 void debugfs_remove(struct dentry *dentry);
-void debugfs_remove_recursive(struct dentry *dentry);
+#define debugfs_remove_recursive debugfs_remove
 
 /* kABI compatibility dummies */
 int debugfs_use_file_start(const struct dentry *dentry, int *srcu_idx);
