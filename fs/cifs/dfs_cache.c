9edf09d001a92bcbd621899d925f63295f7251ca
Paulo Alcantara <palcantara@suse.de>
2020-03-01 19:15:49 -0300

    cifs: Merge is_path_valid() into get_normalized_path() (bsc#1144333).
    suse-commit: acf7a0970f798edc113697d37c99cea26f2d0d5b

--- fs/cifs/dfs_cache.c
+++ fs/cifs/dfs_cache.c
@@ -75,13 +75,11 @@ static void refresh_cache_worker(struct work_struct *work);
 
 static DECLARE_DELAYED_WORK(refresh_task, refresh_cache_worker);
 
-static inline bool is_path_valid(const char *path)
+static int get_normalized_path(const char *path, char **npath)
 {
-	return path && (strchr(path + 1, '\\') || strchr(path + 1, '/'));
-}
+	if (!path || strlen(path) < 3 || (*path != '\\' && *path != '/'))
+		return -EINVAL;
 
-static inline int get_normalized_path(const char *path, char **npath)
-{
 	if (*path == '\\') {
 		*npath = (char *)path;
 	} else {
@@ -826,9 +824,6 @@ int dfs_cache_find(const unsigned int xid, struct cifs_ses *ses,
 	char *npath;
 	struct cache_entry *ce;
 
-	if (unlikely(!is_path_valid(path)))
-		return -EINVAL;
-
 	rc = get_normalized_path(path, &npath);
 	if (rc)
 		return rc;
@@ -872,9 +867,6 @@ int dfs_cache_noreq_find(const char *path, struct dfs_info3_param *ref,
 	int rc;
 	char *npath;
 	struct cache_entry *ce;
-
-	if (unlikely(!is_path_valid(path)))
-		return -EINVAL;
 
 	rc = get_normalized_path(path, &npath);
 	if (rc)
@@ -926,9 +918,6 @@ int dfs_cache_update_tgthint(const unsigned int xid, struct cifs_ses *ses,
 	char *npath;
 	struct cache_entry *ce;
 	struct cache_dfs_tgt *t;
-
-	if (unlikely(!is_path_valid(path)))
-		return -EINVAL;
 
 	rc = get_normalized_path(path, &npath);
 	if (rc)
@@ -987,7 +976,7 @@ int dfs_cache_noreq_update_tgthint(const char *path,
 	struct cache_entry *ce;
 	struct cache_dfs_tgt *t;
 
-	if (unlikely(!is_path_valid(path)) || !it)
+	if (!it)
 		return -EINVAL;
 
 	rc = get_normalized_path(path, &npath);
@@ -1046,8 +1035,6 @@ int dfs_cache_get_tgt_referral(const char *path,
 	unsigned int h;
 
 	if (!it || !ref)
-		return -EINVAL;
-	if (unlikely(!is_path_valid(path)))
 		return -EINVAL;
 
 	rc = get_normalized_path(path, &npath);
