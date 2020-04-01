4281a16617dc65044560c2e0cb1b3dcac5cde07e
Nikolay Borisov <nborisov@suse.com>
2020-04-01 12:29:15 +0300

    btrfs: add a flush step for delayed iputs (bsc#1165949).
    suse-commit: ad90466464720e0bc1e6033605a9136ba50f2d94

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -2549,7 +2549,8 @@ enum btrfs_flush_state {
 	FLUSH_DELALLOC_WAIT	=	6,
 	ALLOC_CHUNK		=	7,
 	ALLOC_CHUNK_FORCE	=	8,
-	COMMIT_TRANS		=	9,
+	RUN_DELAYED_IPUTS	=	9,
+	COMMIT_TRANS		=	10,
 };
 
 void btrfs_trans_release_metadata(struct btrfs_trans_handle *trans,
