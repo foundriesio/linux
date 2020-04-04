b5a504c5bbb65fdbcb34a6f063d65c2c6f166fbd
Ricardo Salveti <ricardo@foundries.io>
2020-04-03 22:14:57 -0300

    [FIO extras] mailbox: arm_mhu_v2: fix incorrect iounmap at mhuv2_probe
    Fix:
    
    In file included from ./include/linux/io.h:13:0,
                     from drivers/mailbox/arm_mhu_v2.c:14:
    drivers/mailbox/arm_mhu_v2.c: In function 'mhuv2_probe':
    ./arch/arm/include/asm/io.h:414:17: warning: 'rx_base' may be
     used uninitialized in this function [-Wmaybe-uninitialized]
     #define iounmap iounmap
                     ^~~~~~~
    drivers/mailbox/arm_mhu_v2.c:140:16: note: 'rx_base' was declared here
      void __iomem *rx_base, *tx_base;
                    ^~~~~~~
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- drivers/mailbox/arm_mhu_v2.c
+++ drivers/mailbox/arm_mhu_v2.c
@@ -152,13 +152,13 @@ static int mhuv2_probe(struct amba_device *adev, const struct amba_id *id)
 	tx_base = of_iomap((struct device_node *)np, 0);
 	if (!tx_base) {
 		dev_err(dev, "failed to map tx registers\n");
-		iounmap(rx_base);
 		return -ENOMEM;
 	}
 
 	rx_base = of_iomap((struct device_node *)np, 1);
 	if (!rx_base) {
 		dev_err(dev, "failed to map rx registers\n");
+		iounmap(tx_base);
 		return -ENOMEM;
 	}
 
