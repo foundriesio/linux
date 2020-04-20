222bc88e4b1ad9cf7a70bc5e07091bb10163640d
Takashi Iwai <tiwai@suse.de>
2020-04-20 17:46:42 +0200

    ALSA: hda: Honor PM disablement in PM freeze and thaw_noirq ops (bsc#1051510).
    suse-commit: 0b361b8090c57b0074ec07cb11d8d0e8ea6af7ab

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -1094,6 +1094,8 @@ static int azx_freeze_noirq(struct device *dev)
 	struct azx *chip = card->private_data;
 	struct pci_dev *pci = to_pci_dev(dev);
 
+	if (!azx_is_pm_ready(card))
+		return 0;
 	if (chip->driver_type == AZX_DRIVER_SKL)
 		pci_set_power_state(pci, PCI_D3hot);
 
@@ -1106,6 +1108,8 @@ static int azx_thaw_noirq(struct device *dev)
 	struct azx *chip = card->private_data;
 	struct pci_dev *pci = to_pci_dev(dev);
 
+	if (!azx_is_pm_ready(card))
+		return 0;
 	if (chip->driver_type == AZX_DRIVER_SKL)
 		pci_set_power_state(pci, PCI_D0);
 
