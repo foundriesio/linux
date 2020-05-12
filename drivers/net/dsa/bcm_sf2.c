e43a933cd30071c2e534d87f4fc8b582d7138127
Jiri Slaby <jslaby@suse.cz>
2020-05-12 10:01:49 +0200

    net: dsa: bcm_sf2: Do not register slave MDIO bus with OF (networking-stable-20_04_09).
    suse-commit: b5869b5fe267bb8a9af7019ced06c0b93a2af5f1

--- drivers/net/dsa/bcm_sf2.c
+++ drivers/net/dsa/bcm_sf2.c
@@ -588,11 +588,7 @@ static int bcm_sf2_mdio_register(struct dsa_switch *ds)
 	priv->slave_mii_bus->parent = ds->dev->parent;
 	priv->slave_mii_bus->phy_mask = ~priv->indir_phy_mask;
 
-	if (dn)
-		err = of_mdiobus_register(priv->slave_mii_bus, dn);
-	else
-		err = mdiobus_register(priv->slave_mii_bus);
-
+	err = mdiobus_register(priv->slave_mii_bus);
 	if (err)
 		of_node_put(dn);
 
