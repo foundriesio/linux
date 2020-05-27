02e8b4cfe24367ef4116902338253d1acdd1f135
Daoying <kimdy@telechips.com>
2020-05-27 08:55:25 +0000

    [upg] DRM Driver
    DRM driver name
     Change DRM driver name to tccdrm instead  of tcc-drm.
     Some SDKs try to convert this DRM driver name into a string using
     preprocessor, but-is recognized as minus and a compile error occurs.
    
    Remove .get_vblank_counter interface from CRTCs
     VIOC Display Device does not maintains a vertical blanking counter.
     Incase of this, Drm drivers should use the drm_vblank_count helper
     function to handle this operation.
    
    Linked Commit:
    ITS:
    
    Change-Id: Ifaa15eb1b1f7ed5a226e7174f2e871f2519bfc47

--- drivers/gpu/drm/tcc/tcc_drm_lcd.c
+++ drivers/gpu/drm/tcc/tcc_drm_lcd.c
@@ -49,6 +49,9 @@
 /* LCD has totally four hardware windows. */
 #define WINDOWS_NR	1
 
+#define LCD_FLAGS_IRQ_BIT	0
+#define LCD_FLAGS_CLK_BIT	1
+
 struct lcd_context {
 	struct device			*dev;
 	struct drm_device		*drm_dev;
@@ -62,7 +65,7 @@ struct lcd_context {
 	unsigned int			ddc_id;		/* TCC display path number */
 	void __iomem			*virt_addr;	/* TCC wmixer node address */
 	int				irq_num;	/* TCC interrupt number */
-	unsigned long			irq_flags;
+	unsigned long			lcd_flags;
 	bool				suspended;
 	wait_queue_head_t		wait_vsync_queue;
 	atomic_t			wait_vsync_event;
@@ -101,8 +104,10 @@ static int lcd_enable_vblank(struct tcc_drm_crtc *crtc)
 	if (ctx->suspended)
 		return -EPERM;
 
-	if (!test_and_set_bit(0, &ctx->irq_flags))
+	if (!test_and_set_bit(LCD_FLAGS_IRQ_BIT, &ctx->lcd_flags)) {
+		DRM_DEBUG("%s-vioc_intr_enable\r\n", __func__);
 		vioc_intr_enable(ctx->irq_num, ctx->ddc_id, VIOC_DISP_INTR_DISPLAY);
+	}
 
 	return 0;
 }
@@ -114,8 +119,10 @@ static void lcd_disable_vblank(struct tcc_drm_crtc *crtc)
 	if (ctx->suspended)
 		return;
 
-	if (test_and_clear_bit(0, &ctx->irq_flags))
+	if (test_and_clear_bit(LCD_FLAGS_IRQ_BIT, &ctx->lcd_flags)) {
+		DRM_DEBUG("%s-vioc_intr_enable\r\n", __func__);
 		vioc_intr_disable(ctx->irq_num, ctx->ddc_id, VIOC_DISP_INTR_DISPLAY);
+	}
 }
 
 static void lcd_wait_for_vblank(struct tcc_drm_crtc *crtc)
@@ -314,20 +321,21 @@ static void lcd_enable(struct tcc_drm_crtc *crtc)
 
 	pm_runtime_get_sync(ctx->dev);
 
-	ret = clk_prepare_enable(ctx->bus_clk);
-	if (ret < 0) {
-		DRM_ERROR("Failed to prepare_enable the bus clk [%d]\n", ret);
-		return;
-	}
+	if (!test_and_set_bit(LCD_FLAGS_CLK_BIT, &ctx->lcd_flags)) {
+		ret = clk_prepare_enable(ctx->bus_clk);
+		if (ret < 0) {
+			DRM_ERROR("Failed to prepare_enable the bus clk [%d]\n", ret);
+			return;
+		}
 
-	ret = clk_prepare_enable(ctx->lcd_clk);
-	if  (ret < 0) {
-		DRM_ERROR("Failed to prepare_enable the lcd clk [%d]\n", ret);
-		return;
+		ret = clk_prepare_enable(ctx->lcd_clk);
+		if  (ret < 0) {
+			DRM_ERROR("Failed to prepare_enable the lcd clk [%d]\n", ret);
+			return;
+		}
 	}
-
 	/* if vblank was enabled status, enable it again. */
-	if (test_and_clear_bit(0, &ctx->irq_flags))
+	if (test_and_clear_bit(LCD_FLAGS_IRQ_BIT, &ctx->lcd_flags))
 		lcd_enable_vblank(ctx->crtc);
 
 	lcd_commit(ctx->crtc);
@@ -341,6 +349,7 @@ static void lcd_disable(struct tcc_drm_crtc *crtc)
 	if (ctx->suspended)
 		return;
 
+	#if !defined(CONFIG_DRM_TCC_CRTC_TO_BE_ALWAYS_ALIVE)
 	/*
 	 * We need to make sure that all windows are disabled before we
 	 * suspend that connector. Otherwise we might try to scan from
@@ -353,8 +362,11 @@ static void lcd_disable(struct tcc_drm_crtc *crtc)
 	lcd_wait_for_vblank(crtc);
 	lcd_disable_vblank(crtc);
 
-	clk_disable_unprepare(ctx->lcd_clk);
-	clk_disable_unprepare(ctx->bus_clk);
+	if (test_and_clear_bit(LCD_FLAGS_CLK_BIT, &ctx->lcd_flags)) {
+		clk_disable_unprepare(ctx->lcd_clk);
+		clk_disable_unprepare(ctx->bus_clk);
+	}
+	#endif
 
 	pm_runtime_put_sync(ctx->dev);
 	ctx->suspended = true;
@@ -386,7 +398,7 @@ static void lcd_te_handler(struct tcc_drm_crtc *crtc)
 		wake_up(&ctx->wait_vsync_queue);
 	}
 
-	if (test_bit(0, &ctx->irq_flags))
+	if (test_bit(LCD_FLAGS_IRQ_BIT, &ctx->lcd_flags))
 		drm_crtc_handle_vblank(&ctx->crtc->base);
 }
 
@@ -420,8 +432,10 @@ static irqreturn_t lcd_irq_handler(int irq, void *dev_id)
 		vioc_intr_clear(ctx->ddc_id, (1 << VIOC_DISP_INTR_RU));
 
 		/* check the crtc is detached already from encoder */
-		if (!ctx->drm_dev)
+		if (ctx->drm_dev == NULL) {
+			printk(KERN_ERR "[ERR][DRM] %s drm_dev is not binded\r\n", __func__);
 			goto out;
+		}
 
 		drm_crtc_handle_vblank(&ctx->crtc->base);
 
@@ -613,11 +627,13 @@ static int lcd_remove(struct platform_device *pdev)
 #ifdef CONFIG_PM
 static int tcc_lcd_suspend(struct device *dev)
 {
+	#if !defined(CONFIG_DRM_TCC_CRTC_TO_BE_ALWAYS_ALIVE)
 	struct lcd_context *ctx = dev_get_drvdata(dev);
-
-	clk_disable_unprepare(ctx->lcd_clk);
-	clk_disable_unprepare(ctx->bus_clk);
-
+	if (test_and_clear_bit(LCD_FLAGS_CLK_BIT, &ctx->lcd_flags)) {
+		clk_disable_unprepare(ctx->lcd_clk);
+		clk_disable_unprepare(ctx->bus_clk);
+	}
+	#endif
 	return 0;
 }
 
@@ -625,17 +641,18 @@ static int tcc_lcd_resume(struct device *dev)
 {
 	struct lcd_context *ctx = dev_get_drvdata(dev);
 	int ret;
+	if (!test_and_set_bit(LCD_FLAGS_CLK_BIT, &ctx->lcd_flags)) {
+		ret = clk_prepare_enable(ctx->bus_clk);
+		if (ret < 0) {
+			DRM_ERROR("Failed to prepare_enable the bus clk [%d]\n", ret);
+			return ret;
+		}
 
-	ret = clk_prepare_enable(ctx->bus_clk);
-	if (ret < 0) {
-		DRM_ERROR("Failed to prepare_enable the bus clk [%d]\n", ret);
-		return ret;
-	}
-
-	ret = clk_prepare_enable(ctx->lcd_clk);
-	if  (ret < 0) {
-		DRM_ERROR("Failed to prepare_enable the lcd clk [%d]\n", ret);
-		return ret;
+		ret = clk_prepare_enable(ctx->lcd_clk);
+		if  (ret < 0) {
+			DRM_ERROR("Failed to prepare_enable the lcd clk [%d]\n", ret);
+			return ret;
+		}
 	}
 
 	return 0;
