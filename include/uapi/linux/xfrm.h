d266464d986a91e2f939a7e11fc4959a2dbdc17c
Coly Li <colyli@suse.de>
2020-06-09 18:39:56 +0800

    xfrm: fix error in comment (git fixes).
    suse-commit: 6945dcc3e017f47765336c0fd060fa1df321a738

--- include/uapi/linux/xfrm.h
+++ include/uapi/linux/xfrm.h
@@ -303,7 +303,7 @@ enum xfrm_attr_type_t {
 	XFRMA_PROTO,		/* __u8 */
 	XFRMA_ADDRESS_FILTER,	/* struct xfrm_address_filter */
 	XFRMA_PAD,
-	XFRMA_OFFLOAD_DEV,	/* struct xfrm_state_offload */
+	XFRMA_OFFLOAD_DEV,	/* struct xfrm_user_offload */
 	__XFRMA_MAX
 
 #define XFRMA_MAX (__XFRMA_MAX - 1)
