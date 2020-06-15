a588adc9bbf14fc7c227c4d74b857a021c2acceb
Alain Volmat <alain.volmat@st.com>
2020-06-15 09:14:49 +0200

    spi: stm32: properly handle 0 byte transfer
    On 0 byte transfer request, return straight from the
    xfer function after finalizing the transfer.
    
    Signed-off-by: Alain Volmat <alain.volmat@st.com>
    Change-Id: Id9e7afcbf9466a62df5dd0a2472af6b50276fcdf
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/170026
    Tested-by: Amelie DELAUNAY <amelie.delaunay@st.com>
    Reviewed-by: Amelie DELAUNAY <amelie.delaunay@st.com>
    Reviewed-by: Fabrice GASNIER <fabrice.gasnier@st.com>

--- drivers/spi/spi-stm32.c
+++ drivers/spi/spi-stm32.c
@@ -1008,6 +1008,12 @@ static int stm32_spi_transfer_one(struct spi_master *master,
 	unsigned long timeout;
 	int ret;
 
+	/* Don't do anything on 0 bytes transfers */
+	if (transfer->len == 0) {
+		spi->xfer_status = 0;
+		goto finalize;
+	}
+
 	spi->tx_buf = transfer->tx_buf;
 	spi->rx_buf = transfer->rx_buf;
 	spi->tx_len = spi->tx_buf ? transfer->len : 0;
@@ -1049,6 +1055,7 @@ static int stm32_spi_transfer_one(struct spi_master *master,
 
 	stm32_spi_disable(spi);
 
+finalize:
 	spi_finalize_current_transfer(master);
 
 	return spi->xfer_status;
