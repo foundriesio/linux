09e7d448b9c0b896cec56fe326a316e07a5ec067
Filipe Manana <fdmanana@suse.com>
2020-07-01 11:53:13 +0100

    - Btrfs: fix list_add corruption and soft lockups in fsync   (bsc#1171761). - Refresh   patches.suse/Btrfs-check-for-the-full-sync-flag-while-holding-the.patch.
    suse-commit: 67a769b188d9524d0646aa0827e6788275cc17ab

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -2060,6 +2060,8 @@ int btrfs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 
 	trace_btrfs_sync_file(file, datasync);
 
+	btrfs_init_log_ctx(&ctx, inode);
+
 	/*
 	 * We write the dirty pages in the range and wait until they complete
 	 * out of the ->i_mutex. If so, we can flush the dirty pages by
@@ -2086,10 +2088,10 @@ int btrfs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 	 * If the inode needs a full sync, make sure we use a full range to
 	 * avoid log tree corruption, due to hole detection racing with ordered
 	 * extent completion for adjacent ranges, and assertion failures during
-	 * hole detection. Do this while holding the inode lock, to avoid races
-	 * with other tasks.
+	 * hole detection.
 	 */
-	if (full_sync) {
+	if (test_bit(BTRFS_INODE_NEEDS_FULL_SYNC,
+		     &BTRFS_I(inode)->runtime_flags)) {
 		start = 0;
 		end = LLONG_MAX;
 	}
@@ -2235,8 +2237,6 @@ int btrfs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 	}
 	trans->sync = true;
 
-	btrfs_init_log_ctx(&ctx, inode);
-
 	ret = btrfs_log_dentry_safe(trans, root, dentry, start, end, &ctx);
 	if (ret < 0) {
 		/* Fallthrough and commit/free transaction. */
@@ -2295,6 +2295,7 @@ int btrfs_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		ret = btrfs_end_transaction(trans);
 	}
 out:
+	ASSERT(list_empty(&ctx.list));
 	err = file_check_and_advance_wb_err(file);
 	if (!ret)
 		ret = err;
