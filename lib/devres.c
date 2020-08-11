1eb592ffc535c587024eeca063219b840dcddc6b
Takashi Iwai <tiwai@suse.de>
2020-08-11 10:13:12 +0200

    devres: keep both device name and resource name in pretty name (git-fixes).
    suse-commit: 1ac0a1d1d662e8dc0d011166faa668e2a25ade0b

--- lib/devres.c
+++ lib/devres.c
@@ -135,6 +135,7 @@ void __iomem *devm_ioremap_resource(struct device *dev, struct resource *res)
 {
 	resource_size_t size;
 	void __iomem *dest_ptr;
+	char *pretty_name;
 
 	BUG_ON(!dev);
 
@@ -145,7 +146,15 @@ void __iomem *devm_ioremap_resource(struct device *dev, struct resource *res)
 
 	size = resource_size(res);
 
-	if (!devm_request_mem_region(dev, res->start, size, dev_name(dev))) {
+	if (res->name)
+		pretty_name = devm_kasprintf(dev, GFP_KERNEL, "%s %s",
+					     dev_name(dev), res->name);
+	else
+		pretty_name = devm_kstrdup(dev, dev_name(dev), GFP_KERNEL);
+	if (!pretty_name)
+		return IOMEM_ERR_PTR(-ENOMEM);
+
+	if (!devm_request_mem_region(dev, res->start, size, pretty_name)) {
 		dev_err(dev, "can't request region for resource %pR\n", res);
 		return IOMEM_ERR_PTR(-EBUSY);
 	}
