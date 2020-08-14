4d449ed3a991218f766716731515ad2527ce5580
Hyunae Park <whitebbb@telechips.com>
2020-08-14 10:23:47 +0900

    [upg] Remove unused ump driver and fix problem with OSS License for viviante driver
    Change-Id: Id6d243940fbbb0878265b0cf25bc7113f27a95cf

--- drivers/gpu/vivante/bare_metal/v2d_cmd.h
+++ drivers/gpu/vivante/bare_metal/v2d_cmd.h
@@ -1,3 +1,19 @@
+/*
+ * drivers/gpu/vivante/bare_metal/v2d_cmd.h
+ *
+ * Vivante 2D Rotation driver with Bare-Metal case
+ *
+ * Copyright (C) 2009 Telechips, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #include "v2d_rotation_api.h"
 
