b691fef12e11c392242c68693f8078783f8b6a79
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2020-08-19 14:31:34 +0200

    - linux/dim: Move implementation to .c files (bsc#1174852). - Update config files.
    suse-commit: 6ecfc7b5e888d2246ff65a003070330876c853f1

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -25,7 +25,7 @@
 #include <net/dst_metadata.h>
 #include <net/switchdev.h>
 #include <net/xdp.h>
-#include <linux/net_dim.h>
+#include <linux/dim.h>
 
 struct tx_bd {
 	__le32 tx_bd_len_flags_type;
