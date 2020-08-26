b79ba12db2260deb1b275cb5f155b7bfdb8b9a4c
Akshu Agrawal <akshu.agrawal@amd.com>
2020-08-27 00:24:20 +0530

    ASoC: AMD: Clean kernel log from deferred probe error messages
    BugLink: https://bugs.launchpad.net/bugs/1908150
    
    [ Upstream commit f7660445c8e7fda91e8b944128554249d886b1d4 ]
    
    While the driver waits for DAIs to be probed and retries probing,
    have the error messages at debug level instead of error.
    
    Signed-off-by: Akshu Agrawal <akshu.agrawal@amd.com>
    Link: https://lore.kernel.org/r/20200826185454.5545-1-akshu.agrawal@amd.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Ian May <ian.may@canonical.com>

--- sound/soc/amd/acp3x-rt5682-max9836.c
+++ sound/soc/amd/acp3x-rt5682-max9836.c
@@ -346,12 +346,17 @@ static int acp3x_probe(struct platform_device *pdev)
 
 	ret = devm_snd_soc_register_card(&pdev->dev, &acp3x_card);
 	if (ret) {
-		dev_err(&pdev->dev,
+		if (ret != -EPROBE_DEFER)
+			dev_err(&pdev->dev,
 				"devm_snd_soc_register_card(%s) failed: %d\n",
 				acp3x_card.name, ret);
-		return ret;
+		else
+			dev_dbg(&pdev->dev,
+				"devm_snd_soc_register_card(%s) probe deferred: %d\n",
+				card->name, ret);
 	}
-	return 0;
+
+	return ret;
 }
 
 static const struct acpi_device_id acp3x_audio_acpi_match[] = {
