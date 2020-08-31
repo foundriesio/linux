77d72ac171defe0f117ea23a016ecdab26cc5004
Michal Kubecek <mkubecek@suse.cz>
2020-08-31 23:16:31 +0200

    kabi: hide new parameter of ip6_dst_lookup_flow() (bsc#1165629).
    suse-commit: a7cf2e2732f9981cd91ae7b04eb694ddf194dbb0

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1072,7 +1072,7 @@ int ip6_dst_lookup(struct net *net, struct sock *sk, struct dst_entry **dst,
 EXPORT_SYMBOL_GPL(ip6_dst_lookup);
 
 /**
- *	ip6_dst_lookup_flow - perform route lookup on flow with ipsec
+ *	ip6_dst_lookup_flow__net - perform route lookup on flow with ipsec
  *	@sk: socket which provides route info
  *	@fl6: flow to lookup
  *	@final_dst: final destination address for ipsec lookup
@@ -1082,8 +1082,8 @@ EXPORT_SYMBOL_GPL(ip6_dst_lookup);
  *	It returns a valid dst pointer on success, or a pointer encoded
  *	error code.
  */
-struct dst_entry *ip6_dst_lookup_flow(struct net *net, const struct sock *sk, struct flowi6 *fl6,
-				      const struct in6_addr *final_dst)
+struct dst_entry *ip6_dst_lookup_flow__net(struct net *net, const struct sock *sk, struct flowi6 *fl6,
+					   const struct in6_addr *final_dst)
 {
 	struct dst_entry *dst = NULL;
 	int err;
@@ -1095,6 +1095,15 @@ struct dst_entry *ip6_dst_lookup_flow(struct net *net, const struct sock *sk, st
 		fl6->daddr = *final_dst;
 
 	return xfrm_lookup_route(net, dst, flowi6_to_flowi(fl6), sk, 0);
+}
+EXPORT_SYMBOL_GPL(ip6_dst_lookup_flow__net);
+
+
+/* We need to preserve this one for kABI. */
+struct dst_entry *ip6_dst_lookup_flow(const struct sock *sk, struct flowi6 *fl6,
+				      const struct in6_addr *final_dst)
+{
+	return ip6_dst_lookup_flow__net(sock_net(sk), sk, fl6, final_dst);
 }
 EXPORT_SYMBOL_GPL(ip6_dst_lookup_flow);
 
@@ -1119,7 +1128,8 @@ struct dst_entry *ip6_sk_dst_lookup_flow(struct sock *sk, struct flowi6 *fl6,
 
 	dst = ip6_sk_dst_check(sk, dst, fl6);
 	if (!dst)
-		dst = ip6_dst_lookup_flow(sock_net(sk), sk, fl6, final_dst);
+		dst = ip6_dst_lookup_flow__net(sock_net(sk), sk, fl6,
+					       final_dst);
 
 	return dst;
 }
