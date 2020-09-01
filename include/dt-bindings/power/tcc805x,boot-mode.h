46a50b5bc3ac86b73866448eb55bed4a8f27c2e8
김용수B (Albert Kim) <kimys@telechips.com>
2020-09-01 12:15:47 +0900

    [upg] power: reset: tcc805x: change reboot reason codes
    In order to support reboot reasons for main/sub cores, we change the reason
    codes to 7 bits.
    
    ITS: TMBSP-2315
    
    Linked commit(s)
      - 5dbfcc3 [upg] reset: tcc: isolate reboot reason for main/sub cores @ trusted-firmware-arm
      - 1ebb804 [upg] tcc805x: update BL1/BL2 firmwares (5dbfcc3) @ boot-firmware
      - c81abd4 [upg] tcc805x: change reboot reason codes @ u-boot
    
    Test: reboot and check reboot reason
    Change-Id: If29fa6f80516aa74e20e8d9c02ed922b8a05d95a

---
+++ include/dt-bindings/power/tcc805x,boot-mode.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (C) 2020 Telechips Inc.
+ */
+
+#ifndef __DT_BINDINGS_TCC805X_BOOT_MODE_H
+#define __DT_BINDINGS_TCC805X_BOOT_MODE_H
+
+#define BOOT_NORMAL	0x51
+#define BOOT_RECOVERY	0x52
+#define BOOT_FASTBOOT	0x50
+#define BOOT_TCUPDATE	0x53
+
+#endif
+
