edd27180a9a8dd7b329bbca1492e4e81734b9740
gwansug_jang <gwansug_jang@telechips.com>
2020-09-04 16:09:39 +0900

    [Update] [TCS][TMBSP-2265] Remove Printk and Compile Warning. [Description] 1. Dynamic Debug Verified. 2. CP3.0 and CP2.0 Verified.

--- drivers/char/tcc_cp.c
+++ drivers/char/tcc_cp.c
@@ -39,73 +39,71 @@
 #include <linux/uaccess.h>
 #include <mach/tcccp_ioctl.h>
 
-#define CP_DEV_NAME			"tcc-cp"
+#define TCC_CP_DEV_NAME			        "tcc-cp"
+#define TCC_CP_TYPE_20B                 0
+#define TCC_CP_TYPE_20C                 1
+#define TCC_CP_TYPE_030                 2
+#define TCC_CP_NOERR                    0
+#define TCC_CP_GPIO_HI			        1
+#define TCC_CP_GPIO_LOW			        0
+#define TCC_CP_STATE_ENABLE	        	1
+#define TCC_CP_STATE_DISABLE            0
+#define TCC_CP_MATCH_SIZE               2
 
-#define CP_GPIO_HI			1
-#define CP_GPIO_LOW			0
+static int gCPPowerState =              0;
+static int gCPPowerGpio =               0;
+static int gCPResetGpio =               0;
+static unsigned int gCpVersion =        0;
+static unsigned int gCpI2cChannel =     0;
+static unsigned int gCpState =          0;
+static struct class *tcc_cp_class =     NULL;
+static struct device *tcc_cp_dev =      NULL;
 
-#define CP_STATE_ENABLE	        		1
-#define CP_STATE_DISABLE                        0
-
-#define CP_DEBUG    0
-
-#if CP_DEBUG
-#define cp_dbg(fmt, arg...)     printk(fmt, ##arg)
-#else
-#define cp_dbg(arg...)
-#endif
-
-static struct class *tcc_cp_class;
-
-static int gCPMajorNum = 0;
-static int gCPPowerState = 0;
-static int gCPPowerGpio = 0;
-static int gCPResetGpio = 0;
-
-unsigned int gCpVersion = 0;
-unsigned int gCpI2cChannel = 0;
-static unsigned int gCpState = 0;
-
-
-int tcc_cp_gpio_init(void)
+void tcc_cp_gpio_init(struct device *dev)
 {
-	int err;
-    cp_dbg("%s  \n", __FUNCTION__);
+	int err = TCC_CP_NOERR;
+    dev_dbg(dev, "%s  \n", __FUNCTION__);
 
     if(gCPPowerState == 1)
     {
 		if(!gpio_is_valid(gCPPowerGpio)) {
-			cp_dbg("can't find dev of node: cp reset gpio\n");
-			return -ENODEV;
+			dev_err(dev, "can't find dev of node: cp reset gpio\n");
+			return;
 		}
 
 		err = gpio_request(gCPPowerGpio, "cp_power_gpio");
 		if(err) {
-			cp_dbg("can't request cp reset gpio\n");
-			return err;
+			dev_err(dev, "can't request cp reset gpio\n");
+			return;
 		}
 	    gpio_direction_output(gCPPowerGpio, 0);
     }
 
-	if(!gpio_is_valid(gCPResetGpio)) {
-		cp_dbg("can't find dev of node: cp reset gpio\n");
-		return -ENODEV;
-	}
-
-	err = gpio_request(gCPResetGpio, "cp_reset_gpio");
-	if(err) {
-		cp_dbg("can't request cp reset gpio\n");
-		return err;
-	}
-    gpio_direction_output(gCPResetGpio, 0);
-	return 0;
+    if (gpio_is_valid(gCPResetGpio))
+    {
+	    err = gpio_request(gCPResetGpio, "cp_reset_gpio");
+	    if(err) {
+	    	dev_err(dev, "can't request cp reset gpio [%d]\n", gCPResetGpio);
+	    	return;
+	    }
+        gpio_direction_output(gCPResetGpio, 0);
+    }
+    else
+    {
+        if (gCpVersion == 0x20B)
+		    dev_err(dev, "Invalid GPIO Set in Device Tree: Reset GPIO[%d]\n", gCPResetGpio);
+        else if (gCpVersion == 0x20C)
+            dev_warn(dev, "I2C Slave Address Will Not Be Changed: GPIO[%d]\n", gCPResetGpio); 
+        else
+            dev_dbg(dev, "No Need To Control Reset: GPIO[%d]\n", gCPResetGpio);
+    }
 }
 
-void tcc_cp_pwr_control(int value)
+void tcc_cp_pwr_control(struct device *dev, int value)
 {
     if(gCPPowerState == 1)
     {
-        cp_dbg("%s value : %d\n", __FUNCTION__, value);
+        dev_dbg(dev, "%s value : %d\n", __FUNCTION__, value);
 		if (gpio_cansleep(gCPPowerGpio))
 			gpio_set_value_cansleep(gCPPowerGpio, value);
 		else
@@ -113,11 +111,11 @@ void tcc_cp_pwr_control(int value)
     }
 }
 
-void tcc_cp_reset_control(int value)
+void tcc_cp_reset_control(struct device *dev, int value)
 {
 	if((gCPPowerState == 1) || (gCpVersion == 0x20B))
 	{
-		cp_dbg("%s value : %d\n", __FUNCTION__, value);
+		dev_dbg(dev, "%s value : %d\n", __FUNCTION__, value);
 		if (gpio_cansleep(gCPResetGpio))
 			gpio_set_value_cansleep(gCPResetGpio, value);
 		else
@@ -131,75 +129,77 @@ void tcc_cp_reset_control(int value)
 
 long tcc_cp_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-    cp_dbg("%s  (0x%x)  \n", __FUNCTION__, cmd);
+    struct device *dev = tcc_cp_dev;
+
+    dev_dbg(dev, "%s  (0x%x)  \n", __FUNCTION__, cmd);
 
     switch (cmd)
     {
         case IOCTL_CP_CTRL_INIT:
         {
-            tcc_cp_gpio_init();
-            cp_dbg("%s:  IOCTL_CP_SET_GPIO_INIT (0x%x) \n", __FUNCTION__, cmd);
+            tcc_cp_gpio_init(dev);
+            dev_dbg(dev, "%s:  IOCTL_CP_SET_GPIO_INIT (0x%x) \n", __FUNCTION__, cmd);
         }
             break;
         case IOCTL_CP_CTRL_PWR:
         {
             if(arg != 0)
-                tcc_cp_pwr_control(CP_GPIO_HI);
+                tcc_cp_pwr_control(dev, TCC_CP_GPIO_HI);
             else
-                tcc_cp_pwr_control(CP_GPIO_LOW);
-            cp_dbg("%s:  IOCTL_CP_CTRL_PWR arg(%lu) \n", __FUNCTION__, arg);
+                tcc_cp_pwr_control(dev, TCC_CP_GPIO_LOW);
+            dev_dbg(dev, "%s:  IOCTL_CP_CTRL_PWR arg(%lu) \n", __FUNCTION__, arg);
         }
             break;
         case IOCTL_CP_CTRL_RESET:
         {
             if(arg != 0)
-                tcc_cp_reset_control(CP_GPIO_HI);
+                tcc_cp_reset_control(dev, TCC_CP_GPIO_HI);
             else
-                tcc_cp_reset_control(CP_GPIO_LOW);
+                tcc_cp_reset_control(dev, TCC_CP_GPIO_LOW);
 
-            cp_dbg("%s:  IOCTL_CP_CTRL_RESET arg(%lu) \n", __FUNCTION__, arg);
+            dev_dbg(dev, "%s:  IOCTL_CP_CTRL_RESET arg(%lu) \n", __FUNCTION__, arg);
         }
             break;
         case IOCTL_CP_CTRL_ALL:
         {
             if(arg != 0){
-                tcc_cp_pwr_control(CP_GPIO_HI);
-                tcc_cp_reset_control(CP_GPIO_HI);
+                tcc_cp_pwr_control(dev, TCC_CP_GPIO_HI);
+                tcc_cp_reset_control(dev, TCC_CP_GPIO_HI);
             } else{
-                tcc_cp_pwr_control(CP_GPIO_LOW);
-                tcc_cp_reset_control(CP_GPIO_LOW);
+                tcc_cp_pwr_control(dev, TCC_CP_GPIO_LOW);
+                tcc_cp_reset_control(dev, TCC_CP_GPIO_LOW);
             }
 
-            cp_dbg("%s:  IOCTL_CP_CTRL_ALL arg(%lu) \n", __FUNCTION__, arg);
+            dev_dbg(dev, "%s:  IOCTL_CP_CTRL_ALL arg(%lu) \n", __FUNCTION__, arg);
         }
             break;
         case IOCTL_CP_GET_VERSION:
             if(copy_to_user((unsigned int*) arg, &gCpVersion, sizeof(unsigned int))!=0)
             {
-                printk(" %s copy version error~!\n",__func__);
+                dev_dbg(dev, " %s copy version error~!\n",__func__);
 			}
             break;
         case IOCTL_CP_GET_CHANNEL:
             if(copy_to_user((unsigned int*) arg, &gCpI2cChannel, sizeof(unsigned int))!=0)
             {
-                printk(" %s copy channel error~!\n",__func__);
+                dev_dbg(dev, " %s copy channel error~!\n",__func__);
             }
             break;
         case IOCTL_CP_SET_STATE:
             if(arg != 0){
-                gCpState = CP_STATE_ENABLE;
+                gCpState = TCC_CP_STATE_ENABLE;
             }else{
-                gCpState = CP_STATE_DISABLE;
+                gCpState = TCC_CP_STATE_DISABLE;
             }
             break;
         case IOCTL_CP_GET_STATE:
             if(copy_to_user((unsigned int*) arg, &gCpState, sizeof(unsigned int))!=0)
             {
-                printk(" %s copy state error~!\n",__func__);
+                dev_dbg(dev, " %s copy state error~!\n",__func__);
             }
             break;
         default:
-            cp_dbg("cp: unrecognized ioctl (0x%x)\n", cmd);
+            dev_dbg(dev, "cp: unrecognized ioctl (0x%x)\n", cmd);
             return -EINVAL;
             break;
     }
@@ -208,15 +208,17 @@ long tcc_cp_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 static int tcc_cp_release(struct inode *inode, struct file *filp)
 {
-    cp_dbg("%s \n", __FUNCTION__);
+    struct device *dev = tcc_cp_dev; 
 
+    dev_dbg(dev, "%s \n", __FUNCTION__);
     return 0;
 }
 
 static int tcc_cp_open(struct inode *inode, struct file *filp)
 {
-    cp_dbg("%s : \n", __FUNCTION__);
+    struct device *dev = tcc_cp_dev;
 
+    dev_dbg(dev, "%s : \n", __FUNCTION__);
     return 0;
 }
 
@@ -228,88 +230,113 @@ struct file_operations tcc_cp_fops =
     .unlocked_ioctl = tcc_cp_ioctl,
 };
 
+#ifdef CONFIG_OF
+static void tcc_cp_read_device_tree(struct device *dev)
+{
+    unsigned int temp = 0;
+
+	if (of_find_property(dev->of_node, "cp-type", 0)) 
+    {
+		of_property_read_u32(dev->of_node, "cp-type", &temp);
+		/* Auth CP type 2 = 3.0, Auth CP type 1 = 2.0C, Auth CP type 0 = 2.0B */
+        switch (temp)
+        {
+            case TCC_CP_TYPE_20B:
+			    gCpVersion = 0x20B;
+                break;
+            case TCC_CP_TYPE_20C:
+			    gCpVersion = 0x20C;
+                break;
+            case TCC_CP_TYPE_030:
+			    gCpVersion = 0x030;
+                break;
+            default:
+			    dev_err(dev, "Invalid Auth CP Type Read [%u]\n", temp);
+        }
+
+	    dev_dbg(dev, "Auth CP Type [0x%04X]\n", gCpVersion);
+	} 
+    else 
+    {
+		gCpVersion = 0x030;	// default 3.0 type
+		dev_err(dev, "Force To Set 3.0 Type\n");
+	}
+
+	if (of_find_property(dev->of_node, "i2c-channel", 0)) 
+    {
+		of_property_read_u32(dev->of_node, "i2c-channel", &gCpI2cChannel);
+		dev_dbg(dev, "cp i2c channel (%d)\n", gCpI2cChannel);
+	} 
+    else 
+    {
+	    gCpI2cChannel = 0;	// default 0 channel
+	    dev_err(dev, "Force To Set i2c channel (%d)\n", gCpI2cChannel);
+	}
+
+    if (of_find_property(dev->of_node, "power-ctrl-able", 0)) 
+    {
+        gCPPowerState = 1;
+        dev_dbg(dev, "Power Controlable Enabled\n");
+    }
+    else 
+    {
+        gCPPowerState = 0;
+        dev_dbg(dev, "Power Controlable Disabled\n");
+    }
+
+	gCPPowerGpio = of_get_named_gpio(dev->of_node, "cp_power-gpio", 0);
+    gCPResetGpio = of_get_named_gpio(dev->of_node, "cp_reset-gpio", 0);
+    dev_dbg(dev, "Power GIPO[%u] Reset GPIO[%u]\n", gCPPowerGpio, gCPResetGpio);
+
+
+}
+#endif
+
 /*****************************************************************************
  * TCC_CP_CTRL Probe/Remove
  ******************************************************************************/
 static int tcc_cp_ctrl_probe(struct platform_device *pdev)
 {
-	int res;
-    unsigned int temp;
+	int res = TCC_CP_NOERR;
 	struct device *dev = &pdev->dev;
 
-    cp_dbg("%s \n", __FUNCTION__);
+    dev_dbg(dev, "%s pdev[%p] dev[%p]\n", __FUNCTION__, pdev, dev);
 
-	res = register_chrdev(CP_DEV_MAJOR_NUM, CP_DEV_NAME, &tcc_cp_fops);
-    cp_dbg("%s: register_chrdev ret : %d\n", __func__, res);
-	if (res < 0)
-		return res;
-
-	gCPMajorNum = CP_DEV_MAJOR_NUM;
-	tcc_cp_class = class_create(THIS_MODULE, CP_DEV_NAME);
-	if(NULL == device_create(tcc_cp_class, NULL, MKDEV(gCPMajorNum, CP_DEV_MINOR_NUM), NULL, CP_DEV_NAME))
-		cp_dbg("%s device_create failed\n", __FUNCTION__);
-
+	if((res = register_chrdev(CP_DEV_MAJOR_NUM, TCC_CP_DEV_NAME, &tcc_cp_fops)) >= TCC_CP_NOERR)
+    {
+	    if ((tcc_cp_class = class_create(THIS_MODULE, TCC_CP_DEV_NAME)) != NULL)
+        {
+        	if((tcc_cp_dev = device_create(tcc_cp_class, NULL, MKDEV(CP_DEV_MAJOR_NUM, CP_DEV_MINOR_NUM), NULL, TCC_CP_DEV_NAME)) != NULL)
+            {
 #ifdef CONFIG_OF
-    if (of_find_property(pdev->dev.of_node, "power-ctrl-able", 0)) {
-        gCPPowerState = 1;
-    }
-    else {
-        gCPPowerState = 0;
-    }
-	gCPPowerGpio = of_get_named_gpio(dev->of_node, "cp_power-gpio", 0);
-    gCPResetGpio = of_get_named_gpio(dev->of_node, "cp_reset-gpio", 0);
-
-	if (of_find_property(dev->of_node, "cp-type", 0)) {
-		of_property_read_u32(dev->of_node, "cp-type", &temp);
-
-		/*
-        * Auth CP type 2 = 3.0
-        * Auth CP type 1 = 2.0C
-		* Auth CP type 0 = 2.0B
-		*/
-        if (temp == 2)
-        {
-			cp_dbg("3.0 type\n");
-			gCpVersion = 0x30;
-        }
-		else if (temp == 1) 
-        {
-			cp_dbg("2.0C type\n");
-			gCpVersion = 0x20C;
-		}
-		else if (temp == 0) 
-        {
-			cp_dbg("2.0B type\n");
-			gCpVersion = 0x20B;
-		}
-		else
-			cp_dbg("bad Auth CP type!\n");
-	} else {
-		gCpVersion = 0x30;	// default 3.0 type
-		cp_dbg("default 3.0 type\n");
-	}
-
-	if (of_find_property(dev->of_node, "i2c-channel", 0)) {
-		of_property_read_u32(dev->of_node, "i2c-channel", &gCpI2cChannel);
-		cp_dbg("cp i2c channel (%d)\n", gCpI2cChannel);
-	} else {
-	    gCpI2cChannel = 0;	// default 0 channel
-	    cp_dbg("default cp i2c channel (%d)\n", gCpI2cChannel);
-	}
+                tcc_cp_read_device_tree(dev);
 #endif
-
-    tcc_cp_gpio_init();
+                tcc_cp_gpio_init(dev);
+            }
+            else
+            {
+	        	dev_err(dev, "%s device_create failed\n", __FUNCTION__);
+            }
+        }
+        else
+        {
+            dev_err(dev, "%s class_create failed\n", __FUNCTION__);
+        }
+    }
+    else
+    {
+        dev_err(dev, "%s register_chrdev failed res[%d]\n", __FUNCTION__, res);
+    }
 
 	return 0;
 }
 
 static int tcc_cp_ctrl_remove(struct platform_device *pdev)
 {
-    cp_dbg("%s\n", __FUNCTION__);
-
-	device_destroy(tcc_cp_class, MKDEV(gCPMajorNum, CP_DEV_MINOR_NUM));
+    dev_dbg(&pdev->dev, "%s\n", __FUNCTION__);
+	device_destroy(tcc_cp_class, MKDEV(CP_DEV_MAJOR_NUM, CP_DEV_MINOR_NUM));
 	class_destroy(tcc_cp_class);
-	unregister_chrdev(gCPMajorNum, CP_DEV_NAME);
+	unregister_chrdev(CP_DEV_MAJOR_NUM, TCC_CP_DEV_NAME);
 
 	return 0;
 }
@@ -318,18 +345,21 @@ static int tcc_cp_ctrl_remove(struct platform_device *pdev)
  * TCC_CP_CTRL Module Init/Exit
  ******************************************************************************/
 #ifdef CONFIG_OF
-static const struct of_device_id tcc_cp_ctrl_of_match[] = {
+static const struct of_device_id tcc_cp_ctrl_of_match[TCC_CP_MATCH_SIZE] = 
+{
 	{.compatible = "telechips, tcc-cp", },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, tcc_cp_ctrl_of_match);
 #endif
 
-static struct platform_driver tcc_cp_ctrl = {
+static struct platform_driver tcc_cp_ctrl = 
+{
 	.probe	= tcc_cp_ctrl_probe,
 	.remove	= tcc_cp_ctrl_remove,
-	.driver	= {
-		.name	= CP_DEV_NAME,
+	.driver	= 
+    {
+		.name	= TCC_CP_DEV_NAME,
 		.owner	= THIS_MODULE,
 #ifdef CONFIG_OF
 		.of_match_table = tcc_cp_ctrl_of_match,
