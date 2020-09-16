73907b0067a58697261e3e60d76a2aeb5c8f87ad
Marcos Paulo de Souza <mpdesouza@suse.com>
2020-09-16 15:50:51 +0200

    btrfs: relocation: allow signal to cancel balance (bsc#1174354).
    suse-commit: dd922932d13be899e45197e2a03fa8a655f5bcc8

--- fs/btrfs/relocation.c
+++ fs/btrfs/relocation.c
@@ -3322,7 +3322,8 @@ int setup_extent_mapping(struct inode *inode, u64 start, u64 end,
  */
 int btrfs_should_cancel_balance(struct btrfs_fs_info *fs_info)
 {
-	return atomic_read(&fs_info->balance_cancel_req);
+	return atomic_read(&fs_info->balance_cancel_req) ||
+		fatal_signal_pending(current);
 }
 
 static int relocate_file_extent_cluster(struct inode *inode,
