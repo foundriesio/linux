3a75d4d6ab16267fa1b86e426b357bab21c1eea1
한동열 (DongYeol Han) <hdy@telechips.com>
2020-09-18 15:11:20 +0900

    [fea] Use iommu(smmu) to access I2C controller 7 on the ISP
    The ISP can not set upper 16bit of APB address on TCC805x ES.
    So, upper 16bit is set to zero.
    
    By using iommu(smmu), upper 16bit can be translated
    to the 0x1637(I2C controller 7 base address).
    
    Linked Commit: N/A
    ITS: CV8050C-618
    
    Change-Id: Iec4ce6e8b2f991e3e6956ca10c4c8b473de7e18a

---
+++ drivers/media/platform/tcc-isp/tcc-isp-iommu.c
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) Telechips Inc.
+ */
+
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of_address.h>
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/platform_device.h>
+#include <linux/iommu.h>
+
+static struct iommu_domain * domain;
+
+static int tcc_isp_iommu_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	int ret = 0;
+
+	domain = iommu_get_domain_for_dev(dev);
+	if (!domain) {
+		ret = PTR_ERR(domain);
+		pr_err("[ERR][tcc-isp-iommu] FAIL iommu_get_domain_for_dev \n");
+		goto END;
+	}
+
+	/*
+	 * Add mapping for IOVA xxxx => PA 0x1637xxxx.
+	 */
+	ret = iommu_map(domain, 0, 0x16370000, \
+		0x10000, IOMMU_READ | IOMMU_WRITE);
+	if (ret) {
+		pr_err("[ERR][tcc-isp-iommu] Cannot add IOMMU mapping \n"); 
+		goto END;
+	}
+
+	pr_info("[INFO][tcc-isp-iommu] success setting iommu for isp \n");
+END:
+	return ret;
+}
+
+int tcc_isp_iommu_remove(struct platform_device *pdev)
+{
+	int ret = 0;
+
+	iommu_unmap(domain, 0, 0x10000);
+
+END:
+	return ret;
+}
+
+static struct of_device_id tcc_isp_iommu_of_match[] = {
+	{
+		.compatible = "telechips,tcc-isp-iommu",
+	},
+};
+MODULE_DEVICE_TABLE(of,tcc_isp_iommu_of_match);
+
+
+static struct platform_driver tcc_isp_iommu_driver = {
+	.probe      = tcc_isp_iommu_probe,
+	.remove     = tcc_isp_iommu_remove,
+	.driver = {
+		.name	= "tcc_isp_iommu",
+		.owner	= THIS_MODULE,
+		.of_match_table = of_match_ptr(tcc_isp_iommu_of_match),
+	},
+};
+module_platform_driver(tcc_isp_iommu_driver);
+
+MODULE_AUTHOR("Telechips <www.telechips.com>");
+MODULE_DESCRIPTION("Telechips TCCXXXX SoC ISP driver");
+MODULE_LICENSE("GPL");
