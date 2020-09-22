34eed5259cbbaa7966a4f99df77e04c347c81bcc
JongWoo Choi <peter.choi@telechips.com>
2020-09-22 21:00:57 +0900

    [ref] Fix QAC and Codesonar warnings
    Fix Codesonar warnings (Rulset1)
    
    Linked Commit:
    TMBSP-2337
    
    Change-Id: I4425543bd0fb18923b365905496bdc3feb939a9c

--- drivers/mmc/host/sdhci-tcc.h
+++ drivers/mmc/host/sdhci-tcc.h
@@ -3,8 +3,8 @@
  * Copyright (C) Telechips Inc.
  */
 
-#ifndef _SDHCI_TCC_
-#define _SDHCI_TCC_
+#ifndef SDHCI_TCC
+#define SDHCI_TCC
 
 #include <asm/system_info.h>
 #include <linux/mmc/host.h>
@@ -12,113 +12,113 @@
 #include "sdhci-pltfm.h"
 
 /* Telechips SDHC Specific Registers for TCC897x */
-#define TCC897X_SDHC_HOSTCFG			0x00
-#define TCC897X_SDHC_HOSTCFG_DISC(x)	(0x1 << (20 + x))
-#define TCC897X_SDHC_ODELAY_OFFSET		0x08
-#define TCC897X_SDHC_ODELAY(x)			(TCC897X_SDHC_ODELAY_OFFSET + (x * 0x4))
-#define TCC897X_SDHC_CAPREG_OFFSET		0x18
-#define TCC897X_SDHC_CAPREG0(x)			(TCC897X_SDHC_CAPREG_OFFSET + (x * 0x18))
-#define TCC897X_SDHC_CAPREG1(x)			(TCC897X_SDHC_CAPREG0(x) + 0x4)
-#define TCC897X_SDHC_DELAY_OFFSET		0x78
-#define TCC897X_SDHC_DELAY_CON(x)		(TCC897X_SDHC_DELAY_OFFSET + (x * 0x2))
+#define TCC897X_SDHC_HOSTCFG			0x00u
+#define TCC897X_SDHC_HOSTCFG_DISC(x)	((unsigned int)1 << (20u + (x)))
+#define TCC897X_SDHC_ODELAY_OFFSET		0x08u
+#define TCC897X_SDHC_ODELAY(x)			(TCC897X_SDHC_ODELAY_OFFSET + ((x) * 0x4u))
+#define TCC897X_SDHC_CAPREG_OFFSET		0x18u
+#define TCC897X_SDHC_CAPREG0(x)			(TCC897X_SDHC_CAPREG_OFFSET + ((x) * 0x18u))
+#define TCC897X_SDHC_CAPREG1(x)			(TCC897X_SDHC_CAPREG0(x) + 0x4u)
+#define TCC897X_SDHC_DELAY_OFFSET		0x78u
+#define TCC897X_SDHC_DELAY_CON(x)		(TCC897X_SDHC_DELAY_OFFSET + ((x) * 0x2u))
 
-#define TCC897X_SDHC_DAT_DELAY_OUT(x)	((x & 0xF) << 0)
-#define TCC897X_SDHC_DAT_DELAY_EN(x)	((x & 0xF) << 8)
-#define TCC897X_SDHC_CMD_DELAY_OUT(x)	((x & 0xF) << 16)
-#define TCC897X_SDHC_CMD_DELAY_EN(x)	((x & 0xF) << 24)
+#define TCC897X_SDHC_DAT_DELAY_OUT(x)	(((x) & 0xFu) << 0u)
+#define TCC897X_SDHC_DAT_DELAY_EN(x)	(((x) & 0xFu) << 8u)
+#define TCC897X_SDHC_CMD_DELAY_OUT(x)	(((x) & 0xFu) << 16u)
+#define TCC897X_SDHC_CMD_DELAY_EN(x)	(((x) & 0xFu) << 24u)
 
 #define TCC897X_SDHC_MK_ODEALY(cmd, data)	(TCC897X_SDHC_DAT_DELAY_OUT(data) \
 	| TCC897X_SDHC_DAT_DELAY_EN(data) \
 	| TCC897X_SDHC_CMD_DELAY_OUT(cmd) \
 	| TCC897X_SDHC_CMD_DELAY_EN(cmd) )
 
-#define TCC897X_SDHC_IPTAP(x)			((x & 0x3F) << 0)
-#define TCC897X_SDHC_IPTAP_EN(x)		((x & 0x1) << 7)
-#define TCC897X_SDHC_TUNE_CNT(x)		((x & 0xF) << 8)
-#define TCC897X_SDHC_DELAY_CTRL(x)		((x & 0x3) << 12)
-#define TCC897X_SDHC_FBEN(x)			((x & 0x1) << 15)
+#define TCC897X_SDHC_IPTAP(x)			(((x) & 0x3Fu) << 0u)
+#define TCC897X_SDHC_IPTAP_EN(x)		(((x) & 0x1u) << 7u)
+#define TCC897X_SDHC_TUNE_CNT(x)		(((x) & 0xFu) << 8u)
+#define TCC897X_SDHC_DELAY_CTRL(x)		(((x) & 0x3u) << 12u)
+#define TCC897X_SDHC_FBEN(x)			(((x) & 0x1u) << 15u)
 
-#define TCC897X_SDHC_DELAY_CON_DEF		(TCC897X_SDHC_IPTAP(0) \
-	| TCC897X_SDHC_IPTAP_EN(0) \
-	| TCC897X_SDHC_TUNE_CNT(16) \
-	| TCC897X_SDHC_TUNE_CNT(0)	\
-	| TCC897X_SDHC_FBEN(0))
+#define TCC897X_SDHC_DELAY_CON_DEF		(TCC897X_SDHC_IPTAP(0u) \
+	| TCC897X_SDHC_IPTAP_EN(0u) \
+	| TCC897X_SDHC_TUNE_CNT(16u) \
+	| TCC897X_SDHC_TUNE_CNT(0u)	\
+	| TCC897X_SDHC_FBEN(0u))
 
 /* Telechips SDHC Specific Registers for TCC803x rev. 1*/
-#define TCC_SDHC_CORE_CLK_REG0			(0x100)
-#define TCC_SDHC_CORE_CLK_REG1			(0x104)
-#define TCC_SDHC_CORE_CLK_REG2			(0x108)
+#define TCC_SDHC_CORE_CLK_REG0			(0x100u)
+#define TCC_SDHC_CORE_CLK_REG1			(0x104u)
+#define TCC_SDHC_CORE_CLK_REG2			(0x108u)
 
-#define TCC_SDHC_CORE_CLK_DIV_EN(x)			((x & 0x1) << 0)
-#define TCC_SDHC_CORE_CLK_CLK_SEL(x)		((x & 0x1) << 1)
-#define TCC_SDHC_CORE_CLK_DIV_VAL_OFFSET	(8)
-#define TCC_SDHC_CORE_CLK_DIV_VAL(x)		((x & 0xFF) << TCC_SDHC_CORE_CLK_DIV_VAL_OFFSET)
+#define TCC_SDHC_CORE_CLK_DIV_EN(x)			(((x) & 0x1u) << 0u)
+#define TCC_SDHC_CORE_CLK_CLK_SEL(x)		(((x) & 0x1u) << 1u)
+#define TCC_SDHC_CORE_CLK_DIV_VAL_OFFSET	(8u)
+#define TCC_SDHC_CORE_CLK_DIV_VAL(x)		(((x) & 0xFFu) << TCC_SDHC_CORE_CLK_DIV_VAL_OFFSET)
 
-#define TCC_SDHC_CORE_CLK_MASK_EN(x)	((x & 0x1) << 0)
-#define TCC_SDHC_CORE_CLK_GATE_DIS(x)	((x & 0x1) << 16)
+#define TCC_SDHC_CORE_CLK_MASK_EN(x)	(((x) & 0x1u) << 0u)
+#define TCC_SDHC_CORE_CLK_GATE_DIS(x)	(((x) & 0x1u) << 16u)
 
-#define TCC_SDHC_DQS_POS_DETECT_DLY(x)	((x & 0xF) << 4)
-#define TCC_SDHC_DQS_NEG_DETECT_DLY(x)	((x & 0xF) << 20)
+#define TCC_SDHC_DQS_POS_DETECT_DLY(x)	(((x) & 0xFu) << 4u)
+#define TCC_SDHC_DQS_NEG_DETECT_DLY(x)	(((x) & 0xFu) << 20u)
 
-#define TCC_SDHC_SD_DQS_DLY		(0x114)
+#define TCC_SDHC_SD_DQS_DLY		(0x114u)
 
-#define TCC_SDHC_TX_CLKDLY_OFFSET(ch)		(0x10C - (ch * 0x50) + ((ch/2) * 0x4))
-#define TCC_SDHC_RX_CLKDLY_VAL_OFFSET(ch)	(0x128 - (ch * 0x48))
+#define TCC_SDHC_TX_CLKDLY_OFFSET(ch)		(0x10Cu - ((ch) * 0x50u) + (((ch)/2u) * 0x4u))
+#define TCC_SDHC_RX_CLKDLY_VAL_OFFSET(ch)	(0x128u - ((ch) * 0x48u))
 /* (0x128 - (ch * 0x50) + (ch * 0x8)) */
-#define TCC_SDHC_TAPDLY_OFFSET(ch)		(0x12C - (ch * 0x2C))
+#define TCC_SDHC_TAPDLY_OFFSET(ch)		(0x12Cu - ((ch) * 0x2Cu))
 
 #define TCC_SDHC_CMDDLY(ch)			TCC_SDHC_TAPDLY_OFFSET(ch)
-#define TCC_SDHC_DATADLY(ch, x)		TCC_SDHC_TAPDLY_OFFSET(ch) + (0x4 + (x * 0x4))
+#define TCC_SDHC_DATADLY(ch, x)		TCC_SDHC_TAPDLY_OFFSET(ch) + (0x4u + ((x) * 0x4u))
 
-#define TCC_SDHC_TAPDLY_IN(x)	((x & 0x1F) << 0)
-#define TCC_SDHC_TAPDLY_OUT(x)	((x & 0x1F) << 8)
-#define TCC_SDHC_TAPDLY_OEN(x)	((x & 0x1F) << 16)
+#define TCC_SDHC_TAPDLY_IN(x)	(((x) & 0x1Fu) << 0u)
+#define TCC_SDHC_TAPDLY_OUT(x)	(((x) & 0x1Fu) << 8u)
+#define TCC_SDHC_TAPDLY_OEN(x)	(((x) & 0x1Fu) << 16u)
 
-#define TCC_SDHC_MK_TX_CLKDLY(ch, x) (ch != 2 ? \
-	((x & 0x1F) << (ch * 16)) : \
-	(((x & 0x1E) << 16) | (x & 0x1)) )
-#define TCC_SDHC_MK_RX_CLKTA_VAL(x) ((x & 0x3) << 0)
+#define TCC_SDHC_MK_TX_CLKDLY(ch, x) (((ch) != (2u)) ? \
+	(((x) & 0x1Fu) << ((ch) * 16u)) : \
+	((((x) & 0x1Eu) << 16u) | ((x) & 0x1u)) )
+#define TCC_SDHC_MK_RX_CLKTA_VAL(x) (((x) & 0x3u) << 0u)
 #define TCC_SDHC_MK_TAPDLY(in, out)	(TCC_SDHC_TAPDLY_IN(in) \
 	| TCC_SDHC_TAPDLY_OUT(out) \
 	| TCC_SDHC_TAPDLY_OEN(out) )
 
-#define TCC_SDHC_CLKOUTDLY_DEF_TAP_V2	15
-#define TCC_SDHC_CMDDLY_DEF_TAP_V2		15
-#define TCC_SDHC_DATADLY_DEF_TAP_V2		15
-#define TCC_SDHC_CLK_TXDLY_DEF_TAP_V2	15
+#define TCC_SDHC_CLKOUTDLY_DEF_TAP_V2	15u
+#define TCC_SDHC_CMDDLY_DEF_TAP_V2		15u
+#define TCC_SDHC_DATADLY_DEF_TAP_V2		15u
+#define TCC_SDHC_CLK_TXDLY_DEF_TAP_V2	15u
 
 /* Telechips SDHC Specific Registers for others (such as TCC803x rev. 0, tcc899x, and so on)*/
 /* Specific registers of SDMMC registers */
-#define TCC_SDHC_VENDOR			0x78
+#define TCC_SDHC_VENDOR			0x78u
 /* Specific registers of Channel Control registers */
-#define TCC_SDHC_TAPDLY			0x00
-#define TCC_SDHC_CAPREG0		0x04
-#define TCC_SDHC_CAPREG1		0x08
-#define TCC_SDHC_DELAY_CON0		0x28
-#define TCC_SDHC_DELAY_CON1		0x2C
-#define TCC_SDHC_DELAY_CON2		0x30
-#define TCC_SDHC_DELAY_CON3		0x34
-#define TCC_SDHC_DELAY_CON4		0x38
-#define TCC_SDHC_CD_WP			0x4C
+#define TCC_SDHC_TAPDLY			0x00u
+#define TCC_SDHC_CAPREG0		0x04u
+#define TCC_SDHC_CAPREG1		0x08u
+#define TCC_SDHC_DELAY_CON0		0x28u
+#define TCC_SDHC_DELAY_CON1		0x2Cu
+#define TCC_SDHC_DELAY_CON2		0x30u
+#define TCC_SDHC_DELAY_CON3		0x34u
+#define TCC_SDHC_DELAY_CON4		0x38u
+#define TCC_SDHC_CD_WP			0x4Cu
 
-#define TCC_SDHC_TAPDLY_TUNE_CNT(x)		((x & 0x3F) << 16)
-#define TCC_SDHC_TAPDLY_ITAP_MAX		(32)
-#define TCC_SDHC_TAPDLY_ITAP_SEL(x)		((x & 0x1F) << 0)
-#define TCC_SDHC_TAPDLY_ITAP_SEL_MASK	(TCC_SDHC_TAPDLY_ITAP_SEL(0x1F))
-#define TCC_SDHC_TAPDLY_ITAP_EN(x)		((x & 0x1) << 5)
-#define TCC_SDHC_TAPDLY_ITAP_CHGWIN(x)	((x & 0x1) << 6)
-#define TCC_SDHC_TAPDLY_OTAP_SEL(x)		((x & 0x1F) << 8)
-#define TCC_SDHC_TAPDLY_OTAP_SEL_MASK	(TCC_SDHC_TAPDLY_OTAP_SEL(0x1F))
-#define TCC_SDHC_TAPDLY_OTAP_EN(x)		((x & 0x1) << 13)
-#define TCC_SDHC_TAPDLY_ASYNCWKUP_EN(x)	((x & 0x1) << 14)
+#define TCC_SDHC_TAPDLY_TUNE_CNT(x)		(((x) & 0x3Fu) << 16u)
+#define TCC_SDHC_TAPDLY_ITAP_MAX		(32u)
+#define TCC_SDHC_TAPDLY_ITAP_SEL(x)		(((x) & 0x1Fu) << 0u)
+#define TCC_SDHC_TAPDLY_ITAP_SEL_MASK	(TCC_SDHC_TAPDLY_ITAP_SEL(0x1Fu))
+#define TCC_SDHC_TAPDLY_ITAP_EN(x)		(((x) & 0x1u) << 5u)
+#define TCC_SDHC_TAPDLY_ITAP_CHGWIN(x)	(((x) & 0x1u) << 6u)
+#define TCC_SDHC_TAPDLY_OTAP_SEL(x)		(((x) & 0x1Fu) << 8u)
+#define TCC_SDHC_TAPDLY_OTAP_SEL_MASK	(TCC_SDHC_TAPDLY_OTAP_SEL(0x1Fu))
+#define TCC_SDHC_TAPDLY_OTAP_EN(x)		(((x) & 0x1u) << 13u)
+#define TCC_SDHC_TAPDLY_ASYNCWKUP_EN(x)	(((x) & 0x1u) << 14u)
 
-#define TCC_SDHC_CMDDLY_IN(x)			((x & 0xF) << 16)
-#define TCC_SDHC_CMDDLY_OUT(x)			((x & 0xF) << 20)
-#define TCC_SDHC_CMDDLY_EN(x)			((x & 0xF) << 24)
+#define TCC_SDHC_CMDDLY_IN(x)			(((x) & 0xFu) << 16u)
+#define TCC_SDHC_CMDDLY_OUT(x)			(((x) & 0xFu) << 20u)
+#define TCC_SDHC_CMDDLY_EN(x)			(((x) & 0xFu) << 24u)
 
-#define TCC_SDHC_DATADLY_IN(n, x)		((x & 0xF) << ((n & 0x1) * 12))
-#define TCC_SDHC_DATADLY_OUT(n, x)		((x & 0xF) << (((n & 0x1) * 12) + 4))
-#define TCC_SDHC_DATADLY_EN(n, x)		((x & 0xF) << (((n & 0x1) * 12) + 8))
+#define TCC_SDHC_DATADLY_IN(n, x)		(((x) & 0xFu) << (((n) & 0x1u) * 12u))
+#define TCC_SDHC_DATADLY_OUT(n, x)		(((x) & 0xFu) << ((((n) & 0x1u) * 12u) + 4u))
+#define TCC_SDHC_DATADLY_EN(n, x)		(((x) & 0xFu) << ((((n) & 0x1u) * 12u) + 8u))
 
 #define TCC_SDHC_CAPARG0_DEF		0xEDFE9970
 #define TCC_SDHC_CAPARG1_DEF		0x00000007
@@ -126,30 +126,30 @@
 #define TCC_SDHC_CMDDLY_DEF_TAP		7
 #define TCC_SDHC_DATADLY_DEF_TAP	7
 
-#define TCC_SDHC_TAPDLY_DEF				(TCC_SDHC_TAPDLY_TUNE_CNT(16) \
-	| TCC_SDHC_TAPDLY_OTAP_SEL(0x8) \
-	| TCC_SDHC_TAPDLY_OTAP_EN(1) \
-	| TCC_SDHC_TAPDLY_ASYNCWKUP_EN(1) )
+#define TCC_SDHC_TAPDLY_DEF				(TCC_SDHC_TAPDLY_TUNE_CNT(16u) \
+	| TCC_SDHC_TAPDLY_OTAP_SEL(0x8u) \
+	| TCC_SDHC_TAPDLY_OTAP_EN(1u) \
+	| TCC_SDHC_TAPDLY_ASYNCWKUP_EN(1u) )
 
 #define TCC_SDHC_MK_CMDDLY(x)			(TCC_SDHC_CMDDLY_IN(x) \
 	| TCC_SDHC_CMDDLY_OUT(x) \
 	| TCC_SDHC_CMDDLY_EN(x) )
 
-#define TCC_SDHC_MK_DATADLY(x)				(TCC_SDHC_DATADLY_IN(0, x) \
-	| TCC_SDHC_DATADLY_OUT(0, x) \
-	| TCC_SDHC_DATADLY_EN(0, x) \
-	| TCC_SDHC_DATADLY_IN(1, x) \
-	| TCC_SDHC_DATADLY_OUT(1, x) \
-	| TCC_SDHC_DATADLY_EN(1, x) )
+#define TCC_SDHC_MK_DATADLY(x)				(TCC_SDHC_DATADLY_IN(0u, x) \
+	| TCC_SDHC_DATADLY_OUT(0u, x) \
+	| TCC_SDHC_DATADLY_EN(0u, x) \
+	| TCC_SDHC_DATADLY_IN(1u, x) \
+	| TCC_SDHC_DATADLY_OUT(1u, x) \
+	| TCC_SDHC_DATADLY_EN(1u, x) )
 #define TCC_SDHC_F_MIN		400000 /* in Hz */
 
-#define TCC_SDHC_AUTO_TUNE_EN(x)		(!!(x & 0x20))
-#define TCC_SDHC_AUTO_TUNE_RESULT(x)	(x & 0x1F)
+#define TCC_SDHC_AUTO_TUNE_EN(x)		((x) & 0x20u)
+#define TCC_SDHC_AUTO_TUNE_RESULT(x)	((x) & 0x1Fu)
 
 #define TCC_SDHC_FORCE_DETECT_DELAY            0
 
 #define SDHCI_TCC_VENDOR_REGISTER	(0x78)
-	#define VENDOR_ENHANCED_STROBE		(1 << 0)
+	#define VENDOR_ENHANCED_STROBE		(1u << 0u)
 
 struct sdhci_tcc_soc_data {
 	const struct sdhci_pltfm_data *pdata;
@@ -180,10 +180,10 @@ struct sdhci_tcc {
 	u64 force_caps;
 	u32 drive_strength;
 	u32 flags;
-#define TCC_SDHC_CLK_GATING (1 << 0) /* Enable Output SDCLK gating */
-	int controller_id;
+#define TCC_SDHC_CLK_GATING (1u << 0u) /* Enable Output SDCLK gating */
+	unsigned int controller_id;
 
-	int hw_reset;
+	u32 hw_reset;
 
 	struct dentry *tap_dly_dbgfs;
 	struct dentry *clk_dly_dbgfs;
