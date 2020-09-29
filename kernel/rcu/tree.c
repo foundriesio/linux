bb0c55534be6cc3c1c093fe7109c844c2ac5e300
Peter Zijlstra <peterz@infradead.org>
2020-09-29 13:33:40 +0200

    rcu,ftrace: Fix ftrace recursion
    BugLink: https://bugs.launchpad.net/bugs/1912027
    
    [ Upstream commit d2098b4440981705e844c50254540ba7b5f82795 ]
    
    Kim reported that perf-ftrace made his box unhappy. It turns out that
    commit:
    
      ff5c4f5cad33 ("rcu/tree: Mark the idle relevant functions noinstr")
    
    removed one too many notrace qualifiers, probably due to there not being
    a helpful comment.
    
    This commit therefore reinstates the notrace and adds a comment to avoid
    losing it again.
    
    [ paulmck: Apply Steven Rostedt's feedback on the comment. ]
    Fixes: ff5c4f5cad33 ("rcu/tree: Mark the idle relevant functions noinstr")
    Reported-by: Kim Phillips <kim.phillips@amd.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -1088,8 +1088,11 @@ noinstr bool __rcu_is_watching(void)
  * CPU can safely enter RCU read-side critical sections.  In other words,
  * if the current CPU is not in its idle loop or is in an interrupt or
  * NMI handler, return true.
+ *
+ * Make notrace because it can be called by the internal functions of
+ * ftrace, and making this notrace removes unnecessary recursion calls.
  */
-bool rcu_is_watching(void)
+notrace bool rcu_is_watching(void)
 {
 	bool ret;
 
