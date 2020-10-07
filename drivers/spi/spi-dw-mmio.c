5fa42b89f6530b3bf3a71a5bdd1ae08ac073806a
Gareth Williams <gareth.williams.jx@renesas.com>
2020-10-07 15:45:57 +0100

    spi: dw: return correct value on dma tranfer success.
    Changes applied in commit 6d15fe48f50af2027c3292827c65277285acb9dc
    upstream also need to be reflected in the RZ/N1 DMA support for
    Designware MMIO. Previously the return value from dma_transfer would not
    be returned by transfer_one unless an error occurs, instead
    transfer_one would return 1 on completion. Since this has changed
    upstream, we must return 1 from dma_transfer to continue returning
    the same value on success.
    
    Signed-off-by: Gareth Williams <gareth.williams.jx@renesas.com>

--- drivers/spi/spi-dw-mmio.c
+++ drivers/spi/spi-dw-mmio.c
@@ -358,7 +358,7 @@ static int mmio_spi_dma_transfer(struct dw_spi *dws, struct spi_transfer *xfer)
 		dma_async_issue_pending(dws->txchan);
 	}
 
-	return 0;
+	return 1;
 }
 
 static void mmio_spi_dma_stop(struct dw_spi *dws)
