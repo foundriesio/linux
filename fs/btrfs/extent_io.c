cba858e8f174cccef1594c1f10b5eeab855d497e
Filipe Manana <fdmanana@suse.com>
2020-10-15 11:28:00 +0100

    btrfs: fix race between page release and a fast fsync (bsc#1177687).
    suse-commit: c174bed6d1bdeda2ab96d2a5e1ca8d49b20e226e

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -4420,16 +4420,26 @@ int try_release_extent_mapping(struct extent_map_tree *map,
 				free_extent_map(em);
 				break;
 			}
-			if (!test_range_bit(tree, em->start,
-					    extent_map_end(em) - 1,
-					    EXTENT_LOCKED | EXTENT_WRITEBACK,
-					    0, NULL)) {
+			if (test_range_bit(tree, em->start,
+					   extent_map_end(em) - 1,
+					   EXTENT_LOCKED | EXTENT_WRITEBACK,
+					   0, NULL))
+				goto next;
+			/*
+			 * If it's not in the list of modified extents, used
+			 * by a fast fsync, we can remove it. If it's being
+			 * logged we can safely remove it since fsync took an
+			 * extra reference on the em.
+			 */
+			if (list_empty(&em->list) ||
+			    test_bit(EXTENT_FLAG_LOGGING, &em->flags)) {
 				set_bit(BTRFS_INODE_NEEDS_FULL_SYNC,
 					&btrfs_inode->runtime_flags);
 				remove_extent_mapping(map, em);
 				/* once for the rb tree */
 				free_extent_map(em);
 			}
+next:
 			start = extent_map_end(em);
 			write_unlock(&map->lock);
 
