f7f596e22f1a035230e1c5820cbdc2e7fd3abac9
Valentin Caron <valentin.caron@st.com>
2020-10-20 09:55:20 +0200

    serial: stm32: rework tx dma state condition
    Tx dma state condition is handled by tx_dma_busy boolean.
    
    This boolean is set when dma descriptor is requested and reset when dma
    channel is stopped (dma_terminate).
    
    In stm32_usart_serial_remove(), stm32_usart_stop_tx() and
    stm32_usart_transmit_chars_dma() fallback error case, dma channel is
    stopped but tx_dma_busy is not handled.
    
    Instead of fixing this issue, we prefer to rework the driver by removing this
    boolean and use two function to replace it:
    
    - stm32_usart_tx_dma_started return true if dma tx have a descriptor.
    - stm32_usart_tx_dma_enabled return true if DMAT bit equal true.
    
    Signed-off-by: Valentin Caron <valentin.caron@st.com>
    Signed-off-by: Erwan Le Ray <erwan.leray@st.com>
    Change-Id: I65b5c55295ec5e777a19e8c1b6027fd2d8b4cb97
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/185464
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Fabrice GASNIER <fabrice.gasnier@st.com>

--- drivers/tty/serial/stm32-usart.h
+++ drivers/tty/serial/stm32-usart.h
@@ -261,7 +261,6 @@ struct stm32_port {
 	u32 cr1_irq;		 /* USART_CR1_RXNEIE or RTOIE */
 	u32 cr3_irq;		 /* USART_CR3_RXFTIE */
 	int last_res;
-	bool tx_dma_busy;	 /* dma tx busy               */
 	bool throttled;		 /* port throttled            */
 	bool hw_flow_control;
 	bool fifoen;
