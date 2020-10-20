79e4b3c1e2846a28b439cd530d980b0472894b41
이진우 (JW Lee) <jw.lee@telechips.com>
2020-10-20 18:02:39 +0900

    [bug] tcc805x : Clock | Fixed wrong logic on suspend/resume.
    - Fixed wrong logic on suspend/resume function
    on clock driver.
    
    Linked Commit: N/A
    ITS: https://tcs.telechips.com:8443/browse/SD805XA1-427
    
    Change-Id: I45887376773529fda72e83ce1e54db7ce886c1ec

--- drivers/clk/tcc/clk-tcc805x.c
+++ drivers/clk/tcc/clk-tcc805x.c
@@ -76,8 +76,9 @@ void tcc_ckc_save(unsigned int clk_down)
 	pr_info("[INFO][tcc_clk][%s] Clock driver suspended\n", TCC_SUBCATEGORY);	
 
 #if !defined(CONFIG_TCC805X_CA53Q)
-	for (i = 0; i < PERI_MAX; i++) {
 	ckc_backup = kzalloc(sizeof(struct ckc_backup_reg), GFP_KERNEL);
+
+	for (i = 0; i < PERI_MAX; i++) {
 		if (((TCC_CKC_SMU_END <= i) && (i < TCC_CKC_DDI))	||
 			((TCC_CKC_DDI_END <= i) && (i < TCC_CKC_HSIO))	 ||
 			((TCC_CKC_HSIO_END <= i) && (i < TCC_CKC_IO))	 ||
