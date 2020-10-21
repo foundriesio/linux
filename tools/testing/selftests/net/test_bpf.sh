e0b460129a94806ad47e4e9c586db53cad9a901a
Seth Forshee <seth.forshee@canonical.com>
2020-10-21 16:29:00 +0200

    UBUNTU: SAUCE: selftests: net: don't fail test_bpf when module is not present
    BugLink: https://bugs.launchpad.net/bugs/1900855
    
    Groovy/linux-aws is carrying this patch:
    
    0deca25e3bb1 UBUNTU: SAUCE: selftests: net: don't fail test_bpf when module is
    not present
    
    since the fix is not AWS specific, (and other kernels/selftests might benefit
    from it), fold it in generic and make it disappear from linux-aws after next
    rebase.
    
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Acked-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    
    (cherry picked from commit 0deca25e3bb1263450abb09af79e48bf80b3946e linux-aws)
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Colin King <colin.king@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- tools/testing/selftests/net/test_bpf.sh
+++ tools/testing/selftests/net/test_bpf.sh
@@ -2,7 +2,10 @@
 # SPDX-License-Identifier: GPL-2.0
 # Runs bpf test using test_bpf kernel module
 
-if /sbin/modprobe -q test_bpf ; then
+# Test whether test_bpf module exists
+if ! /sbin/modprobe -q -n test_bpf; then
+	echo "SKIP: test_bpf module not found"
+elif /sbin/modprobe -q test_bpf ; then
 	/sbin/modprobe -q -r test_bpf;
 	echo "test_bpf: ok";
 else
