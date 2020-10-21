29d2c28404ec3ba9edfb7d7df59489cdc0634103
Denis Kirjanov <denis.kirjanov@suse.com>
2020-10-21 13:00:09 +0300

    Refresh patches.suse/linux-dim-Rename-externally-used-net_dim-members.patch.
    suse-commit: 959a651fbad5b361081873cfbb238d7993279437

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -243,7 +243,7 @@ struct mlx5e_params {
 	u8  num_tc;
 	bool rx_cqe_compress_def;
 	struct dim_cq_moder rx_cq_moderation;
-	struct dim_cq_moder tx_cq_moderation
+	struct dim_cq_moder tx_cq_moderation;
 	bool tunneled_offload_en;
 	bool lro_en;
 	u32 lro_wqe_sz;
