c7b6eed9e5fee8f0127381668311acb2c250cfc4
선현수 (HS Seon) <hsseon@telechips.com>
2020-10-26 22:16:42 +0900

    Revert "Revert "ANDROID: sdcardfs: Add option to not link obb""
    This reverts commit 8959fa883edc48091b0fe54c1c9053282c47372b.
    
    Change-Id: I71b975e7117e4b3cd45d163b5b8f2530eae0036b

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -34,7 +34,6 @@ enum {
 	Opt_reserved_mb,
 	Opt_gid_derivation,
 	Opt_default_normal,
-	Opt_nocache,
 	Opt_err,
 };
 
@@ -128,9 +127,6 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 			break;
 		case Opt_default_normal:
 			opts->default_normal = true;
-			break;
-		case Opt_nocache:
-			opts->nocache = true;
 			break;
 		/* unknown option */
 		default:
