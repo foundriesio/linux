66a804eebeb43ca108ae674b6da04744fe2e27cc
SangWon Lee <leesw@telechips.com>
2020-10-28 14:12:34 +0900

    mailbox: Apply kernel code style
    Change-Id: I6b9277ac5a488933c51c29898395bd9e97e51338
    (cherry picked from commit 7ed0694b8432f9686cbb7ce8d8f1fcd86d029f56)

--- drivers/mailbox/mailbox-tcc.c
+++ drivers/mailbox/mailbox-tcc.c
@@ -1,19 +1,8 @@
-/****************************************************************************
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
- * Copyright (C) 2018 Telechips Inc.
- *
- * This program is free software; you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation;
- * either version 2 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
- ****************************************************************************/
+ * Copyright (C) Telechips Inc.
+ */
 
 #include <linux/interrupt.h>
 #include <linux/mailbox_controller.h>
@@ -25,27 +14,26 @@
 #include <linux/kthread.h>
 
 // clang-format off
-#define MBOX_TXC(x)		(0x00 + (x) * 4)	/* Transmit cmd fifo */
-#define MBOX_RXC(x)		(0x20 + (x) * 4)	/* Receive cmd fifo */
-#define MBOX_CTR		0x40
+#define MBOX_TXC(x)	(0x00 + (x) * 4)	/* Transmit cmd fifo */
+#define MBOX_RXC(x)	(0x20 + (x) * 4)	/* Receive cmd fifo */
+#define MBOX_CTR	0x40
 #define MBOX_CMD_STS	0x44
 #define MBOX_TXD_STS	0x50			/* Transmit data status */
 #define MBOX_RXD_STS	0x54			/* Receive data status */
-#define MBOX_TXD		0x60			/* Transmit data fifo */
-#define MBOX_RXD		0x70			/* Receive data fifo */
+#define MBOX_TXD	0x60			/* Transmit data fifo */
+#define MBOX_RXD	0x70			/* Receive data fifo */
 // clang-format on
 
 #define MBOX_THREADED_IRQ
 
-struct tcc_mbox_device
-{
+struct tcc_mbox_device {
 	struct mbox_controller mbox;
 	void __iomem *mbox_base;
 	int irq;
 	spinlock_t lock;
 	struct tcc_mbox_msg *msg;
 #ifndef MBOX_THREADED_IRQ
-	struct task_struct* t;
+	struct task_struct *t;
 	int received;
 	wait_queue_head_t wait;
 #endif
@@ -74,8 +62,8 @@ static int tcc_mbox_send_data(struct mbox_chan *chan, void *data)
 
 	/* disable transmit data output. */
 	spin_lock_irqsave(&mdev->lock, flags);
-	writel_relaxed(
-		readl_relaxed(mdev->mbox_base + MBOX_CTR) & ~(1 << 5), mdev->mbox_base + MBOX_CTR);
+	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) & ~(1 << 5),
+		mdev->mbox_base + MBOX_CTR);
 	spin_unlock_irqrestore(&mdev->lock, flags);
 
 	writel_relaxed(msg->cmd, mdev->mbox_base + MBOX_TXC(0));
@@ -84,17 +72,21 @@ static int tcc_mbox_send_data(struct mbox_chan *chan, void *data)
 	writel_relaxed(msg->trans_type, mdev->mbox_base + MBOX_TXC(3));
 	if (msg->trans_type == DATA_MBOX && msg->msg_len > 0) {
 		uint32_t *message = (uint32_t *)msg->message;
-		for (idx = 0; idx < (msg->msg_len + 3) / 4; idx++)
-			writel_relaxed(message[idx], mdev->mbox_base + MBOX_TXD);
 
-		while ((readl_relaxed(mdev->mbox_base + MBOX_TXD_STS) & 0xFFFF) != idx)
+		for (idx = 0; idx < (msg->msg_len + 3) / 4; idx++) {
+			writel_relaxed(
+				message[idx], mdev->mbox_base + MBOX_TXD);
+		}
+
+		while ((readl_relaxed(mdev->mbox_base + MBOX_TXD_STS) & 0xFFFF)
+			!= idx)
 			;
 	}
 
 	/* enable transmit data output. */
 	spin_lock_irqsave(&mdev->lock, flags);
-	writel_relaxed(
-		readl_relaxed(mdev->mbox_base + MBOX_CTR) | (1 << 5), mdev->mbox_base + MBOX_CTR);
+	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) | (1 << 5),
+		mdev->mbox_base + MBOX_CTR);
 	spin_unlock_irqrestore(&mdev->lock, flags);
 	return 0;
 }
@@ -106,7 +98,8 @@ static int tcc_mbox_startup(struct mbox_chan *chan)
 
 	/* enable received interrupt */
 	spin_lock_irqsave(&mdev->lock, flags);
-	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) | 0x10, mdev->mbox_base + MBOX_CTR);
+	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) | 0x10,
+		mdev->mbox_base + MBOX_CTR);
 	spin_unlock_irqrestore(&mdev->lock, flags);
 
 	return 0;
@@ -137,8 +130,8 @@ static bool tcc_mbox_tx_done(struct mbox_chan *chan)
 
 	/* disable transmit data output. */
 	spin_lock_irqsave(&mdev->lock, flags);
-	writel_relaxed(
-		readl_relaxed(mdev->mbox_base + MBOX_CTR) & ~(1 << 5), mdev->mbox_base + MBOX_CTR);
+	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) & ~(1 << 5),
+		mdev->mbox_base + MBOX_CTR);
 	spin_unlock_irqrestore(&mdev->lock, flags);
 
 	return true;
@@ -168,7 +161,8 @@ static irqreturn_t tcc_mbox_irq(int irq, void *dev_id)
 		/* Clear mbox received interrupt bit */
 		spin_lock_irqsave(&mdev->lock, flags);
 		writel_relaxed(
-			readl_relaxed(mdev->mbox_base + MBOX_CTR) & ~(1 << 4), mdev->mbox_base + MBOX_CTR);
+			readl_relaxed(mdev->mbox_base + MBOX_CTR) & ~(1 << 4),
+			mdev->mbox_base + MBOX_CTR);
 		spin_unlock_irqrestore(&mdev->lock, flags);
 #ifdef MBOX_THREADED_IRQ
 		return IRQ_WAKE_THREAD;
@@ -197,8 +191,9 @@ static irqreturn_t tcc_mbox_isr(int irq, void *dev_id)
 	if (!mdev->msg)
 		return IRQ_NONE;
 
-	if (((readl_relaxed(mdev->mbox_base + MBOX_CMD_STS) >> 20) & 0xF) == 0) {
-		dev_err(mdev->mbox.dev, "[ERROR][TCC_MBOX] Mailbox FIFO is empty\n");
+	if (((readl_relaxed(mdev->mbox_base + MBOX_CMD_STS) >> 20) & 0xF)
+	    == 0) {
+		dev_err(mdev->mbox.dev, "Mailbox FIFO is empty\n");
 		return IRQ_HANDLED;
 	}
 
@@ -208,35 +203,42 @@ static irqreturn_t tcc_mbox_isr(int irq, void *dev_id)
 	mdev->msg->trans_type = readl_relaxed(mdev->mbox_base + MBOX_RXC(3));
 	if ((mdev->msg->trans_type == DATA_MBOX) && mdev->msg->msg_len > 0) {
 		uint32_t *message = (uint32_t *)mdev->msg->message;
+
 		for (idx = 0; idx < (mdev->msg->msg_len + 3) / 4; idx++) {
-			message[idx] = readl_relaxed(mdev->mbox_base + MBOX_RXD);
+			message[idx] =
+				readl_relaxed(mdev->mbox_base + MBOX_RXD);
 		}
 
+#if (0)
 		/* Disable this statement because of mailbox hang. */
-		/* while ((readl_relaxed(mdev->mbox_base + MBOX_RXD_STS) & (1 << 31)) == 0); */
+		while ((readl_relaxed(mdev->mbox_base + MBOX_RXD_STS)
+			& (1 << 31)) == 0)
+			;
+#endif
 	}
 
-	if(mdev->mbox.chans[0].cl != NULL) {
-		mbox_chan_received_data(&mdev->mbox.chans[0], (void *)mdev->msg);
+	if (mdev->mbox.chans[0].cl != NULL) {
+		mbox_chan_received_data(
+			&mdev->mbox.chans[0], (void *)mdev->msg);
 	}
 
 	/* Set mbox received interrupt bit */
 	spin_lock_irqsave(&mdev->lock, flags);
-	writel_relaxed(
-		readl_relaxed(mdev->mbox_base + MBOX_CTR) | (1 << 4), mdev->mbox_base + MBOX_CTR);
+	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) | (1 << 4),
+		mdev->mbox_base + MBOX_CTR);
 	spin_unlock_irqrestore(&mdev->lock, flags);
 
 	return IRQ_HANDLED;
 }
 
 #ifndef MBOX_THREADED_IRQ
-static int tcc_mbox_thread(void* arg)
+static int tcc_mbox_thread(void *arg)
 {
 	struct tcc_mbox_device *mdev = (struct tcc_mbox_device *)arg;
 
-	while (!kthread_should_stop())
-	{
-		wait_event_interruptible_timeout(mdev->wait, mdev->received == 1, msecs_to_jiffies(500));
+	while (!kthread_should_stop()) {
+		wait_event_interruptible_timeout(
+			mdev->wait, mdev->received == 1, msecs_to_jiffies(500));
 
 		if (mdev->received == 1) {
 			mdev->received = 0;
@@ -261,16 +263,19 @@ static int tcc_mbox_probe(struct platform_device *pdev)
 	if (!pdev->dev.of_node)
 		return -ENODEV;
 
-	mdev = devm_kzalloc(&pdev->dev, sizeof(struct tcc_mbox_device), GFP_KERNEL);
+	mdev = devm_kzalloc(
+		&pdev->dev, sizeof(struct tcc_mbox_device), GFP_KERNEL);
 	if (!mdev)
 		return -ENOMEM;
 
 	/* Allocated one channel */
-	mdev->mbox.chans = devm_kzalloc(&pdev->dev, sizeof(struct mbox_chan), GFP_KERNEL);
+	mdev->mbox.chans =
+		devm_kzalloc(&pdev->dev, sizeof(struct mbox_chan), GFP_KERNEL);
 	if (!mdev->mbox.chans)
 		return -ENOMEM;
 
-	mdev->msg = devm_kzalloc(&pdev->dev, sizeof(struct tcc_mbox_msg), GFP_KERNEL);
+	mdev->msg = devm_kzalloc(
+		&pdev->dev, sizeof(struct tcc_mbox_msg), GFP_KERNEL);
 	if (!mdev->msg)
 		return -ENOMEM;
 
@@ -294,17 +299,18 @@ static int tcc_mbox_probe(struct platform_device *pdev)
 
 #ifdef MBOX_THREADED_IRQ
 	ret = devm_request_threaded_irq(
-		&pdev->dev, mdev->irq, tcc_mbox_irq, tcc_mbox_isr, IRQF_ONESHOT, dev_name(&pdev->dev),
-		mdev);
+		&pdev->dev, mdev->irq, tcc_mbox_irq, tcc_mbox_isr, IRQF_ONESHOT,
+		dev_name(&pdev->dev), mdev);
 #else
-	ret = request_irq(mdev->irq, tcc_mbox_irq, 0, dev_name(&pdev->dev), mdev);
+	ret = request_irq(
+		mdev->irq, tcc_mbox_irq, 0, dev_name(&pdev->dev), mdev);
 #endif
 	if (ret < 0)
 		return ret;
 
 	ret = mbox_controller_register(&mdev->mbox);
 	if (ret < 0)
-		dev_err(&pdev->dev, "[ERROR][TCC_MBOX] Failed to register mailbox: %d\n", ret);
+		dev_err(&pdev->dev, "Failed to register mailbox: %d\n", ret);
 
 #ifndef MBOX_THREADED_IRQ
 	init_waitqueue_head(&mdev->wait);
@@ -329,10 +335,13 @@ static int tcc_mbox_remove(struct platform_device *pdev)
 
 int tcc_mbox_suspend(struct device *dev)
 {
-	//struct tcc_mbox_device *mdev = dev_get_drvdata(dev);
+	// struct tcc_mbox_device *mdev = dev_get_drvdata(dev);
 
+#if (0)
 	/* disable received interrupt */
-	//writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) & ~0x10, mdev->mbox_base + MBOX_CTR);
+	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) & ~0x10,
+		mdev->mbox_base + MBOX_CTR);
+#endif
 
 	return 0;
 }
@@ -342,10 +351,11 @@ int tcc_mbox_resume(struct device *dev)
 	struct tcc_mbox_device *mdev = dev_get_drvdata(dev);
 
 	/* flush cmd data */
-	//writel_relaxed(0xc0, mdev->mbox_base + MBOX_CTR);
+	// writel_relaxed(0xc0, mdev->mbox_base + MBOX_CTR);
 
 	/* enable received interrupt */
-	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) | 0x10, mdev->mbox_base + MBOX_CTR);
+	writel_relaxed(readl_relaxed(mdev->mbox_base + MBOX_CTR) | 0x10,
+		mdev->mbox_base + MBOX_CTR);
 
 	return 0;
 }
@@ -357,8 +367,7 @@ static const struct dev_pm_ops tcc_mbox_pm = {
 static struct platform_driver tcc_mbox_driver = {
 	.probe = tcc_mbox_probe,
 	.remove = tcc_mbox_remove,
-	.driver =
-		{
+	.driver = {
 			.name = "tcc-mailbox",
 			.pm = &tcc_mbox_pm,
 			.of_match_table = of_match_ptr(tcc_mbox_of_match),
