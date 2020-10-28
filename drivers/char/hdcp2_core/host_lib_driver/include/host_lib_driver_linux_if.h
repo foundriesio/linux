0a51ce02e7de243c53aef7f188a937a2eaf8350c
SangWon Lee <leesw@telechips.com>
2020-10-28 11:26:51 +0900

    [ref] hdcp: Apply kernel code style
    Change-Id: I525a1f66dcb4e6a1e64fa6dc5a285eeabf5d5fdf
    ITS: TMBSP-2547
    (cherry picked from commit fae076cefb9a5ce6dee82c4b34e474f357a40525)
    (cherry picked from commit b772f6639680c01cd7c6ce52212505a722021945)

--- drivers/char/hdcp2_core/host_lib_driver/include/host_lib_driver_linux_if.h
+++ drivers/char/hdcp2_core/host_lib_driver/include/host_lib_driver_linux_if.h
@@ -1,32 +1,9 @@
-//-----------------------------------------------------------------------
-//
-//              (C) COPYRIGHT 2014 - 2015 SYNOPSYS, INC.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2
-// as published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-//
-// ------------------------------------------------------------------------
-//
-// Project:
-//
-// ESM Host Library
-//
-// Description:
-//
-// ESM Host Library Driver: Interface to the Linux kernel driver.
-//
-// ------------------------------------------------------------------------
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) Telechips Inc.
+ * (C) COPYRIGHT 2014 - 2015 SYNOPSYS, INC.
+ */
 
 #ifndef ESM_HLD_LINUX_IOCTL_H_
 #define ESM_HLD_LINUX_IOCTL_H_
