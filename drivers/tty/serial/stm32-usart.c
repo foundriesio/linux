44a26ecb4f3391107223562c06c6c4b1a24b72a3
Valentin Caron <valentin.caron@st.com>
2020-10-30 17:50:27 +0100

    serial: stm32: fix call of a non-atomic function in stop_rx
    Stm32_usart_stop_rx() is a function called under atomic context.
    But it called a non-atomic function "dma_terminate_sync()".
    
    Correct it in replace by "dma_terminate_async()"
    
    Fixes: d3afae779aa6 ("serial: stm32: move dma rx disabling from shutdown to stop_rx")
    Signed-off-by: Valentin Caron <valentin.caron@st.com>
    Change-Id: If1abb7e2a9dff45c3b871f90ff3d314c9ad69853
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/185015
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: CIBUILD <smet-aci-builds@lists.codex.cro.st.com>
    Reviewed-by: Fabrice GASNIER <fabrice.gasnier@st.com>

--- drivers/tty/serial/stm32-usart.c
+++ drivers/tty/serial/stm32-usart.c
@@ -794,7 +794,7 @@ static void stm32_usart_stop_rx(struct uart_port *port)
 	/* Disable DMA request line and RX DMA. */
 	if (stm32_port->rx_ch) {
 		stm32_usart_clr_bits(port, ofs->cr3, USART_CR3_DMAR);
-		dmaengine_terminate_sync(stm32_port->rx_ch);
+		dmaengine_terminate_async(stm32_port->rx_ch);
 	}
 
 	stm32_usart_clr_bits(port, ofs->cr1, stm32_port->cr1_irq);
