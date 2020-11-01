6f68348522aa35918996abc7bffc812bebde0eaf
Sasha Levin <sashal@kernel.org>
2020-11-01 12:30:42 -0500

    ionic: no rx flush in deinit
    BugLink: https://bugs.launchpad.net/bugs/1908150
    
    [ Upstream commit 43ecf7b46f2688fd37909801aee264f288b3917b ]
    
    Kmemleak pointed out to us that ionic_rx_flush() is sending
    skbs into napi_gro_XXX with a disabled napi context, and these
    end up getting lost and leaked.  We can safely remove the flush.
    
    Fixes: 0f3154e6bcb3 ("ionic: Add Tx and Rx handling")
    Signed-off-by: Shannon Nelson <snelson@pensando.io>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Ian May <ian.may@canonical.com>

--- drivers/net/ethernet/pensando/ionic/ionic_txrx.c
+++ drivers/net/ethernet/pensando/ionic/ionic_txrx.c
@@ -266,18 +266,6 @@ static u32 ionic_rx_walk_cq(struct ionic_cq *rxcq, u32 limit)
 	return work_done;
 }
 
-void ionic_rx_flush(struct ionic_cq *cq)
-{
-	struct ionic_dev *idev = &cq->lif->ionic->idev;
-	u32 work_done;
-
-	work_done = ionic_rx_walk_cq(cq, cq->num_descs);
-
-	if (work_done)
-		ionic_intr_credits(idev->intr_ctrl, cq->bound_intr->index,
-				   work_done, IONIC_INTR_CRED_RESET_COALESCE);
-}
-
 static struct page *ionic_rx_page_alloc(struct ionic_queue *q,
 					dma_addr_t *dma_addr)
 {
