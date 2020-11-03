2990908b41d58bbd592659aa2e262d945bb0c3e3
Adrian Hunter <adrian.hunter@intel.com>
2020-11-03 16:14:03 +0200

    scsi: ufs: Allow an error return value from ->device_reset()
    BugLink: https://bugs.launchpad.net/bugs/1914472
    
    [ Upstream commit 151f1b664ffbb847c7fbbce5a5b8580f1b9b1d98 ]
    
    It is simpler for drivers to provide a ->device_reset() callback
    irrespective of whether the GPIO, or firmware interface necessary to do the
    reset, is discovered during probe.
    
    Change ->device_reset() to return an error code.  Drivers that provide the
    callback, but do not do the reset operation should return -EOPNOTSUPP.
    
    Link: https://lore.kernel.org/r/20201103141403.2142-3-adrian.hunter@intel.com
    Reviewed-by: Asutosh Das <asutoshd@codeaurora.org>
    Reviewed-by: Stanley Chu <stanley.chu@mediatek.com>
    Reviewed-by: Bean huo <beanhuo@micron.com>
    Reviewed-by: Can Guo <cang@codeaurora.org>
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: William Breathitt Gray <william.gray@canonical.com>

--- drivers/scsi/ufs/ufs-mediatek.c
+++ drivers/scsi/ufs/ufs-mediatek.c
@@ -440,7 +440,7 @@ static int ufs_mtk_link_startup_notify(struct ufs_hba *hba,
 	return ret;
 }
 
-static void ufs_mtk_device_reset(struct ufs_hba *hba)
+static int ufs_mtk_device_reset(struct ufs_hba *hba)
 {
 	struct arm_smccc_res res;
 
@@ -461,6 +461,8 @@ static void ufs_mtk_device_reset(struct ufs_hba *hba)
 	usleep_range(10000, 15000);
 
 	dev_info(hba->dev, "device reset done\n");
+
+	return 0;
 }
 
 static int ufs_mtk_link_set_hpm(struct ufs_hba *hba)
