61398b11068acf3b075065e39345c3d391c781a9
Jason Gunthorpe <jgg@nvidia.com>
2020-11-06 10:00:49 -0400

    RDMA/siw,rxe: Make emulated devices virtual in the device tree
    BugLink: https://bugs.launchpad.net/bugs/1914472
    
    [ Upstream commit a9d2e9ae953f0ddd0327479c81a085adaa76d903 ]
    
    This moves siw and rxe to be virtual devices in the device tree:
    
    lrwxrwxrwx 1 root root 0 Nov  6 13:55 /sys/class/infiniband/rxe0 -> ../../devices/virtual/infiniband/rxe0/
    
    Previously they were trying to parent themselves to the physical device of
    their attached netdev, which doesn't make alot of sense.
    
    My hope is this will solve some weird syzkaller hits related to sysfs as
    it could be possible that the parent of a netdev is another netdev, eg
    under bonding or some other syzkaller found netdev configuration.
    
    Nesting a ib_device under anything but a physical device is going to cause
    inconsistencies in sysfs during destructions.
    
    Link: https://lore.kernel.org/r/0-v1-dcbfc68c4b4a+d6-virtual_dev_jgg@nvidia.com
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: William Breathitt Gray <william.gray@canonical.com>

--- drivers/infiniband/sw/siw/siw_main.c
+++ drivers/infiniband/sw/siw/siw_main.c
@@ -306,24 +306,8 @@ static struct siw_device *siw_device_create(struct net_device *netdev)
 {
 	struct siw_device *sdev = NULL;
 	struct ib_device *base_dev;
-	struct device *parent = netdev->dev.parent;
 	int rv;
 
-	if (!parent) {
-		/*
-		 * The loopback device has no parent device,
-		 * so it appears as a top-level device. To support
-		 * loopback device connectivity, take this device
-		 * as the parent device. Skip all other devices
-		 * w/o parent device.
-		 */
-		if (netdev->type != ARPHRD_LOOPBACK) {
-			pr_warn("siw: device %s error: no parent device\n",
-				netdev->name);
-			return NULL;
-		}
-		parent = &netdev->dev;
-	}
 	sdev = ib_alloc_device(siw_device, base_dev);
 	if (!sdev)
 		return NULL;
@@ -382,7 +366,6 @@ static struct siw_device *siw_device_create(struct net_device *netdev)
 	 * per physical port.
 	 */
 	base_dev->phys_port_cnt = 1;
-	base_dev->dev.parent = parent;
 	base_dev->dev.dma_ops = &dma_virt_ops;
 	base_dev->dev.dma_parms = &sdev->dma_parms;
 	sdev->dma_parms = (struct device_dma_parameters)
@@ -428,7 +411,7 @@ static struct siw_device *siw_device_create(struct net_device *netdev)
 	atomic_set(&sdev->num_mr, 0);
 	atomic_set(&sdev->num_pd, 0);
 
-	sdev->numa_node = dev_to_node(parent);
+	sdev->numa_node = dev_to_node(&netdev->dev);
 	spin_lock_init(&sdev->lock);
 
 	return sdev;
