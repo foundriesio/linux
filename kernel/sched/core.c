fdc4c72b8612e7c31b1c422ddaf27f866610b612
Peter Zijlstra <peterz@infradead.org>
2020-11-17 12:14:51 +0100

    sched: Fix migration_cpu_stop() WARN
    Oleksandr reported hitting the WARN in the 'task_rq(p) != rq' branch
    of migration_cpu_stop(). Valentin noted that using cpu_of(rq) in that
    case is just plain wrong to begin with, since per the earlier branch
    that isn't the actual CPU of the task.
    
    Replace both instances of is_cpu_allowed() by a direct p->cpus_mask
    test using task_cpu().
    
    Reported-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Debugged-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1926,7 +1926,7 @@ static int migration_cpu_stop(void *data)
 			 * and we should be valid again. Nothing to do.
 			 */
 			if (!pending) {
-				WARN_ON_ONCE(!is_cpu_allowed(p, cpu_of(rq)));
+				WARN_ON_ONCE(!cpumask_test_cpu(task_cpu(p), &p->cpus_mask));
 				goto out;
 			}
 
@@ -1954,7 +1954,7 @@ static int migration_cpu_stop(void *data)
 		 * valid again. Nothing to do.
 		 */
 		if (!pending) {
-			WARN_ON_ONCE(!is_cpu_allowed(p, cpu_of(rq)));
+			WARN_ON_ONCE(!cpumask_test_cpu(task_cpu(p), &p->cpus_mask));
 			goto out;
 		}
 
