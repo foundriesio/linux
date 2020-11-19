c42b9a162afb66cfda09864a5a21c8a2425a353a
김석범 (Jason Kim) <jason.kim@telechips.com>
2020-11-19 11:07:12 +0000

    [fea] Adjust a new pmap policy for video-input
    Linked Commit: N/A
    ITS: TMABT-1590
    
    Change-Id: Ic00fe0bba53591d10131c66fa1432f86df70c920

---
+++ include/dt-bindings/pmap/pmap-tcc803x-linux-ivi.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+/*
+ * Copyright (C) 2020 Telechips Inc.
+ */
+#ifndef DT_BINDINGS_PMAP_TCC803X_LINUX_IVI_H
+#define DT_BINDINGS_PMAP_TCC803X_LINUX_IVI_H
+
+#include <dt-bindings/pmap/pmap-tcc803x-linux-ivi-common.h>
+#include <dt-bindings/pmap/pmap-tcc803x-linux-ivi-display.h>
+#include <dt-bindings/pmap/pmap-tcc803x-linux-ivi-video.h>
+
+#endif//DT_BINDINGS_PMAP_TCC803X_LINUX_IVI_H
