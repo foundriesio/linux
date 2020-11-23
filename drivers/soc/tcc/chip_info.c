d42042d813fe3d1f99e5e33a77789b46867ff3f2
김호연지기 (Jigi Kim) <jigi.kim@telechips.com>
2020-11-23 13:06:57 +0900

    [ref] tcc: chipinfo: Drop use of global `system_rev`
    This commit drops support for the use of global variable `system_rev`
    which stores the Telechips SoC revision number.  Instead, add getter
    function for this value so that we can avoid using extern variable.
    
    Note that, for backward compatibility, we sustain `system_rev` for now.
    
    Linked Commit: N/A
    ITS: N/A
    
    Change-Id: I1b6c2ee2c7fd56ed703843d56ac4fa5f4d50a30d

--- drivers/soc/tcc/chip_info.c
+++ drivers/soc/tcc/chip_info.c
@@ -16,18 +16,43 @@
 #include <linux/types.h>
 #include <linux/arm-smccc.h>
 #include <soc/tcc/tcc-sip.h>
+#include <soc/tcc/chipinfo.h>
 
+static u32 chip_rev = ~((unsigned long)0);
+static u32 chip_name = ~((unsigned long)0);
 
+u32 get_chip_rev(void)
+{
+	return chip_rev;
+}
+
+u32 get_chip_name(void)
+{
+	return chip_name;
+}
+
+static inline u32 tcc_chip_ops(ulong cmd, ulong arg)
+{
+	struct arm_smccc_res res;
+	u32 ret;
+
+	arm_smccc_smc(cmd, arg, 0, 0, 0, 0, 0, 0, &res);
+	ret = res.a0 & 0xFFFFFFFFU;
+
+	return ret;
+}
 
 static int __init chip_info_init(void)
 {
 	struct arm_smccc_res res;
 
-	system_rev = 999U;
-	arm_smccc_smc(SIP_CHIP_REV, 0, 0, 0, 0, 0, 0, 0, &res);
-	system_rev = (unsigned int)res.a0;
+	chip_rev = tcc_chip_ops(SIP_CHIP_REV, 0);
+	chip_name = tcc_chip_ops(SIP_CHIP_NAME, 0);
 
-	pr_info("%s, system_rev = %d\n", __func__, system_rev);
+	/* For backward compatibility */
+	system_rev = chip_rev;
+
+	pr_info("%s, package: %x rev: %d\n", __func__, chip_name, chip_rev);
 	return 0;
 }
 pure_initcall(chip_info_init);
---
+++ include/soc/tcc/chipinfo.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) Telechips Inc.
+ */
+
+#ifndef SOC_TCC_CHIPINFO_H
+#define SOC_TCC_CHIPINFO_H
+
+u32 get_chip_rev(void);
+u32 get_chip_name(void);
+
+#endif /* SOC_TCC_CHIPINFO_H */
