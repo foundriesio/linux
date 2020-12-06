2876f6069b9055f965bf9c4a4e035aee0fa63455
Richard Weinberger <richard@nod.at>
2020-12-06 21:22:20 +0100

    mtd: core: Fix refcounting for unpartitioned MTDs
    BugLink: https://bugs.launchpad.net/bugs/1912027
    
    commit 1ca71415f075353974524e96ed175306d8a937a8 upstream.
    
    Apply changes to usecount also to the master partition.
    Otherwise we have no refcounting at all if an MTD has no partitions.
    
    Cc: stable@vger.kernel.org
    Fixes: 46b5889cc2c5 ("mtd: implement proper partition handling")
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Link: https://lore.kernel.org/linux-mtd/20201206202220.27290-1-richard@nod.at
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/mtd/mtdcore.c
+++ drivers/mtd/mtdcore.c
@@ -1013,6 +1013,8 @@ int __get_mtd_device(struct mtd_info *mtd)
 		}
 	}
 
+	master->usecount++;
+
 	while (mtd->parent) {
 		mtd->usecount++;
 		mtd = mtd->parent;
@@ -1078,6 +1080,8 @@ void __put_mtd_device(struct mtd_info *mtd)
 		BUG_ON(mtd->usecount < 0);
 		mtd = mtd->parent;
 	}
+
+	master->usecount--;
 
 	if (master->_put_device)
 		master->_put_device(master);
