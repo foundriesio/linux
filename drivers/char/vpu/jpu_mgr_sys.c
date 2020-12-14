d0f7014c804d040d6434eac5a394ce07dad63d88
조준환 (Jun Cho) <junhwan.cho@telechips.com>
2020-12-14 18:44:45 +0900

    Revert "[upg] Fix warnings and errors of vpu module caused by running           linux/checkpatch.pl following kernel coding style           Note. Modifications applied to Kernel 4.14.202 applied to                 Kernel 4.14.137"
    This reverts commit ee834b69e6e2856857f82e32458ebfdfe3e905a7.
    
    Change-Id: I5858a374174ee8ed3f0ea9d0885629389f242065

--- drivers/char/vpu/jpu_mgr_sys.c
+++ drivers/char/vpu/jpu_mgr_sys.c
@@ -26,20 +26,15 @@
 #include "vpu_comm.h"
 #include "jpu_mgr_sys.h"
 
-#define dprintk(msg...)  V_DBG(VPU_DBG_INFO, "TCC_JPU_MGR_SYS:" msg)
-#define detailk(msg...)  V_DBG(VPU_DBG_INFO, "TCC_JPU_MGR_SYS:" msg)
-#define err(msg...)      V_DBG(VPU_DBG_INFO, "TCC_JPU_MGR_SYS[Err]:" msg)
-#define info(msg...)     V_DBG(VPU_DBG_INFO, "TCC_JPU_MGR_SYS[Info]:" msg)
-
-static struct clk *vbus_jpeg_clk;	// = NULL;
+static struct clk* vbus_jpeg_clk = NULL; // for pwdn and vBus.
 
 #if defined(VIDEO_IP_DIRECT_RESET_CTRL)
 #include <linux/reset.h>
-static struct reset_control *vbus_jpeg_reset;	// = NULL;
+static struct reset_control* vbus_jpeg_reset = NULL; // for pwdn and vBus.
 #endif
 
 extern int tccxxx_sync_player(int sync);
-static int cache_droped;	// = 0;
+static int cache_droped = 0;
 
 void jmgr_enable_clock(int vbus_no_ctrl, int only_clk_ctrl)
 {
@@ -47,13 +42,17 @@ void jmgr_enable_clock(int vbus_no_ctrl, int only_clk_ctrl)
 		clk_prepare_enable(vbus_jpeg_clk);
 
 #if defined(CONFIG_ARCH_TCC899X) || defined(CONFIG_ARCH_TCC901X)
-	if (!only_clk_ctrl) {
+	if (!only_clk_ctrl)
+	{
 		int ret = jpu_optee_open();
-
 		if (ret != 0)
-			err("jpu_optee_open: failed !! - ret [%d]", ret);
+		{
+			printk("jpu_optee_open: failed !! - ret [%d]\n", ret);
+		}
 		else
-			dprintk("jpu_optee_open: success !!");
+		{
+			printk("jpu_optee_open: success !! \n");
+		}
 	}
 #endif
 }
@@ -64,21 +63,27 @@ void jmgr_disable_clock(int vbus_no_ctrl, int only_clk_ctrl)
 		clk_disable_unprepare(vbus_jpeg_clk);
 
 #if defined(CONFIG_ARCH_TCC899X) || defined(CONFIG_ARCH_TCC901X)
-	if (!only_clk_ctrl) {
+	if (!only_clk_ctrl)
+	{
 		int ret = jpu_optee_close();
-
 		if (ret != 0)
-			err("jpu_optee_close: failed !! - ret [%d]", ret);
+		{
+			printk("jpu_optee_close: failed !! - ret [%d]\n", ret);
+		}
 		else
-			dprintk("jpu_optee_close: success !!");
+		{
+			printk("jpu_optee_close: success !! \n");
+		}
 	}
 #endif
 }
 
-void jmgr_get_clock(struct device_node *node)
+void jmgr_get_clock(struct device_node* node)
 {
 	if (node == NULL)
-		err("device node is null");
+	{
+		printk("device node is null\n");
+	}
 
 	vbus_jpeg_clk = of_clk_get(node, 1);
 	BUG_ON(IS_ERR(vbus_jpeg_clk));
@@ -86,18 +91,20 @@ void jmgr_get_clock(struct device_node *node)
 
 void jmgr_put_clock(void)
 {
-	if (vbus_jpeg_clk) {
+	if (vbus_jpeg_clk)
+	{
 		clk_put(vbus_jpeg_clk);
 		vbus_jpeg_clk = NULL;
 	}
 }
 
-void jmgr_get_reset(struct device_node *node)
+void jmgr_get_reset(struct device_node* node)
 {
 #if defined(VIDEO_IP_DIRECT_RESET_CTRL)
 	if (node == NULL)
-		pr_info("device node is null\n");
-
+	{
+		printk("device node is null\n");
+	}
 	vbus_jpeg_reset = of_reset_control_get(node, "jpeg");
 	BUG_ON(IS_ERR(vbus_jpeg_reset));
 #endif
@@ -106,7 +113,8 @@ void jmgr_get_reset(struct device_node *node)
 void jmgr_put_reset(void)
 {
 #if defined(VIDEO_IP_DIRECT_RESET_CTRL)
-	if (vbus_jpeg_reset) {
+	if (vbus_jpeg_reset)
+	{
 		reset_control_put(vbus_jpeg_reset);
 		vbus_jpeg_reset = NULL;
 	}
@@ -123,8 +131,9 @@ void jmgr_hw_assert(void)
 #if defined(VIDEO_IP_DIRECT_RESET_CTRL)
 	V_DBG(VPU_DBG_RSTCLK, "enter");
 	if (vbus_jpeg_reset)
+	{
 		reset_control_assert(vbus_jpeg_reset);
-
+	}
 	V_DBG(VPU_DBG_RSTCLK, "out!! (rsr:0x%x)", jmgr_get_reset_register());
 #endif
 }
@@ -134,11 +143,12 @@ void jmgr_hw_deassert(void)
 #if defined(VIDEO_IP_DIRECT_RESET_CTRL)
 	V_DBG(VPU_DBG_RSTCLK, "enter");
 	if (vbus_jpeg_reset)
+	{
 		reset_control_deassert(vbus_jpeg_reset);
-
+	}
 	V_DBG(VPU_DBG_RSTCLK, "out!! (rsr:0x%x)", jmgr_get_reset_register());
 #endif
-}
+	}
 
 void jmgr_hw_reset(void)
 {
@@ -162,7 +172,8 @@ void jmgr_restore_clock(int vbus_no_ctrl, int opened_cnt)
 
 	jmgr_hw_assert();
 
-	while (opened_count) {
+	while (opened_count)
+	{
 		jmgr_disable_clock(vbus_no_ctrl, 0);
 		if (opened_count > 0)
 			opened_count--;
@@ -171,7 +182,8 @@ void jmgr_restore_clock(int vbus_no_ctrl, int opened_cnt)
 	udelay(1000);
 
 	opened_count = opened_cnt;
-	while (opened_count) {
+	while (opened_count)
+	{
 		jmgr_enable_clock(vbus_no_ctrl, 0);
 		if (opened_count > 0)
 			opened_count--;
@@ -193,14 +205,13 @@ void jmgr_disable_irq(unsigned int irq)
 	disable_irq(irq);
 }
 
-void jmgr_free_irq(unsigned int irq, void *dev_id)
+void jmgr_free_irq(unsigned int irq, void* dev_id)
 {
 	free_irq(irq, dev_id);
 }
 
-int jmgr_request_irq(unsigned int irq,
-		     irqreturn_t (*handler)(int irq, void *dev_id),
-		     unsigned long frags, const char *device, void *dev_id)
+int jmgr_request_irq(unsigned int irq, irqreturn_t (*handler)(int irq, void* dev_id),
+					 unsigned long frags, const char* device, void* dev_id)
 {
 	return request_irq(irq, handler, frags, device, dev_id);
 }
@@ -212,7 +223,7 @@ unsigned long jmgr_get_int_flags(void)
 
 void jmgr_init_interrupt(void)
 {
-//	return;
+	return;
 }
 
 int jmgr_BusPrioritySetting(int mode, int type)
@@ -220,9 +231,9 @@ int jmgr_BusPrioritySetting(int mode, int type)
 	return 0;
 }
 
-void jmgr_status_clear(unsigned int *base_addr)
+void jmgr_status_clear(unsigned int* base_addr)
 {
-//	return;
+	return;
 }
 
 void jmgr_init_variable(void)
