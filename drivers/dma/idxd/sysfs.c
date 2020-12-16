9b349e5fec7d1fe95db86c8a84a1f731814a1086
Dan Carpenter <dan.carpenter@oracle.com>
2020-12-16 11:29:46 +0300

    dmaengine: idxd: off by one in cleanup code
    BugLink: https://bugs.launchpad.net/bugs/1914832
    
    commit ff58f7dd0c1352a01de3a40327895bd51e03de3a upstream.
    
    The clean up is off by one so this will start at "i" and it should start
    with "i - 1" and then it doesn't unregister the zeroeth elements in the
    array.
    
    Fixes: c52ca478233c ("dmaengine: idxd: add configuration component of driver")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Acked-by: Dave Jiang <dave.jiang@intel.com>
    Link: https://lore.kernel.org/r/X9nFeojulsNqUSnG@mwanda
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: William Breathitt Gray <william.gray@canonical.com>

--- drivers/dma/idxd/sysfs.c
+++ drivers/dma/idxd/sysfs.c
@@ -389,7 +389,7 @@ int idxd_register_driver(void)
 	return 0;
 
 drv_fail:
-	for (; i > 0; i--)
+	while (--i >= 0)
 		driver_unregister(&idxd_drvs[i]->drv);
 	return rc;
 }
@@ -1554,7 +1554,7 @@ int idxd_register_bus_type(void)
 	return 0;
 
 bus_err:
-	for (; i > 0; i--)
+	while (--i >= 0)
 		bus_unregister(idxd_bus_types[i]);
 	return rc;
 }
