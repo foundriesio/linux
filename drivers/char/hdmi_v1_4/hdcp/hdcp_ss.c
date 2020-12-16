1d6b93666a87e28f610ecacf789b4306a4c1f369
YongHee Choi <yhchoi@telechips.com>
2020-12-16 18:03:14 +0900

    [upg] tcc897x hdcp 1.4 driver sources are arranged.
    Change-Id: I58a0344e4ef05e0cb8e0aa9e4e1416ba2d9a55da

--- drivers/char/hdmi_v1_4/hdcp/hdcp_ss.c
+++ drivers/char/hdmi_v1_4/hdcp/hdcp_ss.c
@@ -29,26 +29,16 @@
 #include <linux/wait.h>
 #include <linux/clk.h>
 #include <linux/delay.h>
-
 #include <linux/io.h>
 #include <linux/uaccess.h>
-
 #include "hdcp_api.h"
 #include "hdcp_ss.h"
-
 
 //#define HDCP_IRQ_HANDLING		// kernel 3.18
 
 #define HDCP_DEV_NAME "hdcp"
 //#define HDCP_DEV_MAJOR 233
 #define HDCP_DEV_MINOR 0
-
-// int hdcp_api_initialize(void) { return 0; }
-// int hdcp_api_cmd_process(unsigned int cmd, unsigned long arg) { return 0; }
-// int hdcp_api_status_chk(unsigned char flag) { return 0; }
-// unsigned int hdcp_api_poll_chk(struct file *file, poll_table *wait) { return
-// 0; } int hdcp_api_open(void) { return 0; } int hdcp_api_close(void) { return
-// 0; }
 
 static long hdcp_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
@@ -64,21 +54,16 @@ static long hdcp_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 static irqreturn_t hdcp_handler(int irq, void *dev_id)
 {
 	unsigned char flag;
-
 	// check HDCP INT
 	flag = hdcp_readb(HDMI_SS_INTC_FLAG);
 
 	if (flag & (1 << HDMI_IRQ_HDCP)) {
-		// ILOG("%s : flag = %d\n", __func__, flag)
-		// check HDCP Status
 		flag = hdcp_readb(HDMI_STATUS);
-
 		if (flag)
 			hdcp_api_status_chk(flag);
 	} else {
 		return IRQ_NONE;
 	}
-
 	return IRQ_HANDLED;
 }
 #endif
@@ -86,23 +71,19 @@ static irqreturn_t hdcp_handler(int irq, void *dev_id)
 static unsigned int hdcp_poll(struct file *file, poll_table *wait)
 {
 	unsigned int mask = 0;
-
 	mask = hdcp_api_poll_chk(file, wait);
-
 	return mask;
 }
 
 static int hdcp_release(struct inode *inode, struct file *filp)
 {
 	hdcp_api_close();
-
 	return 0;
 }
 
 static int hdcp_open(struct inode *inode, struct file *filp)
 {
 	hdcp_api_open();
-
 	return 0;
 }
 
@@ -124,14 +105,12 @@ static struct miscdevice hdcp_misc_device = {
 static __init int hdcp_init(void)
 {
 	int ret = -ENODEV;
-
 	ret = misc_register(&hdcp_misc_device);
 	if (ret < 0) {
 		ILOG("%s: Couldn't register device - (ret: 0x%X)\n",
 		     HDCP_DEV_NAME, ret);
 		return -EBUSY;
 	}
-
 #if defined(HDCP_IRQ_HANDLING)
 	ret = request_irq(
 		IRQ_HDMI, hdcp_handler, IRQF_SHARED, HDCP_DEV_NAME,
@@ -142,23 +121,18 @@ static __init int hdcp_init(void)
 		return -EIO;
 	}
 #endif
-
 	hdcp_api_initialize();
-
-	ILOG("init() ret: %d (0x%X)\n", ret, ret);
-
+	TRACE;
 	return 0;
 }
 
 static __exit void hdcp_exit(void)
 {
-	ILOG("exit()\n");
-
 #if defined(HDCP_IRQ_HANDLING)
 	free_irq(IRQ_HDMI, hdcp_handler);
 #endif
-
 	misc_deregister(&hdcp_misc_device);
+	TRACE;
 }
 
 module_init(hdcp_init);
