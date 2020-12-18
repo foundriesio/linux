54c3b1fe68a79a4225f40082512e08c87bbf58aa
Pavel Begunkov <asml.silence@gmail.com>
2020-12-18 13:12:21 +0000

    io_uring: close a small race gap for files cancel
    BugLink: https://bugs.launchpad.net/bugs/1914472
    
    commit dfea9fce29fda6f2f91161677e0e0d9b671bc099 upstream.
    
    The purpose of io_uring_cancel_files() is to wait for all requests
    matching ->files to go/be cancelled. We should first drop files of a
    request in io_req_drop_files() and only then make it undiscoverable for
    io_uring_cancel_files.
    
    First drop, then delete from list. It's ok to leave req->id->files
    dangling, because it's not dereferenced by cancellation code, only
    compared against. It would potentially go to sleep and be awaken by
    following in io_req_drop_files() wake_up().
    
    Fixes: 0f2122045b946 ("io_uring: don't rely on weak ->files references")
    Cc: <stable@vger.kernel.org> # 5.5+
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: William Breathitt Gray <william.gray@canonical.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1449,15 +1449,15 @@ static void io_req_drop_files(struct io_kiocb *req)
 	struct io_ring_ctx *ctx = req->ctx;
 	unsigned long flags;
 
-	spin_lock_irqsave(&ctx->inflight_lock, flags);
-	list_del(&req->inflight_entry);
-	if (waitqueue_active(&ctx->inflight_wait))
-		wake_up(&ctx->inflight_wait);
-	spin_unlock_irqrestore(&ctx->inflight_lock, flags);
-	req->flags &= ~REQ_F_INFLIGHT;
 	put_files_struct(req->work.files);
 	put_nsproxy(req->work.nsproxy);
+	spin_lock_irqsave(&ctx->inflight_lock, flags);
+	list_del(&req->inflight_entry);
+	spin_unlock_irqrestore(&ctx->inflight_lock, flags);
+	req->flags &= ~REQ_F_INFLIGHT;
 	req->work.files = NULL;
+	if (waitqueue_active(&ctx->inflight_wait))
+		wake_up(&ctx->inflight_wait);
 }
 
 static void __io_req_aux_free(struct io_kiocb *req)
