4b88e8c2c48cffd14e523c77451cdf27fdea7239
박호식 (Hosik Park) <hspark@telechips.com>
2020-12-23 15:21:44 +0900

    [fea] [SDRIPC] Support SDRAUDIPC(audio data transfer from sub to main) [upg] [SDRIPC] Apply codesonar ruleset
    - Allocate shared memory for audio transfer.
    - Add ioctl to get info of shared memory
    
    Change-Id: I45fa85909c4264b8fcca8ac397acbf525a8b7776

--- include/linux/tcc_sdr_ipc.h
+++ include/linux/tcc_sdr_ipc.h
@@ -6,7 +6,15 @@
 #ifndef TCC_SDR_IPC_H
 #define TCC_SDR_IPC_H
 
-#define	SDRIPC_MAGIC		('S')
-#define	IOCTL_SDRPIC_SET_ID		_IO(SDRIPC_MAGIC, 0) /* range : 0 ~ 15 */
+#include <asm/ioctl.h>
+
+#define	SDRIPC_MAGIC ('S')
+#define	IOCTL_SDRPIC_SET_ID         (_IO(SDRIPC_MAGIC,  0)) /* range : 0 ~ 15 */
+#define	IOCTL_SDRPIC_GET_SHM_BUFFER (_IO(SDRIPC_MAGIC, 11))
+
+typedef struct {
+	uint64_t phyaddr;
+	int32_t  size;
+} sdripc_get_shm_param;
 
 #endif
