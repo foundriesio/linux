d7dc46d7ec5a9753f49ec2e4f963b736e8887e63
Dinghao Liu <dinghao.liu@zju.edu.cn>
2020-12-26 14:15:03 +0800

    scsi: scsi_debug: Fix memleak in scsi_debug_init()
    BugLink: https://bugs.launchpad.net/bugs/1917964
    
    [ Upstream commit 3b01d7ea4dae907d34fa0eeb3f17bacd714c6d0c ]
    
    When sdeb_zbc_model does not match BLK_ZONED_NONE, BLK_ZONED_HA or
    BLK_ZONED_HM, we should free sdebug_q_arr to prevent memleak. Also there is
    no need to execute sdebug_erase_store() on failure of sdeb_zbc_model_str().
    
    Link: https://lore.kernel.org/r/20201226061503.20050-1-dinghao.liu@zju.edu.cn
    Acked-by: Douglas Gilbert <dgilbert@interlog.com>
    Signed-off-by: Dinghao Liu <dinghao.liu@zju.edu.cn>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/scsi/scsi_debug.c
+++ drivers/scsi/scsi_debug.c
@@ -6635,7 +6635,7 @@ static int __init scsi_debug_init(void)
 		k = sdeb_zbc_model_str(sdeb_zbc_model_s);
 		if (k < 0) {
 			ret = k;
-			goto free_vm;
+			goto free_q_arr;
 		}
 		sdeb_zbc_model = k;
 		switch (sdeb_zbc_model) {
@@ -6648,7 +6648,8 @@ static int __init scsi_debug_init(void)
 			break;
 		default:
 			pr_err("Invalid ZBC model\n");
-			return -EINVAL;
+			ret = -EINVAL;
+			goto free_q_arr;
 		}
 	}
 	if (sdeb_zbc_model != BLK_ZONED_NONE) {
