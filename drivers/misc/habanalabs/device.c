7b0436ec5ca6e3cb441060c92018c64b4e10e180
Dinghao Liu <dinghao.liu@zju.edu.cn>
2020-12-26 15:27:14 +0800

    habanalabs: Fix memleak in hl_device_reset
    BugLink: https://bugs.launchpad.net/bugs/1916960
    
    [ Upstream commit b000700d6db50c933ce8b661154e26cf4ad06dba ]
    
    When kzalloc() fails, we should execute hl_mmu_fini()
    to release the MMU module. It's the same when
    hl_ctx_init() fails.
    
    Signed-off-by: Dinghao Liu <dinghao.liu@zju.edu.cn>
    Reviewed-by: Oded Gabbay <ogabbay@kernel.org>
    Signed-off-by: Oded Gabbay <ogabbay@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/misc/habanalabs/device.c
+++ drivers/misc/habanalabs/device.c
@@ -980,6 +980,7 @@ again:
 						GFP_KERNEL);
 		if (!hdev->kernel_ctx) {
 			rc = -ENOMEM;
+			hl_mmu_fini(hdev);
 			goto out_err;
 		}
 
@@ -991,6 +992,7 @@ again:
 				"failed to init kernel ctx in hard reset\n");
 			kfree(hdev->kernel_ctx);
 			hdev->kernel_ctx = NULL;
+			hl_mmu_fini(hdev);
 			goto out_err;
 		}
 	}
