3e7401ad625eb958027f0d52455be533d1666314
김호연지기 (Jigi Kim) <jigi.kim@telechips.com>
2020-12-29 20:37:29 +0900

    [ref] tcc: bootstage: Adopt Telechips coding rule (CodeSonar)
    Linked Commit: N/A
    ITS: SD805XL1-1281
    
    Change-Id: I01c745245b8b2a4055a9dc5abae00a758ea02fef

--- drivers/soc/tcc/bootstage.c
+++ drivers/soc/tcc/bootstage.c
@@ -4,7 +4,6 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/arm-smccc.h>
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
@@ -13,8 +12,8 @@
 #include <linux/suspend.h>
 #include <soc/tcc/tcc-sip.h>
 
-#define bootstage_pr_err(msg, err) \
-	pr_err("[ERROR][bootstage] Failed to %s. (err: %d)\n", (msg), err)
+#define bootstage_err(msg, err) \
+	(void)pr_err("[ERROR][bootstage] Failed to " msg " (err: %d)\n", (err))
 
 #if defined(CONFIG_ARCH_TCC805X)
 #define NR_BOOT_STAGES (25U)
@@ -58,14 +57,14 @@ static inline void add_boot_timestamp(void)
 {
 	struct arm_smccc_res res;
 
-	arm_smccc_smc(SIP_CHIP_ADD_BOOTTIME, 0, 0, 0, 0, 0, 0, 0, &res);
+	tcc_sip_chip(ADD_BOOTTIME, 0, 0, 0, 0, 0, 0, 0, &res);
 }
 
 static inline u32 get_boot_timestamp(ulong index)
 {
 	struct arm_smccc_res res;
 
-	arm_smccc_smc(SIP_CHIP_GET_BOOTTIME, index, 0, 0, 0, 0, 0, 0, &res);
+	tcc_sip_chip(GET_BOOTTIME, index, 0, 0, 0, 0, 0, 0, &res);
 
 	return (u32)res.a0;
 }
@@ -74,7 +73,7 @@ static inline u32 get_boot_timestamp_num(void)
 {
 	struct arm_smccc_res res;
 
-	arm_smccc_smc(SIP_CHIP_GET_BOOTTIME_NUM, 0, 0, 0, 0, 0, 0, 0, &res);
+	tcc_sip_chip(GET_BOOTTIME_NUM, 0, 0, 0, 0, 0, 0, 0, &res);
 
 	return (u32)res.a0;
 }
@@ -90,7 +89,12 @@ static inline void validate_boot_timestamp_num(struct seq_file *m, u32 num)
 static inline const char *u32_to_str_in_format(u32 num, char *format)
 {
 	u32 ths = 1000000U;
-	s32 idx = 0;
+	s32 idx;
+
+	if (format == NULL) {
+		/* XXX: Should not happen */
+		return NULL;
+	}
 
 	for (idx = 0; idx < 12; idx += 4) {
 		(void)sprintf(&format[idx], ",%03u", (num / ths) % 1000U);
@@ -114,39 +118,47 @@ static inline const char *u32_to_str_in_format(u32 num, char *format)
 
 static int bootstage_report_show(struct seq_file *m, void *v)
 {
-	u32 n, num, stamp, prev = 0;
+	u32 num;
+	u32 n;
+	u32 prev = 0;
 	s32 nr_actual = 0;
-	const char *fmt, *desc;
-	char buf[13]; /* ",000,000,000" */
 
 	num = get_boot_timestamp_num();
 	validate_boot_timestamp_num(m, num);
 
 	seq_printf(m, "Timer summary in microseconds (%u records):\n", num);
 
-	seq_printf(m, "%11s%11s  %s\n", "Mark", "Elapsed", "Stage");
-	seq_printf(m, "%11u%11u  %s\n", 0U, 0U, "reset");
+	/* format:           11s        11s      s */
+	seq_puts(m, "       Mark    Elapsed  Stage\n");
+	seq_puts(m, "          0          0  reset\n");
 
 	for (n = 0; n < num; n++) {
+		char buf[13]; /* ",000,000,000" */
+		const char *desc;
+		const char *fmt;
+		u32 stamp;
+
 		desc = (n < NR_BOOT_STAGES) ? bootstage_desc[n] : "-";
 		stamp = get_boot_timestamp(n);
 
-		if (stamp != 0U) {
-			fmt = u32_to_str_in_format(stamp, buf);
-			seq_printf(m, "%11s", fmt);
-
-			if (stamp >= prev) {
-				fmt = u32_to_str_in_format(stamp - prev, buf);
-			} else {
-				fmt = "overflow";
-			}
-			seq_printf(m, "%11s", fmt);
-
-			prev = stamp;
-			seq_printf(m, "  %s\n", desc);
-
-			++nr_actual;
+		if (stamp == 0U) {
+			/* Ignore printing stamp for skipped stages */
+			continue;
 		}
+
+		fmt = u32_to_str_in_format(stamp, buf);
+		seq_printf(m, "%11s", fmt);
+
+		if (stamp >= prev) {
+			fmt = u32_to_str_in_format(stamp - prev, buf);
+		} else {
+			fmt = "overflow";
+		}
+
+		seq_printf(m, "%11s  %s\n", fmt, desc);
+
+		prev = stamp;
+		++nr_actual;
 	}
 
 	seq_printf(m, "\n%d stages out of %u stages were proceeded. (%d stages skipped)\n",
@@ -159,9 +171,9 @@ DEFINE_SHOW_ATTRIBUTE(bootstage_report);
 
 static int bootstage_data_show(struct seq_file *m, void *v)
 {
-	u32 n, num;
-	u32 stamp, prev = 0;
-	const char *desc;
+	u32 num;
+	u32 n;
+	u32 prev = 0;
 
 	num = get_boot_timestamp_num();
 	validate_boot_timestamp_num(m, num);
@@ -169,6 +181,9 @@ static int bootstage_data_show(struct seq_file *m, void *v)
 	seq_puts(m, "reset,0\n");
 
 	for (n = 0; n < num; n++) {
+		const char *desc;
+		u32 stamp;
+
 		desc = (n < NR_BOOT_STAGES) ? bootstage_desc[n] : "-";
 		stamp = get_boot_timestamp(n);
 
@@ -211,7 +226,8 @@ static struct notifier_block bootstage_pm_notifier = {
 
 static struct proc_dir_entry *bootstage_procfs_init(void)
 {
-	struct proc_dir_entry *pdir, *pent;
+	struct proc_dir_entry *pdir;
+	struct proc_dir_entry *pent;
 
 	/* Create '/proc/bootstage' procfs directory */
 	pdir = proc_mkdir("bootstage", NULL);
@@ -259,14 +275,14 @@ static int bootstage_init(void)
 	pdir = bootstage_procfs_init();
 	if (pdir == NULL) {
 		ret = -ENOMEM;
-		(void)bootstage_pr_err("init 'proc/bootstage' procfs", ret);
+		bootstage_err("init 'proc/bootstage' procfs", ret);
 		goto procfs_init_error;
 	}
 
 	/* Register PM notifier for boot time until resumption */
 	ret = register_pm_notifier(&bootstage_pm_notifier);
 	if (ret != 0) {
-		(void)bootstage_pr_err("register pm notifier", ret);
+		bootstage_err("register pm notifier", ret);
 		goto pm_notifier_register_error;
 	}
 
