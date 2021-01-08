81c7476ad85f2db1fe7eafb5260e38daf8e165fa
Takashi Iwai <tiwai@suse.de>
2021-01-08 14:28:00 +0100

    drm: bail out of nouveau_channel_new if channel init fails (CVE-2020-25639 bsc#1176846).
    suse-commit: 55debf72f29474deb071971cc5308ff640f72cbb

--- drivers/gpu/drm/nouveau/nouveau_chan.c
+++ drivers/gpu/drm/nouveau/nouveau_chan.c
@@ -455,6 +455,7 @@ nouveau_channel_new(struct nouveau_drm *drm, struct nvif_device *device,
 	if (ret) {
 		NV_PRINTK(err, cli, "channel failed to initialise, %d\n", ret);
 		nouveau_channel_del(pchan);
+		goto done;
 	}
 
 done:
