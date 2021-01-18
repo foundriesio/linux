9b0653f10fd5f634cbb4351b8b2dc3a295987e60
김시아 (Sia Kim) <sia.kim@telechips.com>
2021-01-18 15:34:11 +0900

    Revert "[upg] Modify Copyright"
    This reverts commit a519b61de652840124c5fb0c9747805483207946.
    
    Change-Id: I2c8bed6ab1f4d9e30aa8c1ae0b2a3b09de74fbe7

--- drivers/video/fbdev/tcc-fb/fb.c
+++ drivers/video/fbdev/tcc-fb/fb.c
@@ -1,33 +1,21 @@
-/*******************************************************************************
-
- *
- * Copyright (c) 2009 Nuvoton technology corporation
- * All rights reserved.
+/*
+ * Copyright (C) 2008-2019 Telechips Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- *  Description:
- *    Nuvoton LCD Controller Driver
- *  Author:
- *    Wang Qiang (rurality.linux@gmail.com) 2009/12/11
-
-*******************************************************************************
-
-
-*   Modified by Telechips Inc.
- 
- 
-*   Modified date :
- 
- 
-*   Description :
-
-
-*******************************************************************************/
-
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see the file COPYING, or write
+ * to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/errno.h>
