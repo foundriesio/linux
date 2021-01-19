e5692876ae486adb437fa3f9cb90776a70f49e27
Yuchung Cheng <ycheng@google.com>
2021-01-19 11:26:19 -0800

    tcp: fix TCP socket rehash stats mis-accounting
    BugLink: https://bugs.launchpad.net/bugs/1917964
    
    commit 9c30ae8398b0813e237bde387d67a7f74ab2db2d upstream.
    
    The previous commit 32efcc06d2a1 ("tcp: export count for rehash attempts")
    would mis-account rehashing SNMP and socket stats:
    
      a. During handshake of an active open, only counts the first
         SYN timeout
    
      b. After handshake of passive and active open, stop updating
         after (roughly) TCP_RETRIES1 recurring RTOs
    
      c. After the socket aborts, over count timeout_rehash by 1
    
    This patch fixes this by checking the rehash result from sk_rethink_txhash.
    
    Fixes: 32efcc06d2a1 ("tcp: export count for rehash attempts")
    Signed-off-by: Yuchung Cheng <ycheng@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Link: https://lore.kernel.org/r/20210119192619.1848270-1-ycheng@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1909,10 +1909,13 @@ static inline void sk_set_txhash(struct sock *sk)
 	sk->sk_txhash = net_tx_rndhash();
 }
 
-static inline void sk_rethink_txhash(struct sock *sk)
+static inline bool sk_rethink_txhash(struct sock *sk)
 {
-	if (sk->sk_txhash)
+	if (sk->sk_txhash) {
 		sk_set_txhash(sk);
+		return true;
+	}
+	return false;
 }
 
 static inline struct dst_entry *
@@ -1935,11 +1938,9 @@ sk_dst_get(struct sock *sk)
 	return dst;
 }
 
-static inline void dst_negative_advice(struct sock *sk)
+static inline void __dst_negative_advice(struct sock *sk)
 {
 	struct dst_entry *ndst, *dst = __sk_dst_get(sk);
-
-	sk_rethink_txhash(sk);
 
 	if (dst && dst->ops->negative_advice) {
 		ndst = dst->ops->negative_advice(dst);
@@ -1950,6 +1951,12 @@ static inline void dst_negative_advice(struct sock *sk)
 			sk->sk_dst_pending_confirm = 0;
 		}
 	}
+}
+
+static inline void dst_negative_advice(struct sock *sk)
+{
+	sk_rethink_txhash(sk);
+	__dst_negative_advice(sk);
 }
 
 static inline void
