2ce4e86195d692d3195b1b39a7f5c152cfc07367
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2021-01-27 17:17:36 +0100

    RDMA/mlx5: Fix typo in enum name (bsc#1103991 FATE#326007).
    suse-commit: 78c65e2071111d6fc899f3337975d4e717c7aa4c

--- include/uapi/rdma/mlx5_user_ioctl_cmds.h
+++ include/uapi/rdma/mlx5_user_ioctl_cmds.h
@@ -160,7 +160,7 @@ enum mlx5_ib_create_flow_attrs {
 	MLX5_IB_ATTR_CREATE_FLOW_TAG,
 };
 
-enum mlx5_ib_destoy_flow_attrs {
+enum mlx5_ib_destroy_flow_attrs {
 	MLX5_IB_ATTR_DESTROY_FLOW_HANDLE = (1U << UVERBS_ID_NS_SHIFT),
 };
 
