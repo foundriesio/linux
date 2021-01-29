e3b1d6f1d372941f478160b83f06809f781341ea
육동훈 (DH Yook) <dhyook@telechips.com>
2021-01-29 10:20:38 +0900

    [ref] irq-pic: Adopt Telechips coding rules
    Adopt Telechips coding rules and kernel coding style.
    
    Linked Commit: None
    ITS: None
    
    Change-Id: If4f629325569b6ce0ae0968e9913fe306645e063

--- include/soc/tcc/irq.h
+++ include/soc/tcc/irq.h
@@ -9,20 +9,20 @@
 #include <linux/interrupt.h>
 
 
-#define PIC_GATE_STRGB      (u32)1
-#define PIC_GATE_CMB        (u32)2
-#define SMU_GATE_CA7        (u32)3
+#define PIC_GATE_STRGB      ((u32)1)
+#define PIC_GATE_CMB        ((u32)2)
+#define SMU_GATE_CA7        ((u32)3)
 
 /*
  * These _ca7 suffixed routines are using from Cortex-A7 only
  */
-extern int tcc_irq_set_polarity_ca7(u32 irq, u32 type);
-extern int tcc_irq_mask_ca7(u32 irq);       /* disable ca7 interrupt */
-extern int tcc_irq_unmask_ca7(u32 irq);    /* enable ca7 interrupt */
+extern s32 tcc_irq_set_polarity_ca7(u32 irq, u32 type);
+extern s32 tcc_irq_mask_ca7(u32 irq);       /* disable ca7 interrupt */
+extern s32 tcc_irq_unmask_ca7(u32 irq);    /* enable ca7 interrupt */
 
-extern int tcc_irq_set_polarity(struct irq_data *irqd, u32 type);
-extern int tcc_irq_mask_cm4(u32 irq, u32 bus);
-extern int tcc_irq_unmask_cm4(u32 irq, u32 bus);
+extern s32 tcc_irq_set_polarity(struct irq_data *irqd, u32 type);
+extern s32 tcc_irq_mask_cm4(u32 irq, u32 bus);
+extern s32 tcc_irq_unmask_cm4(u32 irq, u32 bus);
 
 
 #endif /* TCC_IRQ_H */
