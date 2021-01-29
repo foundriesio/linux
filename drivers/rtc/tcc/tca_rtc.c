88e26b14aba40b02076c58fcfdd5a0b140b68728
육동훈 (DH Yook) <dhyook@telechips.com>
2021-01-29 10:18:33 +0900

    [ref] tcc_rtc: Adopt Telechips coding rules
    Adopt Telechips coding rules and kernel coding style.
    
    Linked Commit: None
    ITS: SD805XL1-957
    
    Change-Id: Idd9cb6b0fbc8e97a4cbc3502856bfdb33b93191a

--- drivers/rtc/tcc/tca_rtc.c
+++ drivers/rtc/tcc/tca_rtc.c
@@ -5,13 +5,17 @@
 #include <linux/io.h>
 #include "tca_rtc.h"
 
-#define rtc_readl   __raw_readl
-#define rtc_writel  __raw_writel
+#define rtc_readl       (__raw_readl)
+#define rtc_writel      (__raw_writel)
 
 void tca_rtc_gettime(void __iomem *pRTC, struct rtctime *pTime)
 {
-	unsigned int data;
-	unsigned short seconds;
+	u32 data;
+	u32 seconds;
+
+	if (pTime == NULL) {
+		return;
+	}
 
 	/* Enable RTC control */
 	rtc_writel(rtc_readl(pRTC + RTCCON) | Hw1, pRTC + RTCCON);
@@ -19,29 +23,29 @@ void tca_rtc_gettime(void __iomem *pRTC, struct rtctime *pTime)
 	rtc_writel(rtc_readl(pRTC + RTCCON) & ~Hw4, pRTC + RTCCON);
 
 	do {
-		data = rtc_readl(pRTC + BCDSEC) & 0x7f;
+		data = rtc_readl(pRTC + BCDSEC) & (u32)0x7f;
 		seconds = FROM_BCD(data);
 
-		data = rtc_readl(pRTC + BCDYEAR) & 0xFFFF;
-		pTime->wYear = (FROM_BCD(data >> 8) * 100)
-				+ FROM_BCD(data & 0x00FF); /* YearFix : hjbae */
+		data = rtc_readl(pRTC + BCDYEAR) & (u32)0xFFFF;
+		pTime->wYear = (FROM_BCD(data >> (u32)8) * (u32)100)
+				+ FROM_BCD(data & (u32)0xFF);
 
-		data = rtc_readl(pRTC + BCDMON) & 0x1f;
+		data = rtc_readl(pRTC + BCDMON) & (u32)0x1f;
 		pTime->wMonth = FROM_BCD(data);
 
-		data = rtc_readl(pRTC + BCDDATE) & 0x3f;
+		data = rtc_readl(pRTC + BCDDATE) & (u32)0x3f;
 		pTime->wDay = FROM_BCD(data);
 
-		data = rtc_readl(pRTC + BCDDAY) & 0x07;
+		data = rtc_readl(pRTC + BCDDAY) & (u32)0x07;
 		pTime->wDayOfWeek = FROM_BCD(data);
 
-		data = rtc_readl(pRTC + BCDHOUR) & 0x3f;
+		data = rtc_readl(pRTC + BCDHOUR) & (u32)0x3f;
 		pTime->wHour = FROM_BCD(data);
 
-		data = rtc_readl(pRTC + BCDMIN) & 0x7f;
+		data = rtc_readl(pRTC + BCDMIN) & (u32)0x7f;
 		pTime->wMinute = FROM_BCD(data);
 
-		data = rtc_readl(pRTC + BCDSEC) & 0x7f;
+		data = rtc_readl(pRTC + BCDSEC) & (u32)0x7f;
 		pTime->wSecond = FROM_BCD(data);
 
 		pTime->wMilliseconds = 0;
@@ -53,6 +57,10 @@ void tca_rtc_gettime(void __iomem *pRTC, struct rtctime *pTime)
 
 void tca_rtc_settime(void __iomem *pRTC, struct rtctime *pTime)
 {
+	if (pTime == NULL) {
+		return;
+	}
+
 	/* RTC Register write enabled */
 	rtc_writel(rtc_readl(pRTC + RTCCON) | Hw1, pRTC + RTCCON);
 	/* Interrupt Block Write Enable */
@@ -93,9 +101,9 @@ void tca_rtc_settime(void __iomem *pRTC, struct rtctime *pTime)
 	rtc_writel(rtc_readl(pRTC + RTCCON) & ~Hw1, pRTC + RTCCON);
 }
 
-unsigned int tca_rtc_checkvalidtime(void __iomem *devbaseaddresss)
+s32 tca_rtc_checkvalidtime(void __iomem *devbaseaddresss)
 {
-	unsigned int IsNeedMending;
+	s32 IsNeedMending;
 	struct rtctime pTimeTest;
 
 	tca_rtc_gettime(devbaseaddresss, &pTimeTest);
@@ -106,37 +114,37 @@ unsigned int tca_rtc_checkvalidtime(void __iomem *devbaseaddresss)
 	IsNeedMending = 0;
 
 	/* Second */
-	if (pTimeTest.wSecond > 59) {
+	if (pTimeTest.wSecond > (u32)59) {
 		IsNeedMending = 1;
 	}
 
 	/* Minute */
-	if (pTimeTest.wMinute > 59) {
+	if (pTimeTest.wMinute > (u32)59) {
 		IsNeedMending = 1;
 	}
 
 	/* Hour */
-	if (pTimeTest.wHour > 23) {
+	if (pTimeTest.wHour > (u32)23) {
 		IsNeedMending = 1;
 	}
 
 	/* Date, 1~31 */
-	if ((pTimeTest.wDay < 1) || (pTimeTest.wDay > 31)) {
+	if ((pTimeTest.wDay < (u32)1) || (pTimeTest.wDay > (u32)31)) {
 		IsNeedMending = 1;
 	}
 
 	/* Day, Sunday ~ Saturday */
-	if (pTimeTest.wDayOfWeek > 6) {
+	if (pTimeTest.wDayOfWeek > (u32)6) {
 		IsNeedMending = 1;
 	}
 
 	/* Month */
-	if ((pTimeTest.wMonth < 1) || (pTimeTest.wMonth > 12)) {
+	if ((pTimeTest.wMonth < (u32)1) || (pTimeTest.wMonth > (u32)12)) {
 		IsNeedMending = 1;
 	}
 
 	/* Year */
-	if (pTimeTest.wYear < 1980 || pTimeTest.wYear > 2037) {
+	if ((pTimeTest.wYear < (u32)1980) || (pTimeTest.wYear > (u32)2037)) {
 		IsNeedMending = 1;
 	}
 
--- drivers/rtc/tcc/tca_rtc.h
+++ drivers/rtc/tcc/tca_rtc.h
@@ -5,91 +5,88 @@
 #ifndef TCA_RTC_H
 #define TCA_RTC_H
 
-#define FROM_BCD(n)     ((((n) >> 4) * 10) + ((n) & 0xf))
-#define TO_BCD(n)       ((((n) / 10) << 4) | ((n) % 10))
-#define RTC_YEAR_DATUM  1980
+#define FROM_BCD(n)     ((((n) >> (u32)4) * (u32)10) + ((n) & (u32)0xf))
+#define TO_BCD(n)       ((((n) / (u32)10) << (u32)4) | ((n) % (u32)10))
+#define RTC_YEAR_DATUM  (1980)
 
-#define Hw31		0x80000000
-#define Hw30		0x40000000
-#define Hw29		0x20000000
-#define Hw28		0x10000000
-#define Hw27		0x08000000
-#define Hw26		0x04000000
-#define Hw25		0x02000000
-#define Hw24		0x01000000
-#define Hw23		0x00800000
-#define Hw22		0x00400000
-#define Hw21		0x00200000
-#define Hw20		0x00100000
-#define Hw19		0x00080000
-#define Hw18		0x00040000
-#define Hw17		0x00020000
-#define Hw16		0x00010000
-#define Hw15		0x00008000
-#define Hw14		0x00004000
-#define Hw13		0x00002000
-#define Hw12		0x00001000
-#define Hw11		0x00000800
-#define Hw10		0x00000400
-#define Hw9		0x00000200
-#define Hw8		0x00000100
-#define Hw7		0x00000080
-#define Hw6		0x00000040
-#define Hw5		0x00000020
-#define Hw4		0x00000010
-#define Hw3		0x00000008
-#define Hw2		0x00000004
-#define Hw1		0x00000002
-#define Hw0		0x00000001
+#define Hw31            ((u32)0x80000000)
+#define Hw30            ((u32)0x40000000)
+#define Hw29            ((u32)0x20000000)
+#define Hw28            ((u32)0x10000000)
+#define Hw27            ((u32)0x08000000)
+#define Hw26            ((u32)0x04000000)
+#define Hw25            ((u32)0x02000000)
+#define Hw24            ((u32)0x01000000)
+#define Hw23            ((u32)0x00800000)
+#define Hw22            ((u32)0x00400000)
+#define Hw21            ((u32)0x00200000)
+#define Hw20            ((u32)0x00100000)
+#define Hw19            ((u32)0x00080000)
+#define Hw18            ((u32)0x00040000)
+#define Hw17            ((u32)0x00020000)
+#define Hw16            ((u32)0x00010000)
+#define Hw15            ((u32)0x00008000)
+#define Hw14            ((u32)0x00004000)
+#define Hw13            ((u32)0x00002000)
+#define Hw12            ((u32)0x00001000)
+#define Hw11            ((u32)0x00000800)
+#define Hw10            ((u32)0x00000400)
+#define Hw9             ((u32)0x00000200)
+#define Hw8             ((u32)0x00000100)
+#define Hw7             ((u32)0x00000080)
+#define Hw6             ((u32)0x00000040)
+#define Hw5             ((u32)0x00000020)
+#define Hw4             ((u32)0x00000010)
+#define Hw3             ((u32)0x00000008)
+#define Hw2             ((u32)0x00000004)
+#define Hw1             ((u32)0x00000002)
+#define Hw0             ((u32)0x00000001)
 
-#define RTCCON      0x00
-#define INTCON      0x04
-#define RTCALM      0x08
-#define ALMSEC      0x0C
-#define ALMMIN      0x10
-#define ALMHOUR     0x14
-#define ALMDATE     0x18
-#define ALMDAY      0x1C
-#define ALMMON      0x20
-#define ALMYEAR     0x24
-#define BCDSEC      0x28
-#define BCDMIN      0x2C
-#define BCDHOUR     0x30
-#define BCDDATE     0x34
-#define BCDDAY      0x38
-#define BCDMON      0x3C
-#define BCDYEAR     0x40
-#define RTCIM       0x44
-#define RTCPEND     0x48
-#define RTCSTR      0x4C
+#define RTCCON          ((u32)0x00)
+#define INTCON          ((u32)0x04)
+#define RTCALM          ((u32)0x08)
+#define ALMSEC          ((u32)0x0C)
+#define ALMMIN          ((u32)0x10)
+#define ALMHOUR         ((u32)0x14)
+#define ALMDATE         ((u32)0x18)
+#define ALMDAY          ((u32)0x1C)
+#define ALMMON          ((u32)0x20)
+#define ALMYEAR         ((u32)0x24)
+#define BCDSEC          ((u32)0x28)
+#define BCDMIN          ((u32)0x2C)
+#define BCDHOUR         ((u32)0x30)
+#define BCDDATE         ((u32)0x34)
+#define BCDDAY          ((u32)0x38)
+#define BCDMON          ((u32)0x3C)
+#define BCDYEAR         ((u32)0x40)
+#define RTCIM           ((u32)0x44)
+#define RTCPEND         ((u32)0x48)
+#define RTCSTR          ((u32)0x4C)
 
-#define BITSET(X, MASK)			((X) |= (u32)(MASK))
+#define BITSET(X, MASK)         ((X) |= (u32)(MASK))
 #define BITSCLR(X, SMASK, CMASK)	\
 	((X) = ((((u32)(X)) | ((u32)(SMASK))) & ~((u32)(CMASK))))
 #define BITCSET(X, CMASK, SMASK)	\
 	((X) = ((((u32)(X)) & ~((u32)(CMASK))) | ((u32)(SMASK))))
-#define BITCLR(X, MASK)			((X) &= ~((u32)(MASK)))
-#define BITXOR(X, MASK)			((X) ^= (u32)(MASK))
-#define ISZERO(X, MASK)			(!(((u32)(X)) & ((u32)(MASK))))
-#define	ISSET(X, MASK)			((u32)(X) & ((u32)(MASK)))
-
-//#define rtc_readl	__raw_readl
-//#define rtc_writel	__raw_writel
+#define BITCLR(X, MASK)         ((X) &= ~((u32)(MASK)))
+#define BITXOR(X, MASK)         ((X) ^= (u32)(MASK))
+#define ISZERO(X, MASK)         ((((u32)(X)) & ((u32)(MASK))) == (u32)0)
+#define	ISSET(X, MASK)          (((u32)(X) & ((u32)(MASK))) != (u32)0)
 
 struct rtctime {
-	unsigned int wYear;
-	unsigned int wMonth;
-	unsigned int wDayOfWeek;
-	unsigned int wDay;
-	unsigned int wHour;
-	unsigned int wMinute;
-	unsigned int wSecond;
-	unsigned int wMilliseconds;
+	u32 wYear;
+	u32 wMonth;
+	u32 wDayOfWeek;
+	u32 wDay;
+	u32 wHour;
+	u32 wMinute;
+	u32 wSecond;
+	u32 wMilliseconds;
 };
 
 void tca_rtc_gettime(void *pRTC, struct rtctime *pTime);
 void tca_rtc_settime(void *pRTC, struct rtctime *pTime);
-unsigned int	tca_rtc_checkvalidtime(void *devbaseaddresss);
+s32 tca_rtc_checkvalidtime(void __iomem *devbaseaddresss);
 
 #ifdef __cplusplus
 extern
@@ -101,4 +98,3 @@ extern
 #endif
 
 #endif /* TCA_RTC_H */
-
