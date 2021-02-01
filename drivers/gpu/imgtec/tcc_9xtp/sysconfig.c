161d7bf81d3ed46f814c41157184711514d0b166
Hyunae Park <whitebbb@telechips.com>
2021-02-01 08:39:00 +0000

    [fea][PowerVR-9XTP GPU] Support Dynamic Driver mode on AutoVZ
    Change-Id: I53dff505891970a8fc8855db2097f6483d78125b
    (cherry picked from commit c08cd89a26b546fd43f69dbf3922e4643b371351)

--- drivers/gpu/imgtec/tcc_9xtp/sysconfig.c
+++ drivers/gpu/imgtec/tcc_9xtp/sysconfig.c
@@ -48,6 +48,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "physheap.h"
 #include "tcc_9xtp_init.h"
 #include "pvrsrv.h"
+#include <linux/of.h>
+#if defined (SUPPORT_COLD_RESET)
+#include <linux/soc/telechips/tcc_sc_protocol.h>
+#endif
 
 
 typedef enum _PHYS_HEAP_IDX_
@@ -109,6 +113,152 @@ void UMAPhysHeapDevPAddrToCpuPAddr(IMG_HANDLE hPrivData,
 	}
 }
 
+#if defined(SUPPORT_DYNAMIC_DRIVER_MODE)
+
+#if (RGX_NUM_OS_SUPPORTED < 2) || !defined(SUPPORT_AUTOVZ)
+#error "SUPPORT_DYNAMIC_DRIVER_MODE works only with an AutoVz-enabled driver."
+#endif
+
+#if !defined(SUPPORT_AUTOVZ_HW_REGS)
+#error "SUPPORT_DYNAMIC_DRIVER_MODE requires that hardware register be used for FW-KM connection tracking under AutoVz."
+#endif
+
+typedef enum _CORE_ID_
+{
+	CORE_SUB		= 10,
+	CORE_MAIN		= 20
+} CORE_ID;
+
+typedef struct  _HOST_ARBITRATION_CTRL__
+{
+	volatile IMG_UINT32 ui32SubCoreFlag;
+	volatile IMG_UINT32 ui32MainCoreFlag;
+	volatile IMG_UINT32 ui32Turn;
+	volatile IMG_UINT32 ui32HostCoreID;
+} HOST_ARBITRATION_CTRL;
+
+void __iomem *gpvRegBankOSID1;
+HOST_ARBITRATION_CTRL *gpsHostArbitrationCtrl;
+
+static CORE_ID GetCoreID(void)
+{
+	/* We assume that Subcore is always started by default as Host and Maincore as Guest.
+	 * By checking the default DriverMode values, a driver can determine on which core it runs. */
+	IMG_UINT32 ui32AppHintDriverMode = GetApphintDriverMode();
+pr_info("PVRSRV_VZ_APPHINT_MODE:%d\n", PVRSRV_VZ_APPHINT_MODE(ui32AppHintDriverMode));
+
+	return (PVRSRV_VZ_APPHINT_MODE(ui32AppHintDriverMode) == DRIVER_MODE_HOST) ? CORE_SUB : CORE_MAIN;
+}
+
+static void DeclareCoreAsHost(void)
+{
+	/* Save the ID of the current core into SCRATCH3 register belonging to OSID 7 reg bank */
+	gpsHostArbitrationCtrl->ui32HostCoreID = GetCoreID();
+}
+
+static IMG_BOOL DriverWasHostPreviously(void)
+{
+pr_info("gpsHostArbitrationCtrl->ui32HostCoreID:%d, %d\n", gpsHostArbitrationCtrl->ui32HostCoreID, GetCoreID());
+	return (gpsHostArbitrationCtrl->ui32HostCoreID == GetCoreID());
+}
+
+static IMG_BOOL HostInitialized(void)
+{
+	return (gpsHostArbitrationCtrl->ui32HostCoreID == CORE_MAIN) || 
+			 (gpsHostArbitrationCtrl->ui32HostCoreID == CORE_SUB);
+}
+
+static IMG_BOOL ShouldEnterHostMode(void)
+{
+
+	pr_err("   >>>   %s: start: SubCoreFlag = %u; MainCoreFlag = %u; Turn = %u; HostCoreID = %u;", __func__,
+			gpsHostArbitrationCtrl->ui32SubCoreFlag, gpsHostArbitrationCtrl->ui32MainCoreFlag, gpsHostArbitrationCtrl->ui32Turn, gpsHostArbitrationCtrl->ui32HostCoreID);
+	if (PVRSRV_VZ_MODE_IS(HOST) || DriverWasHostPreviously())
+	{
+		pr_err("    >>   %s: TRUE (default host mode = %u, DriverWasHost = %u) \n", __func__, PVRSRV_VZ_MODE_IS(HOST), DriverWasHostPreviously());
+		return IMG_TRUE;
+	}
+	else
+	{
+		IMG_UINT32 loopcount = 0;
+
+		while(OSReadHWReg32(gpvRegBankOSID1, RGX_CR_OS0_SCRATCH3) == RGXFW_CONNECTION_FW_OFFLINE)
+		{
+			OSWaitus(1000000);
+			loopcount++;
+			pr_err("    >>   %s: Guest waiting of firmware to come online ... %u \n", __func__, loopcount);
+			if (loopcount == 5)
+				break;
+		}
+		pr_err("    >>   %s: Default Guest: %s ", __func__, (loopcount == 5) ? "timed out waiting for firmware, becoming Host" : "fw is online, staying Guest");
+		return (loopcount == 5);
+	}
+
+	pr_err("   >>>   %s: finish: SubCoreFlag = %u; MainCoreFlag = %u; Turn = %u; HostCoreID = %u;", __func__,
+			gpsHostArbitrationCtrl->ui32SubCoreFlag, gpsHostArbitrationCtrl->ui32MainCoreFlag, gpsHostArbitrationCtrl->ui32Turn, gpsHostArbitrationCtrl->ui32HostCoreID);
+}
+
+/* the Enter/ExitVzCriticalSection functions are an implementation of the Peterson's concurrency algorithm,
+ * and ensure that write access to Host Arbitration Control structure is mutually exclusive between cores */
+static void EnterVzCriticalSection(void)
+{
+	IMG_UINT32 *aui32Flag = (IMG_UINT32*) gpsHostArbitrationCtrl;
+
+	IMG_UINT32 this = (GetCoreID() == CORE_SUB) ? 0 : 1;
+	IMG_UINT32 other = (this) ? 0 : 1;
+
+	aui32Flag[this] = 1;
+	gpsHostArbitrationCtrl->ui32Turn = other;
+
+	while ((aui32Flag[other] == 1) &&
+			(gpsHostArbitrationCtrl->ui32Turn == this))
+	{
+		OSWaitus(1000);
+		pr_err("   >>>   %s: waiting to enter critical section", __func__);
+	}
+	pr_err("   >>>   %s: critical section entered", __func__);
+}
+
+static void ExitVzCriticalSection(void)
+{
+	IMG_UINT32 *aui32Flag = (IMG_UINT32*) gpsHostArbitrationCtrl;
+	IMG_UINT32 this = (GetCoreID() == CORE_SUB) ? 0 : 1;
+
+	aui32Flag[this] = 0;
+	pr_err("   >>>   %s: critical section exited", __func__);
+}
+#endif /* SUPPORT_DYNAMIC_DRIVER_MODE */
+
+#if defined (SUPPORT_COLD_RESET)
+int setColdReset(void)
+{
+	int ret = 0;
+	struct platform_device *psDev;
+	struct device_node *fw_np;
+	const struct tcc_sc_fw_handle *handle;
+
+	psDev = to_platform_device((struct device *)gsDevCfg.pvOSDevice);
+	fw_np = of_parse_phandle(psDev->dev.of_node, "sc-firmware", 0);
+	if (!fw_np) {
+		dev_err(&psDev->dev, "[ERROR] No sc-firmware node\n");
+		return -ENODEV;
+	}
+
+	handle = tcc_sc_fw_get_handle(fw_np);
+	if(!handle) {
+		dev_err(&psDev->dev, "[ERROR]Failed to get handle\n");
+		return -ENODEV;
+	}
+	ret = handle->ops.reset_ops->cold_reset_request(handle);
+	if (ret != 0) {
+		dev_err(&psDev->dev, "[ERROR] failed to get protocol info\n");
+		return -ENODEV;
+	}
+	return ret;
+}
+EXPORT_SYMBOL(setColdReset);
+#endif
+
 PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 {
 	int iIrq;
@@ -128,7 +278,71 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 	gsDevCfg.pszVersion              = NULL;
 
 	/* Device setup information */
+
 	psDevMemRes = platform_get_resource(psDev, IORESOURCE_MEM, 0);
+
+#if defined (SUPPORT_DYNAMIC_DRIVER_MODE)
+	gpvRegBankOSID1 = (void __iomem *)ioremap(psDevMemRes->start + RGX_VIRTUALISATION_REG_SIZE_PER_OS, RGX_VIRTUALISATION_REG_SIZE_PER_OS);
+
+	/* under AutoVz, if the FW-KM state tracking is done using hardware registers, the FwConnectionCtl structures allocated at the start of
+	 * every VM's Firmware Config Heap are reserved but not used. FwConnectionCtl is allocated from a dedicated memory page that is never cleared.
+	 * In this special page belonging to OSID1 we will store the state variables required for arbitrating which VM gets to be Host. */
+	gpsHostArbitrationCtrl = (HOST_ARBITRATION_CTRL *)ioremap(IMG_UINT64_C(0x90000000)+RGX_FIRMWARE_RAW_HEAP_SIZE, 4096);
+
+	if (ShouldEnterHostMode())
+	{
+		EnterVzCriticalSection();
+
+		if (!HostInitialized() || DriverWasHostPreviously())
+		{
+			PVRSRV_VZ_MODE_SET(HOST);
+			iIrq = platform_get_irq(psDev, 0);
+			if (iIrq >= 0)
+				gsDevCfg.ui32IRQ  = (IMG_UINT32) iIrq;
+			else
+				PVR_DPF((PVR_DBG_WARNING, "%s: platform_get_irq failed (%d)", __func__, -iIrq));
+			DeclareCoreAsHost();
+		}
+		else
+		{
+			PVRSRV_VZ_MODE_SET(GUEST);
+			iIrq = platform_get_irq(psDev, 1);
+			if (iIrq >= 0)
+				gsDevCfg.ui32IRQ  = (IMG_UINT32) iIrq;
+			else
+				PVR_DPF((PVR_DBG_WARNING, "%s: platform_get_irq failed (%d)", __func__, -iIrq));	
+		}
+
+		ExitVzCriticalSection();
+	}
+	else {
+		iIrq = platform_get_irq(psDev, 1);
+                if (iIrq >= 0)
+	                gsDevCfg.ui32IRQ  = (IMG_UINT32) iIrq;
+                else
+			PVR_DPF((PVR_DBG_WARNING, "%s: platform_get_irq failed (%d)", __func__, -iIrq));
+		
+	}
+
+	pr_err("   >>>   %s: Driver will initialize with mode %s;", __func__, PVRSRV_VZ_MODE_IS(GUEST) ? "GUEST" : "HOST");
+
+	pr_err("   >>>   %s: SubCoreFlag = %u; MainCoreFlag = %u; Turn = %u; HostCoreID = %u;", __func__,
+			gpsHostArbitrationCtrl->ui32SubCoreFlag, gpsHostArbitrationCtrl->ui32MainCoreFlag, gpsHostArbitrationCtrl->ui32Turn, gpsHostArbitrationCtrl->ui32HostCoreID);
+	iounmap(gpvRegBankOSID1);
+#else /* SUPPORT_DYNAMIC_DRIVER_MODE */
+// this should be updated accordingly with the dtb files:
+	iIrq = platform_get_irq(psDev, 0);
+	if (iIrq >= 0)
+		gsDevCfg.ui32IRQ  = (IMG_UINT32) iIrq;
+	else
+	{
+		PVR_DPF((PVR_DBG_WARNING, "%s: platform_get_irq failed (%d)", __func__, -iIrq));
+		gsDevCfg.ui32IRQ = TCC_PowerVR_9XTP_IRQ;
+	}
+
+#endif 
+
+	
 	if (psDevMemRes)
 	{
 		gsDevCfg.sRegsCpuPBase.uiAddr = psDevMemRes->start + (PVRSRV_VZ_MODE_IS(GUEST) ? RGX_VIRTUALISATION_REG_SIZE_PER_OS : 0); /* NB: address offset required only if 2nd stage MMU translation is disabled */;
@@ -139,15 +353,6 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 		PVR_DPF((PVR_DBG_WARNING, "%s: platform_get_resource failed", __func__));
 		gsDevCfg.sRegsCpuPBase.uiAddr = TCC_PowerVR_9XTP_PBASE + (PVRSRV_VZ_MODE_IS(GUEST) ? RGX_VIRTUALISATION_REG_SIZE_PER_OS : 0); /* NB: address offset required only if 2nd stage MMU translation is disabled */;
 		gsDevCfg.ui32RegsSize         = TCC_PowerVR_9XTP_SIZE;
-	}
-
-	iIrq = platform_get_irq(psDev, 0);
-	if (iIrq >= 0)
-		gsDevCfg.ui32IRQ  = (IMG_UINT32) iIrq;
-	else
-	{
-		PVR_DPF((PVR_DBG_WARNING, "%s: platform_get_irq failed (%d)", __func__, -iIrq));
-		gsDevCfg.ui32IRQ = TCC_PowerVR_9XTP_IRQ;
 	}
 
 //	gsDevCfg.eCacheSnoopingMode     = PVRSRV_DEVICE_SNOOP_EMULATED;
@@ -210,13 +415,16 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 		gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions = OSAllocMem(sizeof(PHYS_HEAP_REGION));	
 		gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->uiSize = RGX_FIRMWARE_RAW_HEAP_SIZE;	
 		/* Set FW_CARVEOUT_IPA_BASE Address */
-		#if defined(CONFIG_TCC805X_CA53Q) //HOST
-		gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->sStartAddr.uiAddr = IMG_UINT64_C(0x80000000); // pmap_pvr_vz.base;	
-		gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->sCardBase.uiAddr = IMG_UINT64_C(0x80000000); // pmap_pvr_vz.base;	
-		#else
-                gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->sStartAddr.uiAddr = IMG_UINT64_C(0x80000000)+RGX_FIRMWARE_RAW_HEAP_SIZE;
-                gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->sCardBase.uiAddr = IMG_UINT64_C(0x80000000)+RGX_FIRMWARE_RAW_HEAP_SIZE;
-		#endif
+		if (PVRSRV_VZ_MODE_IS(HOST))
+		{
+			gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->sStartAddr.uiAddr = IMG_UINT64_C(0x90000000); // pmap_pvr_vz.base;
+			gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->sCardBase.uiAddr = IMG_UINT64_C(0x90000000); // pmap_pvr_vz.base;
+		}
+		else
+		{
+			gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->sStartAddr.uiAddr = IMG_UINT64_C(0x90000000)+RGX_FIRMWARE_RAW_HEAP_SIZE;
+			gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions->sCardBase.uiAddr = IMG_UINT64_C(0x90000000)+RGX_FIRMWARE_RAW_HEAP_SIZE;
+		}
 	}
 	/* Device's physical heaps */
 	gsDevCfg.pasPhysHeaps             = gsPhysHeapConfig;
@@ -233,9 +441,13 @@ PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
 	return PVRSRV_OK;
 }
 
+
 void SysDevDeInit(PVRSRV_DEVICE_CONFIG *psDevConfig)
 {
 	PVR_UNREFERENCED_PARAMETER(psDevConfig);
+#if defined (SUPPORT_DYNAMIC_DRIVER_MODE)
+	iounmap((void __iomem *)gpsHostArbitrationCtrl);
+#endif
 
 	OSFreeMem(gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions);
 	gsPhysHeapConfig[PHYS_HEAP_IDX_CARVEOUT].pasRegions = NULL;
