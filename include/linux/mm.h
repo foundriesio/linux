ce179f77ecd1f99f27834f6b0ba77a84674e617b
Pavel Tatashin <pasha.tatashin@soleen.com>
2021-02-03 15:34:48 +1100

    mm-gup-do-not-migrate-zero-page-fix
    add is_pinnable_page() stub
    
    Link: https://lkml.kernel.org/r/CA+CK2bBjC8=cRsL5VhWkcevPsqSXWhsANVjsFNMERLT8vWtiQw@mail.gmail.com
    Signed-off-by: Pavel Tatashin <pasha.tatashin@soleen.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1121,12 +1121,19 @@ static inline bool is_zone_movable_page(const struct page *page)
 	return page_zonenum(page) == ZONE_MOVABLE;
 }
 
+#ifdef CONFIG_MIGRATION
 /* MIGRATE_CMA and ZONE_MOVABLE do not allow pin pages */
 static inline bool is_pinnable_page(struct page *page)
 {
 	return !(is_zone_movable_page(page) || is_migrate_cma_page(page)) ||
 		is_zero_pfn(page_to_pfn(page));
 }
+#else
+static inline bool is_pinnable_page(struct page *page)
+{
+	return true;
+}
+#endif
 
 #ifdef CONFIG_DEV_PAGEMAP_OPS
 void free_devmap_managed_page(struct page *page);
