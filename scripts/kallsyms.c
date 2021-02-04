e25873c9235ceef104c371093783bcb53edb2d8f
Arnd Bergmann <arnd@arndb.de>
2021-02-04 16:29:47 +0100

    kallsyms: fix nonconverging kallsyms table with lld
    BugLink: https://bugs.launchpad.net/bugs/1920571
    
    [ Upstream commit efe6e3068067212b85c2d0474b5ee3b2d0c7adab ]
    
    ARM randconfig builds with lld sometimes show a build failure
    from kallsyms:
    
      Inconsistent kallsyms data
      Try make KALLSYMS_EXTRA_PASS=1 as a workaround
    
    The problem is the veneers/thunks getting added by the linker extend
    the symbol table, which in turn leads to more veneers being needed,
    so it may take a few extra iterations to converge.
    
    This bug has been fixed multiple times before, but comes back every time
    a new symbol name is used. lld uses a different set of identifiers from
    ld.bfd, so the additional ones need to be added as well.
    
    I looked through the sources and found that arm64 and mips define similar
    prefixes, so I'm adding those as well, aside from the ones I observed. I'm
    not sure about powerpc64, which seems to already be handled through a
    section match, but if it comes back, the "__long_branch_" and "__plt_"
    prefixes would have to get added as well.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- scripts/kallsyms.c
+++ scripts/kallsyms.c
@@ -111,6 +111,12 @@ static bool is_ignored_symbol(const char *name, char type)
 		".LASANPC",		/* s390 kasan local symbols */
 		"__crc_",		/* modversions */
 		"__efistub_",		/* arm64 EFI stub namespace */
+		"__AArch64ADRPThunk_",	/* arm64 lld */
+		"__ARMV5PILongThunk_",	/* arm lld */
+		"__ARMV7PILongThunk_",
+		"__ThumbV7PILongThunk_",
+		"__LA25Thunk_",		/* mips lld */
+		"__microLA25Thunk_",
 		NULL
 	};
 
