ce6e4842e5a44b6a17311c929a1911576ee8d4b3
한길종 (Jay HAN) <jjongspi@telechips.com>
2021-02-05 19:26:46 +0900

    [upg] Improve results of kernel coding style for video-input part modules
    ITS: BDCA-95
    
    Change-Id: If0e72855c72f32312c87e7d5c122031b123ab1a0

--- include/dt-bindings/pmap/tcc805x/pmap-tcc805x-linux-ivi-subcore-a53q-display.h
+++ include/dt-bindings/pmap/tcc805x/pmap-tcc805x-linux-ivi-subcore-a53q-display.h
@@ -37,9 +37,9 @@
 //****************************** Calculation ******************************/
 #undef SZ_1MB
 #define SZ_1MB				(1024*1024)
-#define ARRAY_16MBYTE(x)		((((x) + ((16*SZ_1MB)-1))>> 24) << 24)
-#define ARRAY_MBYTE(x)			((((x) + (SZ_1MB-1))>> 20) << 20)
-#define ARRAY_256KBYTE(x)		((((x) + ((SZ_1MB/4)-1))>> 18) << 18)
+#define ARRAY_16MBYTE(x)		((((x) + ((16*SZ_1MB)-1)) >> 24) << 24)
+#define ARRAY_MBYTE(x)			((((x) + (SZ_1MB-1)) >> 20) << 20)
+#define ARRAY_256KBYTE(x)		((((x) + ((SZ_1MB/4)-1)) >> 18) << 18)
 
 #define PMAP_SIZE_FB_VIDEO		\
 	ARRAY_MBYTE(PRIMARY_FRAMEBUFFER_WIDTH * \
@@ -60,7 +60,7 @@
 	ARRAY_MBYTE((PRIMARY_FRAMEBUFFER_WIDTH * 4) * \
 		PRIMARY_FRAMEBUFFER_HEIGHT * 4 * 4)
 #define PMAP_SIZE_CAMERA_VIQE		\
-	ARRAY_MBYTE((CAMERA_INPUT_WIDTH * (CAMERA_INPUT_HEIGHT / 2 ) * 4 * \
+	ARRAY_MBYTE((CAMERA_INPUT_WIDTH * (CAMERA_INPUT_HEIGHT / 2) * 4 * \
 		FRAMES_CAMERA_VIQE) * 3 / 2)
 #define PMAP_SIZE_CAMERA_PREVIEW0	\
 	ARRAY_MBYTE(CAMERA_OUTPUT_WIDTH * CAMERA_OUTPUT_HEIGHT * 4 * \
