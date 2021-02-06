4e78099d0013586a7d93d6f1f77bc92d64c27b01
김종현B (Jonghyun Kim) <jhkim34@telechips.com>
2021-02-06 18:06:55 +0900

    [bug] tcc805x : Fix gmac resume problem
    When gmac driver resume, clock and gpio setting should be reinitialized.
    Add misc driver for mii utility.
    Add OSS phrase
    
    ITS: SD805XL-1138
    Linked Commit: N/A
    
    Change-Id: Ie1650edd795838370d508a31e356ef4cb5f55a66

--- drivers/net/ethernet/tcc_gmac/tcc_gmac_ptp.c
+++ drivers/net/ethernet/tcc_gmac/tcc_gmac_ptp.c
@@ -1,29 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * linux/driver/net/tcc_gmac/tcc_gmac_ptp.c
- *
- * Author : Telechips <linux@telechips.com>
- * Created : Jan 28, 2013
- * Description : This is the driver for the
- * Telechips MAC 10/100/1000 on-chip Ethernet controllers.
- *               Telechips Ethernet IPs are built around a Synopsys IP Core.
- *
- * Copyright (C) 2013 Telechips
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * The full GNU General Public License is included in this distribution in
- * the file called "COPYING".
+ * Copyright (C) Telechips Inc.
  */
 
 #include "tcc_gmac_ptp.h"
