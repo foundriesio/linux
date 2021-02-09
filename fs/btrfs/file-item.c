eb8520c0424972cf4f16b96394b8fca2f7a2e0f9
Nikolay Borisov <nborisov@suse.com>
2021-02-09 15:34:34 +0200

    btrfs: correctly calculate item size used when item key collision  happens (bsc#1181996).
    suse-commit: 0680de59c6576c4b7e478193f6c477fd0f5887c4

--- fs/btrfs/file-item.c
+++ fs/btrfs/file-item.c
@@ -819,8 +819,10 @@ again:
 	 * enough yet to put our csum in.  Grow it
 	 */
 	btrfs_release_path(path);
+	path->search_for_extension = 1;
 	ret = btrfs_search_slot(trans, root, &file_key, path,
 				csum_size, 1);
+	path->search_for_extension = 0;
 	if (ret < 0)
 		goto fail_unlock;
 
