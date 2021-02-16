edbccee82ccb56f9eec6ddfe03565525e481da0d
JunCho <junhwan.cho@telechips.com>
2021-02-16 17:22:38 +0900

    [upg] Fix warnings and errors of vpu module caused by running        linux/checkpatch.pl following kernel coding style - printk is replaced to pr_err for always print messages. - Follow Kernel coding style in vpu module.
    Change-Id: I7b26b57032716a394dc85a2224a0ca305bd597cb
    (cherry picked from commit 77b900b6f3f9349a48e9b83c6f5a5b3557107909)

--- drivers/char/vpu/vpu_dbg.c
+++ drivers/char/vpu/vpu_dbg.c
@@ -52,13 +52,13 @@ void V_DEBUG(enum vdbg_mask_type dbg_mask, const char *fn,
 	vaf.va = &args;
 	if (vpu_prt_mode == VPU_ALWAYS_PRT_DBG) {
 		if (dbg_mask & VPU_DBG_ERROR)
-			printk(KERN_ERR "[%s:%d] %pV\n", fn, ln, &vaf);
+			pr_err("[%s:%d] %pV\n", fn, ln, &vaf);
 		else
 			if (vpu_have_debug_mask(dbg_mask))
-				printk(KERN_ERR "[%s:%d] %pV\n", fn, ln, &vaf);
+				pr_err("[%s:%d] %pV\n", fn, ln, &vaf);
 	} else {
 		if (dbg_mask & VPU_DBG_ERROR)
-			pr_info("[%s:%d] %pV\n", fn, ln, &vaf);
+			pr_err("[%s:%d] %pV\n", fn, ln, &vaf);
 		else
 			if (vpu_have_debug_mask(dbg_mask))
 				pr_debug("[%s:%d] %pV\n", fn, ln, &vaf);
