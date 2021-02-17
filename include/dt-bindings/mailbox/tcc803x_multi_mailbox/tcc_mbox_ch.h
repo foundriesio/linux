883d2c956fea7b3c30ba6f393babaa5df2b96b7c
김호연지기 (Jigi Kim) <jigi.kim@telechips.com>
2021-02-17 14:49:29 +0900

    [fea] tcc: reset: Introduce mbox reboot driver
    This commit adds support for mbox reboot driver for TCC803x subcore
    (Cortex-A7 Single Core) kernel.  As the name refers, this driver uses
    A7-MCU mailbox device to send reboot request to MCU.
    
    Note that this commit replaces legacy unused mbox reboot driver which
    was ported from previous version of Telechips kernel (v4.4).
    
    Linked Commit: N/A
    ITS: TMBSP-2952
    
    Change-Id: I982e2da22ba6ffae4de9e1d24267f8c78f1836d2
    (cherry picked from commit 3a113c7856e374b74905ba90f4c96976e8297f74)

--- include/dt-bindings/mailbox/tcc803x_multi_mailbox/tcc_mbox_ch.h
+++ include/dt-bindings/mailbox/tcc803x_multi_mailbox/tcc_mbox_ch.h
@@ -8,26 +8,26 @@
 
 /*  MBOX0 : A53 <-> R5 */
 #define TCC_MBOX0_CH_RESERVED		(0)
-#define TCC_MBOX0_CH_IPC			(1)
-#define TCC_MBOX0_CH_SUSPEND        (2)
-#define TCC_MBOX0_CH_RESUME			(3)
+#define TCC_MBOX0_CH_IPC		(1)
+#define TCC_MBOX0_CH_SUSPEND		(2)
+#define TCC_MBOX0_CH_REBOOT		(3)
 #define TCC_MBOX0_CH_SNOR_UPDATE	(4)
-#define TCC_MBOX0_CH_HLC			(5)
-#define TCC_MBOX0_CH_HSM			(6)
-#define TCC_MBOX0_CH_SND			(7)
-#define TCC_MBOX0_CH_MAX			(8)
+#define TCC_MBOX0_CH_HLC		(5)
+#define TCC_MBOX0_CH_HSM		(6)
+#define TCC_MBOX0_CH_SND		(7)
+#define TCC_MBOX0_CH_MAX		(8)
 
 /*  MBOX1 : A53 <-> A7S */
 #define TCC_MBOX1_CH_RESERVED		(0)
-#define TCC_MBOX1_CH_IPC			(1)
-#define TCC_MBOX1_CH_VIOC			(2)
-#define TCC_MBOX1_CH_SND			(3)
-#define TCC_MBOX1_CH_SWITCH			(4)
+#define TCC_MBOX1_CH_IPC		(1)
+#define TCC_MBOX1_CH_VIOC		(2)
+#define TCC_MBOX1_CH_SND		(3)
+#define TCC_MBOX1_CH_SWITCH		(4)
 #define TCC_MBOX1_CH_TCHSHARE		(5)
 #define TCC_MBOX1_CH_SCRSHARE		(6)
-#define TCC_MBOX1_CH_HSM			(7)
-#define TCC_MBOX1_CH_MAX			(8)
+#define TCC_MBOX1_CH_HSM		(7)
+#define TCC_MBOX1_CH_MAX		(8)
 
-#define TCC_MBOX_CH_LIMIT			(8)
+#define TCC_MBOX_CH_LIMIT		(8)
 
 #endif
