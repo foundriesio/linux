f5016e973c711c479ee398d844c37ef4c53991e8
JunCho <junhwan.cho@telechips.com>
2021-02-17 11:54:49 +0900

    [upg] Fix warnings and errors of vpu module caused by running linux/checkpatch.pl following kernel coding style - vpu header files for 897x chipset - TCS: https://tcs.telechips.com:8443/browse/BDCA-130
    Change-Id: Ic1901d022e6a274caba13c3b3e40ad79755b23e3
    (cherry picked from commit f250ac554cca3514645afbc6244cbec24833360e)

--- arch/arm/mach-tcc897x/include/mach/tcc_vpu_wbuffer.h
+++ arch/arm/mach-tcc897x/include/mach/tcc_vpu_wbuffer.h
@@ -1,31 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *   FileName    : tcc_vpu_wbuffer.h
- *   Author:  <linux@telechips.com>
- *   Created: June 10, 2008
- *   Description: TCC VPU h/w block
- *
- *   Copyright (C) 2008-2009 Telechips
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see the file COPYING, or write
- * to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * Copyright (C) Telechips Inc.
+ * FileName   : tcc_vpu_wbuffer.h
+ * Description: TCC VPU h/w block
  */
 
 #define VPU_INST_MAX 5
 
-#if defined(CONFIG_VENC_CNT_5)
+#if defined(CONFIG_VENC_CNT_8)
+#define VPU_ENC_MAX_CNT 8
+#elif defined(CONFIG_VENC_CNT_7)
+#define VPU_ENC_MAX_CNT 7
+#elif defined(CONFIG_VENC_CNT_6)
+#define VPU_ENC_MAX_CNT 6
+#elif defined(CONFIG_VENC_CNT_5)
 #define VPU_ENC_MAX_CNT 5
 #elif defined(CONFIG_VENC_CNT_4)
 #define VPU_ENC_MAX_CNT 4
@@ -69,7 +57,7 @@
 #ifndef _VPU_WBUFFER_H_
 #define _VPU_WBUFFER_H_
 
-#define ARRAY_MBYTE(x)	((((x) + (SZ_1M-1))>> 20) << 20)
+#define ARRAY_MBYTE(x)	((((x) + (SZ_1M-1)) >> 20) << 20)
 
 #define VPU_WORK_BUF_SIZE	(PAGE_ALIGN(WORK_CODE_PARA_BUF_SIZE))
 
@@ -78,29 +66,32 @@
 #elif defined(CONFIG_SUPPORT_TCC_WAVE410_HEVC) // HEVC
 #define WAVExxx_WORK_BUF_SIZE	(PAGE_ALIGN(WAVE4_WORK_CODE_BUF_SIZE))
 #else
-#define WAVExxx_WORK_BUF_SIZE	0
+#define WAVExxx_WORK_BUF_SIZE	(0)
 #endif
 
 #ifdef CONFIG_SUPPORT_TCC_G2V2_VP9 // VP9
-#define G2V2_VP9_WORK_BUF_SIZE	0//(PAGE_ALIGN(VP9_WORK_CODE_BUF_SIZE))
+#define G2V2_VP9_WORK_BUF_SIZE	(0) //(PAGE_ALIGN(VP9_WORK_CODE_BUF_SIZE))
 #else
-#define G2V2_VP9_WORK_BUF_SIZE	0
+#define G2V2_VP9_WORK_BUF_SIZE	(0)
 #endif
 
 #ifdef CONFIG_SUPPORT_TCC_JPU
-#define JPU_WORK_BUF_SIZE	0//(PAGE_ALIGN(JPU_WORK_CODE_BUF_SIZE))
+#define JPU_WORK_BUF_SIZE	(0) //(PAGE_ALIGN(JPU_WORK_CODE_BUF_SIZE))
 #else
-#define JPU_WORK_BUF_SIZE	0
+#define JPU_WORK_BUF_SIZE	(0)
 #endif
 
 #ifdef CONFIG_SUPPORT_TCC_WAVE420L_VPU_HEVC_ENC
-#define VPU_HEVC_ENC_WORK_BUF_SIZE	(PAGE_ALIGN(VPU_HEVC_ENC_WORK_CODE_BUF_SIZE))
+#define VPU_HEVC_ENC_WORK_BUF_SIZE \
+	(PAGE_ALIGN(VPU_HEVC_ENC_WORK_CODE_BUF_SIZE))
 #else
-#define VPU_HEVC_ENC_WORK_BUF_SIZE	0
+#define VPU_HEVC_ENC_WORK_BUF_SIZE 0
 #endif
 
-#if defined(CONFIG_VENC_CNT_1) || defined(CONFIG_VENC_CNT_2) || defined(CONFIG_VENC_CNT_3) \
-	|| defined(CONFIG_VENC_CNT_4) || defined(CONFIG_VENC_CNT_5)
+#if defined(CONFIG_VENC_CNT_1) || defined(CONFIG_VENC_CNT_2) || \
+	defined(CONFIG_VENC_CNT_3) || defined(CONFIG_VENC_CNT_4) || \
+	defined(CONFIG_VENC_CNT_5) || defined(CONFIG_VENC_CNT_6) || \
+	defined(CONFIG_VENC_CNT_7) || defined(CONFIG_VENC_CNT_8)
 #if defined(CONFIG_SUPPORT_TCC_WAVE420L_VPU_HEVC_ENC)
 #define ENC_HEADER_BUF_SIZE (PAGE_ALIGN(VPU_HEVC_ENC_HEADER_BUF_SIZE))
 #else
@@ -119,8 +110,25 @@
 #endif
 
 #if defined(CONFIG_TEST_VPU_DRAM_INTLV)
-#define VPU_SW_ACCESS_REGION_SIZE ARRAY_MBYTE(VPU_WORK_BUF_SIZE + WAVExxx_WORK_BUF_SIZE + G2V2_VP9_WORK_BUF_SIZE + JPU_WORK_BUF_SIZE + VPU_HEVC_ENC_WORK_BUF_SIZE + (ENC_HEADER_BUF_SIZE*VPU_ENC_MAX_CNT) + (USER_DATA_BUF_SIZE*VPU_INST_MAX) +(PAGE_ALIGN(PS_SAVE_SIZE) + PAGE_ALIGN(SLICE_SAVE_SIZE) + PAGE_ALIGN(LARGE_STREAM_BUF_SIZE)))
+#define VPU_SW_ACCESS_REGION_SIZE ARRAY_MBYTE( \
+				VPU_WORK_BUF_SIZE + \
+				WAVExxx_WORK_BUF_SIZE + \
+				G2V2_VP9_WORK_BUF_SIZE + \
+				JPU_WORK_BUF_SIZE + \
+				VPU_HEVC_ENC_WORK_BUF_SIZE + \
+				(ENC_HEADER_BUF_SIZE * VPU_ENC_MAX_CNT) + \
+				(USER_DATA_BUF_SIZE * VPU_INST_MAX) + \
+				(PAGE_ALIGN(PS_SAVE_SIZE) + \
+					PAGE_ALIGN(SLICE_SAVE_SIZE) + \
+					PAGE_ALIGN(LARGE_STREAM_BUF_SIZE)))
 #else
-#define VPU_SW_ACCESS_REGION_SIZE ARRAY_MBYTE(VPU_WORK_BUF_SIZE + WAVExxx_WORK_BUF_SIZE + G2V2_VP9_WORK_BUF_SIZE + JPU_WORK_BUF_SIZE + VPU_HEVC_ENC_WORK_BUF_SIZE + (ENC_HEADER_BUF_SIZE*VPU_ENC_MAX_CNT) + (USER_DATA_BUF_SIZE*VPU_INST_MAX))
+#define VPU_SW_ACCESS_REGION_SIZE ARRAY_MBYTE( \
+				VPU_WORK_BUF_SIZE + \
+				WAVExxx_WORK_BUF_SIZE + \
+				G2V2_VP9_WORK_BUF_SIZE + \
+				JPU_WORK_BUF_SIZE + \
+				VPU_HEVC_ENC_WORK_BUF_SIZE + \
+				(ENC_HEADER_BUF_SIZE * VPU_ENC_MAX_CNT) + \
+				(USER_DATA_BUF_SIZE * VPU_INST_MAX))
 #endif
 #endif
