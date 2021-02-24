7e2c3be689415f86a8e36e37024fd82ac8b2c4ec
Jan Kara <jack@suse.cz>
2021-02-24 10:41:47 +0100

    quota: Fix error codes in v2_read_file_info() (bsc#1182652).
    suse-commit: c407a14f3c6126980524a36f4bb17b2e552ad61a

--- fs/quota/quota_v2.c
+++ fs/quota/quota_v2.c
@@ -96,17 +96,17 @@ static int v2_read_file_info(struct super_block *sb, int type)
 	unsigned int version;
 
 	if (!v2_read_header(sb, type, &dqhead))
-		return -1;
+		return -EIO;
 	version = le32_to_cpu(dqhead.dqh_version);
 	if ((info->dqi_fmt_id == QFMT_VFS_V0 && version != 0) ||
 	    (info->dqi_fmt_id == QFMT_VFS_V1 && version != 1))
-		return -1;
+		return -EINVAL;
 
 	size = sb->s_op->quota_read(sb, type, (char *)&dinfo,
 	       sizeof(struct v2_disk_dqinfo), V2_DQINFOOFF);
 	if (size != sizeof(struct v2_disk_dqinfo)) {
 		quota_error(sb, "Can't read info structure");
-		return -1;
+		return -EIO;
 	}
 	info->dqi_priv = kmalloc(sizeof(struct qtree_mem_dqinfo), GFP_NOFS);
 	if (!info->dqi_priv) {
