147598a826cb9e56f30d01cfbdd80d59066326b1
Alexandre Torgue <alexandre.torgue@foss.st.com>
2021-03-02 14:41:06 +0100

    cpufreq: dt-platdev: Blacklist "st,stm32mp151" and "st,stm32mp153" SoCs
    Add  "st,stm32mp151" and "st,stm32mp153" SoC in the blacklist since
    a dedicated cpufreq driver exist for those platforms.
    
    Signed-off-by: Alexandre Torgue <alexandre.torgue@foss.st.com>
    Change-Id: I7fd55f023620de8b1da0a31fcaa863d30966a188
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/196347
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Pascal PAILLET <p.paillet@st.com>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -131,6 +131,8 @@ static const struct of_device_id blacklist[] __initconst = {
 
 	{ .compatible = "st,stih407", },
 	{ .compatible = "st,stih410", },
+	{ .compatible = "st,stm32mp151", },
+	{ .compatible = "st,stm32mp153", },
 	{ .compatible = "st,stm32mp157", },
 
 	{ .compatible = "sigma,tango4", },
