ae40c3fe6c3f3168fbd876aa0944906673443640
Lee Duncan <lduncan@suse.com>
2021-03-15 08:21:32 -0700

    blk-mq: Allow blocking queue tag iter callbacks (CVE-2020-0433 bsc#1176720 bsc#1167316).
    suse-commit: 8d03667896c6844230a62946bcce053a08d831e1

--- block/blk-mq-tag.c
+++ block/blk-mq-tag.c
@@ -340,11 +340,8 @@ void blk_mq_queue_tag_busy_iter(struct request_queue *q, busy_iter_fn *fn,
 	 * synchronize_rcu to ensure all of the users go out of the critical
 	 * section below and see zeroed q_usage_counter.
 	 */
-	rcu_read_lock();
-	if (percpu_ref_is_zero(&q->q_usage_counter)) {
-		rcu_read_unlock();
+	if (!percpu_ref_tryget(&q->q_usage_counter))
 		return;
-	}
 
 	queue_for_each_hw_ctx(q, hctx, i) {
 		struct blk_mq_tags *tags = hctx->tags;
@@ -360,7 +357,7 @@ void blk_mq_queue_tag_busy_iter(struct request_queue *q, busy_iter_fn *fn,
 			bt_for_each(hctx, &tags->breserved_tags, fn, priv, true);
 		bt_for_each(hctx, &tags->bitmap_tags, fn, priv, false);
 	}
-	rcu_read_unlock();
+	blk_queue_exit(q);
 }
 
 static int bt_alloc(struct sbitmap_queue *bt, unsigned int depth,
