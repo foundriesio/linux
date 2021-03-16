9300c2e0d4ddbfbd8a0b857d766918f61857f4ce
Amelie Delaunay <amelie.delaunay@foss.st.com>
2021-03-16 11:57:33 +0100

    usb: dwc2: drd: add role-switch-default-node support
    When using usb-role-switch, while the usb role is not yet define
    (USB_ROLE_NONE), the user may want to configure the default mode to host
    or device.
    Use role-switch-default-mode for that purpose.
    
    Change-Id: I8faf2124a82896b8f06eda853e6b9eae6ab1767f
    Signed-off-by: Amelie Delaunay <amelie.delaunay@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/200610
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Fabrice GASNIER <fabrice.gasnier@foss.st.com>

--- drivers/usb/dwc2/drd.c
+++ drivers/usb/dwc2/drd.c
@@ -24,6 +24,10 @@ static void dwc2_ovr_init(struct dwc2_hsotg *hsotg)
 	gotgctl |= GOTGCTL_BVALOEN | GOTGCTL_AVALOEN | GOTGCTL_VBVALOEN;
 	gotgctl |= GOTGCTL_DBNCE_FLTR_BYPASS;
 	gotgctl &= ~(GOTGCTL_BVALOVAL | GOTGCTL_AVALOVAL | GOTGCTL_VBVALOVAL);
+	if (hsotg->role_sw_default_mode == USB_DR_MODE_HOST)
+		gotgctl |= GOTGCTL_AVALOVAL | GOTGCTL_VBVALOVAL;
+	else if (hsotg->role_sw_default_mode == USB_DR_MODE_PERIPHERAL)
+		gotgctl |= GOTGCTL_BVALOVAL | GOTGCTL_VBVALOVAL;
 	dwc2_writel(hsotg, gotgctl, GOTGCTL);
 
 	spin_unlock_irqrestore(&hsotg->lock, flags);
@@ -141,6 +145,16 @@ int dwc2_drd_init(struct dwc2_hsotg *hsotg)
 	if (device_property_read_bool(hsotg->dev, "usb-role-switch")) {
 		struct usb_role_switch_desc role_sw_desc = {0};
 		struct usb_role_switch *role_sw;
+		const char *str;
+
+		hsotg->role_sw_default_mode = USB_DR_MODE_UNKNOWN;
+		ret = device_property_read_string(hsotg->dev, "role-switch-default-mode", &str);
+		if (!ret) {
+			if (!strncmp(str, "host", strlen("host")))
+				hsotg->role_sw_default_mode = USB_DR_MODE_HOST;
+			else if (!strncmp(str, "peripheral", strlen("peripheral")))
+				hsotg->role_sw_default_mode = USB_DR_MODE_PERIPHERAL;
+		}
 
 		role_sw_desc.fwnode = dev_fwnode(hsotg->dev);
 		role_sw_desc.set = dwc2_drd_role_sw_set;
