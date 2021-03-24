2c6e8ffd80cc3bb8dbc390806bdc8dc36b95ca7b
Vitaly Kuznetsov <vkuznets@redhat.com>
2021-03-24 16:22:19 +0100

    ACPI: processor: Fix CPU0 wakeup in acpi_idle_play_dead()
    BugLink: https://bugs.launchpad.net/bugs/1927150
    
    commit 8cdddd182bd7befae6af49c5fd612893f55d6ccb upstream.
    
    Commit 496121c02127 ("ACPI: processor: idle: Allow probing on platforms
    with one ACPI C-state") broke CPU0 hotplug on certain systems, e.g.
    I'm observing the following on AWS Nitro (e.g r5b.xlarge but other
    instance types are affected as well):
    
     # echo 0 > /sys/devices/system/cpu/cpu0/online
     # echo 1 > /sys/devices/system/cpu/cpu0/online
     <10 seconds delay>
     -bash: echo: write error: Input/output error
    
    In fact, the above mentioned commit only revealed the problem and did
    not introduce it. On x86, to wakeup CPU an NMI is being used and
    hlt_play_dead()/mwait_play_dead() loops are prepared to handle it:
    
    	/*
    	 * If NMI wants to wake up CPU0, start CPU0.
    	 */
    	if (wakeup_cpu0())
    		start_cpu0();
    
    cpuidle_play_dead() -> acpi_idle_play_dead() (which is now being called on
    systems where it wasn't called before the above mentioned commit) serves
    the same purpose but it doesn't have a path for CPU0. What happens now on
    wakeup is:
     - NMI is sent to CPU0
     - wakeup_cpu0_nmi() works as expected
     - we get back to while (1) loop in acpi_idle_play_dead()
     - safe_halt() puts CPU0 to sleep again.
    
    The straightforward/minimal fix is add the special handling for CPU0 on x86
    and that's what the patch is doing.
    
    Fixes: 496121c02127 ("ACPI: processor: idle: Allow probing on platforms with one ACPI C-state")
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Cc: 5.10+ <stable@vger.kernel.org> # 5.10+
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/acpi/processor_idle.c
+++ drivers/acpi/processor_idle.c
@@ -29,6 +29,7 @@
  */
 #ifdef CONFIG_X86
 #include <asm/apic.h>
+#include <asm/cpu.h>
 #endif
 
 #define ACPI_PROCESSOR_CLASS            "processor"
@@ -550,6 +551,12 @@ static int acpi_idle_play_dead(struct cpuidle_device *dev, int index)
 			wait_for_freeze();
 		} else
 			return -ENODEV;
+
+#if defined(CONFIG_X86) && defined(CONFIG_HOTPLUG_CPU)
+		/* If NMI wants to wake up CPU0, start CPU0. */
+		if (wakeup_cpu0())
+			start_cpu0();
+#endif
 	}
 
 	/* Never reached */
