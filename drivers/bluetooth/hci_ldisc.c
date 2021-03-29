7b0d44acbdbef3bfbce2c20fb0455265cc62b379
Oliver Neukum <oneukum@suse.com>
2021-03-29 13:59:17 +0200

    Bluetooth: hci_uart: Fix a race for write_work scheduling (git-fixes).
    suse-commit: 62e5d208330136281aa3df4bd9f2f10193a0ae4b

--- drivers/bluetooth/hci_ldisc.c
+++ drivers/bluetooth/hci_ldisc.c
@@ -135,10 +135,9 @@ int hci_uart_tx_wakeup(struct hci_uart *hu)
 	if (!test_bit(HCI_UART_PROTO_READY, &hu->flags))
 		goto no_schedule;
 
-	if (test_and_set_bit(HCI_UART_SENDING, &hu->tx_state)) {
-		set_bit(HCI_UART_TX_WAKEUP, &hu->tx_state);
+	set_bit(HCI_UART_TX_WAKEUP, &hu->tx_state);
+	if (test_and_set_bit(HCI_UART_SENDING, &hu->tx_state))
 		goto no_schedule;
-	}
 
 	BT_DBG("");
 
