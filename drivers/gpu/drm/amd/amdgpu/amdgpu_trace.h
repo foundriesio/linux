c4d769ef4b9ace41371e420f92a4d89bdf9b9e85
Thomas Zimmermann <tzimmermann@suse.de>
2021-03-30 08:42:36 +0200

    drm/amdgpu: Fix macro name _AMDGPU_TRACE_H_ in preprocessor if (bsc#1129770)
    Backporting notes:
    
    	* context changes
    
    suse-commit: 0c6609b0e1e3c2bb3ecc2f4343579618abb71396

--- drivers/gpu/drm/amd/amdgpu/amdgpu_trace.h
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_trace.h
@@ -1,4 +1,4 @@
-#if !defined(_AMDGPU_TRACE_H) || defined(TRACE_HEADER_MULTI_READ)
+#if !defined(_AMDGPU_TRACE_H_) || defined(TRACE_HEADER_MULTI_READ)
 #define _AMDGPU_TRACE_H_
 
 #include <linux/stringify.h>
