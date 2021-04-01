3abcee0250be8ab1682922565e2d894edc644242
Jens Axboe <axboe@kernel.dk>
2021-04-01 08:38:34 -0600

    io_uring: don't mark S_ISBLK async work as unbounded
    BugLink: https://bugs.launchpad.net/bugs/1928386
    
    [ Upstream commit 4b982bd0f383db9132e892c0c5144117359a6289 ]
    
    S_ISBLK is marked as unbounded work for async preparation, because it
    doesn't match S_ISREG. That is incorrect, as any read/write to a block
    device is also a bounded operation. Fix it up and ensure that S_ISBLK
    isn't marked unbounded.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -1092,7 +1092,7 @@ static inline void io_prep_async_work(struct io_kiocb *req,
 	if (req->flags & REQ_F_ISREG) {
 		if (def->hash_reg_file)
 			io_wq_hash_work(&req->work, file_inode(req->file));
-	} else {
+	} else if (!req->file || !S_ISBLK(file_inode(req->file)->i_mode)) {
 		if (def->unbound_nonreg_file)
 			req->work.flags |= IO_WQ_WORK_UNBOUND;
 	}
