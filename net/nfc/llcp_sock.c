76f57abdfafb1125bd25833283d15bc6771aca9c
Takashi Iwai <tiwai@suse.de>
2021-04-10 13:12:01 +0200

    nfc: fix memory leak in llcp_sock_connect() (CVE-2020-25672 bsc#1178181).
    suse-commit: adfe7b4d068bc597d6773c57b477537e07bac831

--- net/nfc/llcp_sock.c
+++ net/nfc/llcp_sock.c
@@ -757,6 +757,9 @@ sock_unlink:
 	nfc_llcp_put_ssap(local, llcp_sock->ssap);
 
 	nfc_llcp_sock_unlink(&local->connecting_sockets, sk);
+	kfree(llcp_sock->service_name);
+	llcp_sock->service_name = NULL;
+
 	nfc_llcp_local_put(llcp_sock->local);
 
 put_dev:
