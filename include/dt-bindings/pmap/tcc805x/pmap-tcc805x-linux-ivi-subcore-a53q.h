c27d2560a8efc3c646c050fab1be2b595db27bb8
장은석 (eschang) <sdindra@telechips.com>
2021-04-27 08:45:02 +0900

    [upg] HWDMX : hwdmx for sub-core - hwdemux for sub-core - Modified DTB
    Linked Commit: N/A
    ITS: N/A
    
    Change-Id: I4c11e3b841021b1928e4643940704faf6475ef35

--- include/dt-bindings/pmap/tcc805x/pmap-tcc805x-linux-ivi-subcore-a53q.h
+++ include/dt-bindings/pmap/tcc805x/pmap-tcc805x-linux-ivi-subcore-a53q.h
@@ -7,6 +7,9 @@
 
 #include <dt-bindings/pmap/tcc805x/pmap-tcc805x-linux-ivi-subcore-a53q-display.h>
 
+#define SIZE_TSIF			0x00400000
+#define PMAP_SIZE_TSIF			SIZE_TSIF
+
 #define RESERVED_MEM_BASE		(0x4A800000)
 #define RESERVED_MEM_SIZE		(0x15800000 - \
 					PMAP_SIZE_CAMERAS - \
