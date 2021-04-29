9d62816005e9ecc70b642937eb406a25188a5a4c
Kang Yoongu (Frank Kang) <frank.kang@telechips.com>
2021-04-29 10:59:51 +0900

    [bug] Fix Build error on vioc_disp
    Apply tcc805x modification to other chip
    
    Change-Id: Ic8afe65970c419c5e1d0aa661dcffe231f81229b

--- include/video/tcc/tcc898x/vioc_disp.h
+++ include/video/tcc/tcc898x/vioc_disp.h
@@ -649,7 +649,7 @@ extern void VIOC_DISP_EmergencyFlag_SetEofm(void __iomem *reg,
 extern void VIOC_DISP_EmergencyFlag_SetHdmiVs(void __iomem *reg,
 	unsigned int hdmivs);
 extern void __iomem *VIOC_DISP_GetAddress(unsigned int vioc_id);
-void vioc_disp_set_clkdiv(volatile void __iomem *reg, unsigned int div);
+void vioc_disp_set_clkdiv(void __iomem *reg, unsigned int div);
 extern void VIOC_DISP_DUMP(void __iomem *reg, unsigned int vioc_id);
 
 #endif
