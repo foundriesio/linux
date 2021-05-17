a9eb7bd15329dfdc5c11b3de0695a07253a1a96d
Michal Kubecek <mkubecek@suse.cz>
2021-05-17 23:56:59 +0200

    af_packet: fix the tx skb protocol in raw sockets with ETH_P_ALL (bsc#1176081).
    suse-commit: cf2f9ad469d5e4c81ed06ea7ecdf8093065c1f60

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -1868,7 +1868,8 @@ oom:
 
 static void packet_parse_headers(struct sk_buff *skb, struct socket *sock)
 {
-	if (!skb->protocol && sock->type == SOCK_RAW) {
+	if ((!skb->protocol || skb->protocol == htons(ETH_P_ALL)) &&
+	    sock->type == SOCK_RAW) {
 		skb_reset_mac_header(skb);
 		skb->protocol = dev_parse_header_protocol(skb);
 	}
