1f9766ddc1073de9aa4a2e43943a50610d7a6cb6
김정수 (Jeong Su Kim) <deviance@telechips.com>
2021-06-09 18:00:29 +0900

    [ref] Change initcall priority for gpio-regulator
    - After the gpio expander (PCA953x) is loaded,
    	the gpio number of the expander can be loaded only by using the gpio-regulator.
    - This may be a temporary prescription.
    
    Linked Commit:
    ITS:
    
    Change-Id: Iaa1e21e7b25cfe18f024be045202a2c51a452ca2

--- drivers/regulator/gpio-regulator.c
+++ drivers/regulator/gpio-regulator.c
@@ -414,7 +414,7 @@ static int __init gpio_regulator_init(void)
 {
 	return platform_driver_register(&gpio_regulator_driver);
 }
-subsys_initcall(gpio_regulator_init);
+fs_initcall(gpio_regulator_init);
 
 static void __exit gpio_regulator_exit(void)
 {
