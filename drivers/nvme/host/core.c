9f18f3a641923865789ea26fc5e2c607946e70d4
Enzo Matsumiya <ematsumiya@suse.de>
2021-06-17 12:11:36 -0300

    - nvme: have nvme_wait_freeze_timeout return if it timed out   (bsc#1181161). - Refresh patches.suse/nvme-introduce-nvme_sync_io_queues.patch.
    suse-commit: 045f17adfe05a782ea654eb379140a6db071fae8

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -3880,7 +3880,7 @@ void nvme_unfreeze(struct nvme_ctrl *ctrl)
 }
 EXPORT_SYMBOL_GPL(nvme_unfreeze);
 
-void nvme_wait_freeze_timeout(struct nvme_ctrl *ctrl, long timeout)
+int nvme_wait_freeze_timeout(struct nvme_ctrl *ctrl, long timeout)
 {
 	struct nvme_ns *ns;
 
@@ -3891,6 +3891,7 @@ void nvme_wait_freeze_timeout(struct nvme_ctrl *ctrl, long timeout)
 			break;
 	}
 	up_read(&ctrl->namespaces_rwsem);
+	return timeout;
 }
 EXPORT_SYMBOL_GPL(nvme_wait_freeze_timeout);
 
