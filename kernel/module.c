f3bb4105322da0a893725eb18bca843ed1c52d12
Ian May <ian.may@canonical.com>
2021-06-17 22:39:00 +0200

    UBUNTU: SAUCE: Revert "modules: mark ref_module static"
    BugLink: https://bugs.launchpad.net/bugs/1932065
    
    This reverts commit 49ea3be5063837093b76477b67445094b0ad45ef.
    
    Upstream introduced 'module' patches that removed exported symbols
    that might cause potential disruption and breakage for customers.
    
    Reverting as SAUCE patches to prevent regressions.
    
    Signed-off-by: Ian May <ian.may@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- kernel/module.c
+++ kernel/module.c
@@ -870,7 +870,7 @@ static int add_module_usage(struct module *a, struct module *b)
 }
 
 /* Module a uses b: caller needs module_mutex() */
-static int ref_module(struct module *a, struct module *b)
+int ref_module(struct module *a, struct module *b)
 {
 	int err;
 
@@ -889,6 +889,7 @@ static int ref_module(struct module *a, struct module *b)
 	}
 	return 0;
 }
+EXPORT_SYMBOL_GPL(ref_module);
 
 /* Clear the unload stuff of the module. */
 static void module_unload_free(struct module *mod)
@@ -1169,10 +1170,11 @@ static inline void module_unload_free(struct module *mod)
 {
 }
 
-static int ref_module(struct module *a, struct module *b)
+int ref_module(struct module *a, struct module *b)
 {
 	return strong_try_module_get(b);
 }
+EXPORT_SYMBOL_GPL(ref_module);
 
 static inline int module_unload_init(struct module *mod)
 {
