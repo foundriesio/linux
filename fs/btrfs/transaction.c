7cacdd4c0702016100a9e7de395dd691dc0cf7dc
Nikolay Borisov <nborisov@suse.com>
2021-06-30 17:27:33 +0300

    btrfs: return an error from btrfs_record_root_in_trans (bsc#1187833).
    suse-commit: af906199a5fc21e89ecbf37e95204a98ab12bd17

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -308,6 +308,7 @@ static int record_root_in_trans(struct btrfs_trans_handle *trans,
 			       int force)
 {
 	struct btrfs_fs_info *fs_info = root->fs_info;
+	int ret = 0;
 
 	if ((test_bit(BTRFS_ROOT_REF_COWS, &root->state) &&
 	    root->last_trans < trans->transid) || force) {
@@ -356,11 +357,11 @@ static int record_root_in_trans(struct btrfs_trans_handle *trans,
 		 * lock.  smp_wmb() makes sure that all the writes above are
 		 * done before we pop in the zero below
 		 */
-		btrfs_init_reloc_root(trans, root);
+		ret = btrfs_init_reloc_root(trans, root);
 		smp_mb__before_atomic();
 		clear_bit(BTRFS_ROOT_IN_TRANS_SETUP, &root->state);
 	}
-	return 0;
+	return ret;
 }
 
 
