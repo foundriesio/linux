7e59c841aac68de8e159ac7d49cc55a3bee8d178
Olaf Hering <ohering@suse.de>
2021-07-02 09:39:19 +0200

    Revert "scsi: storvsc: Validate length of incoming packet in storvsc_on_channel_callback()" (bsc#1183682).
    suse-commit: fa61bc6d51fe86fc8d6c3514ad82e4d5eefce9ff

--- drivers/scsi/storvsc_drv.c
+++ drivers/scsi/storvsc_drv.c
@@ -1261,11 +1261,6 @@ static void storvsc_on_channel_callback(void *context)
 
 		request = (struct storvsc_cmd_request *)(unsigned long)cmd_rqst;
 
-		if (hv_pkt_datalen(desc) < sizeof(struct vstor_packet) - vmscsi_size_delta) {
-			dev_err(&device->device, "Invalid packet len\n");
-			continue;
-		}
-
 		if (request == &stor_device->init_request ||
 		    request == &stor_device->reset_request) {
 			memcpy(&request->vstor_packet, packet,
