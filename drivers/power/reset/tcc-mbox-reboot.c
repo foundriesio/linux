b2ade6bc6777d30b46f9e45d93b857cb45a0675b
김호연지기 (Jigi Kim) <jigi.kim@telechips.com>
2021-07-13 13:16:45 +0900

    [upg] tcc: reset: Backport mbox reboot driver
    This commit brings several changes for mbox reboot driver from kernel
    5.4 to update the driver implementation to the latest, including:
    
    - Register restart handler instead of implementing arm_pm_restart()
    - Add driver remove() operation
    - Add module tags for license, author and description
    - Supplement kconfig help message
    
    BACKPORT from Telechips kernel-5.4.git [1].
    
    Linked Commit @ kernel-5.4.git
    [1] 42043bf [fea] tcc: reset: Introduce mbox reboot driver
    
    ITS: N/A
    
    Change-Id: I8c1847344bdd4c6a4e50f74e5b40ba3f982d84ea

--- drivers/power/reset/tcc-mbox-reboot.c
+++ drivers/power/reset/tcc-mbox-reboot.c
@@ -3,53 +3,61 @@
  * Copyright (C) Telechips Inc.
  */
 
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/platform_device.h>
+#define pr_fmt(fmt) "mbox-reboot: " fmt
 
+#include <linux/kernel.h>
 #include <linux/mailbox_controller.h>
 #include <linux/mailbox_client.h>
 #include <linux/mailbox/tcc_multi_mbox.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/reboot.h>
 
-#include <asm/system_misc.h>
-
-static inline void mbox_reboot_err(const char *msg, s32 err)
-{
-	(void)pr_err("[ERROR][reboot] Failed to %s (err: %d)\n", msg, err);
-}
-
-struct mbox_reboot_drvdata {
+struct mbox_reboot_data {
 	struct platform_device *pdev;
 	struct mbox_chan *mbox_chan;
 	struct mutex lock;
 };
 
-static struct mbox_reboot_drvdata *mbox_reboot_drvdata_ptr;
+static struct mbox_reboot_data *mbox_reboot_data_ptr;
 
-struct mbox_reboot_drvdata *get_mbox_reboot_drvdata(void)
+struct mbox_reboot_data *get_mbox_reboot_data(void)
 {
-	return mbox_reboot_drvdata_ptr;
+	return mbox_reboot_data_ptr;
 }
 
-static void mbox_reboot_reset_cpu(enum reboot_mode mode, const char *cmd)
+static void mbox_reboot_reset_cpu(void)
 {
-	struct mbox_reboot_drvdata *drvdata = get_mbox_reboot_drvdata();
+	struct mbox_reboot_data *data = get_mbox_reboot_data();
 	struct tcc_mbox_data msg;
 
-	(void)memset(msg.cmd, 0, sizeof(*msg.cmd) * (size_t)MBOX_CMD_FIFO_SIZE);
+	memset(msg.cmd, 0, sizeof(*msg.cmd) * (size_t)MBOX_CMD_FIFO_SIZE);
 	msg.cmd[1] = 1U << 16U;
 	msg.data_len = 0;
 
-	mutex_lock(&drvdata->lock);
-	mbox_send_message(drvdata->mbox_chan, &msg);
-	mbox_client_txdone(drvdata->mbox_chan, 0);
-	mutex_unlock(&drvdata->lock);
+	mutex_lock(&data->lock);
+	mbox_send_message(data->mbox_chan, &msg);
+	mbox_client_txdone(data->mbox_chan, 0);
+	mutex_unlock(&data->lock);
 
 	while (1) {
 		/* Wait CR5 resetting cpu */
 		wfe();
 	}
 }
+
+static int mbox_reboot_reset_notifier_call(struct notifier_block *nb,
+					   unsigned long action, void *data)
+{
+	mbox_reboot_reset_cpu();
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block mbox_reboot_reset_notifier_block = {
+	.notifier_call = &mbox_reboot_reset_notifier_call,
+	.priority = 128,
+};
 
 static void mbox_reboot_power_off(void)
 {
@@ -59,9 +67,10 @@ static void mbox_reboot_power_off(void)
 	}
 }
 
-static s32 mbox_reboot_mbox_init(struct mbox_chan **ch, struct device *dev)
+static s32 mbox_reboot_mbox_init(struct mbox_chan **chan_p, struct device *dev)
 {
 	struct mbox_client *cl;
+	struct mbox_chan *chan;
 	bool err;
 
 	cl = devm_kzalloc(dev, sizeof(struct mbox_client), GFP_KERNEL);
@@ -71,66 +80,83 @@ static s32 mbox_reboot_mbox_init(struct mbox_chan **ch, struct device *dev)
 	}
 
 	cl->dev = dev;
-	cl->tx_block = (bool)false;
-	cl->knows_txdone = (bool)false;
+	cl->tx_block = false;
 	cl->tx_tout = 500;
+	cl->knows_txdone = false;
 
-	*ch = mbox_request_channel(cl, 0);
-	err = IS_ERR(*ch);
+	chan = mbox_request_channel(cl, 0);
+	err = IS_ERR(chan);
 
 	if (err) {
 		devm_kfree(dev, cl);
-		return (s32)PTR_ERR(*ch);
+		return (s32)PTR_ERR(chan);
 	}
 
+	*chan_p = chan;
+
 	return 0;
+}
+
+static void mbox_reboot_mbox_free(struct mbox_chan *chan, struct device *dev)
+{
+	if ((dev != NULL) && (chan != NULL)) {
+		struct mbox_client *cl = chan->cl;
+
+		mbox_free_channel(chan);
+		devm_kfree(dev, cl);
+	}
 }
 
 static int mbox_reboot_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct mbox_reboot_drvdata *drvdata;
+	struct mbox_reboot_data *data;
 	s32 ret = -ENODEV;
 
-	if (dev == NULL) {
-		mbox_reboot_err("get device", ret);
-		goto pre_init_error;
-	}
-
 	/* Allocate memory for mbox reboot driver data */
-	drvdata = devm_kzalloc(dev, sizeof(struct mbox_reboot_drvdata),
-			       GFP_KERNEL);
-	if (drvdata == NULL) {
+	data = devm_kzalloc(dev, sizeof(struct mbox_reboot_data), GFP_KERNEL);
+	if (data == NULL) {
 		ret = -ENOMEM;
-		mbox_reboot_err("allocate driver data", ret);
+		pr_err("Failed to allocate driver data (err: %d)\n", ret);
 		goto pre_init_error;
 	}
 
 	/* Initialize AP-MC mailbox channel for reboot request */
-	ret = mbox_reboot_mbox_init(&drvdata->mbox_chan, dev);
+	ret = mbox_reboot_mbox_init(&data->mbox_chan, dev);
 	if (ret != 0) {
-		mbox_reboot_err("init AP-MC mailbox channel", ret);
+		pr_err("Failed to init AP-MC mbox channel (err: %d)\n", ret);
 		goto mbox_init_error;
 	}
 
 	/* Initialize mutex to be used on reboot */
-	mutex_init(&drvdata->lock);
+	mutex_init(&data->lock);
 
-	/* Register power-off and restart operation */
+	/* Register restart handler and power-off operation */
+	register_restart_handler(&mbox_reboot_reset_notifier_block);
 	pm_power_off = mbox_reboot_power_off;
-	arm_pm_restart = mbox_reboot_reset_cpu;
 
 	/* Now we can register driver data */
-	platform_set_drvdata(pdev, drvdata);
-	mbox_reboot_drvdata_ptr = drvdata;
-	drvdata->pdev = pdev;
+	platform_set_drvdata(pdev, data);
+	data->pdev = pdev;
+
+	mbox_reboot_data_ptr = data;
 
 	return 0;
 
 mbox_init_error:
-	devm_kfree(dev, drvdata);
+	devm_kfree(dev, data);
 pre_init_error:
 	return ret;
+}
+static int mbox_reboot_remove(struct platform_device *pdev)
+{
+	struct mbox_reboot_data *data = platform_get_drvdata(pdev);
+	struct device *dev = &pdev->dev;
+
+	mbox_reboot_mbox_free(data->mbox_chan, dev);
+	devm_kfree(dev, data);
+
+	return 0;
 }
 
 static const struct of_device_id tcc_mbox_reboot_match[] = {
@@ -140,10 +166,16 @@ static const struct of_device_id tcc_mbox_reboot_match[] = {
 
 static struct platform_driver mbox_reboot_driver = {
 	.probe = mbox_reboot_probe,
+	.remove = mbox_reboot_remove,
 	.driver = {
 		.name = "tcc-mbox-reboot",
+		.owner = THIS_MODULE,
 		.of_match_table = of_match_ptr(tcc_mbox_reboot_match),
 	},
 };
 
-builtin_platform_driver(mbox_reboot_driver);
+module_platform_driver(mbox_reboot_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Jigi Kim <jigi.kim@telechips.com>");
+MODULE_DESCRIPTION("Telechips mbox reboot driver");
