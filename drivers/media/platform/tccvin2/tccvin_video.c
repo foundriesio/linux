2b3b4e145ddd048b16d1a8a014d551eae185bbaa
한동열 (DongYeol Han) <hdy@telechips.com>
2021-08-17 14:53:15 +0900

    [bug] media: tccvin2: tcc-mipi-csi2: Check VIN4~7 and MIPI CSI2 operation on TCC803x and TCC803xpe
    - tcc803x evb's video decoder is isl79988.
    - tcc803x evb camera related device's I2C master is I2C0.
    - TCC803x and TCC803xpe's VIN to process MIPI CSI2's channel 0 output has been changed.
    (VIN3 to VIN1)
    (VIN1, 4~6 are used to process MIPI CSI2 output)
    - clock-names property of MIPI CSI2 device node has been added.
    - To use VIN3~7, wrong conditions in tccvin2 have been fixed.
    - kernel coding style error has been fixed.
    
    Linked Commit: N/A
    ITS: BDCA-498, BDCA-494
    
    Change-Id: Ie0e12c62a4d2e2bf115accf5e846b954b52438d8
    (cherry picked from commit 8d86e353b11ec5294d708bb68f4e58977540eb75)

--- drivers/media/platform/tccvin2/tccvin_video.c
+++ drivers/media/platform/tccvin2/tccvin_video.c
@@ -425,13 +425,8 @@ static int32_t tccvin_parse_device_tree(struct tccvin_streaming *vdev)
 #if defined(CONFIG_OVERLAY_PGL)
 	vioc_path->pgl = -1;
 	vdev->cif.use_pgl = -1;
-#if defined(CONFIG_ARCH_TCC803X)
-	/* VIDEO_IN00~02 have RDMA */
-	if ((vioc_path->vin >= VIOC_VIN00) && (vioc_path->vin <= VIOC_VIN20)) {
-#elif defined(CONFIG_ARCH_TCC805X)
 	/* VIDEO_IN00~03 have RDMA */
 	if ((vioc_path->vin >= VIOC_VIN00) && (vioc_path->vin <= VIOC_VIN30)) {
-#endif
 		vioc_node = of_parse_phandle(main_node, "rdma", 0);
 		if (vioc_node != NULL) {
 			of_property_read_u32_index(main_node,
@@ -555,11 +550,8 @@ static int32_t tccvin_parse_device_tree(struct tccvin_streaming *vdev)
 	}
 
 #if defined(CONFIG_OVERLAY_PGL)
-#if defined(CONFIG_ARCH_TCC803X)
-	if ((vioc_path->vin >= VIOC_VIN00) && (vioc_path->vin <= VIOC_VIN20)) {
-#elif defined(CONFIG_ARCH_TCC805X)
-	if ((vioc_path->vin >= VIOC_VIN00) && (vioc_path->vin <= VIOC_VIN30)) {
-#endif
+	if (vdev->cif.use_pgl &&
+	    (vioc_path->vin >= VIOC_VIN00) && (vioc_path->vin <= VIOC_VIN30)) {
 		/* pmap_pgl */
 		pmap_node = of_parse_phandle(main_node, "memory-region", 0);
 		if (pmap_node != NULL) {
@@ -583,11 +575,7 @@ static int32_t tccvin_parse_device_tree(struct tccvin_streaming *vdev)
 	}
 #endif/* defined(CONFIG_OVERLAY_PGL) */
 
-#if defined(CONFIG_ARCH_TCC803X)
-	if ((vioc_path->vin >= VIOC_VIN00) && (vioc_path->vin <= VIOC_VIN20)) {
-#elif defined(CONFIG_ARCH_TCC805X)
 	if ((vioc_path->vin >= VIOC_VIN00) && (vioc_path->vin <= VIOC_VIN30)) {
-#endif
 		/* pmap_viqe */
 		pmap_node = of_parse_phandle(main_node, "memory-region", 1);
 		if (pmap_node != NULL) {
@@ -1796,12 +1784,8 @@ static int32_t tccvin_request_irq(struct tccvin_streaming *vdev)
 
 		vioc_base_id	= VIOC_VIN00;
 		intr_base_id	= VIOC_INTR_VIN0;
-#if defined(CONFIG_ARCH_TCC803X)
-		if (vioc_path->vin >= VIOC_VIN30) {
-			vioc_base_id	= VIOC_VIN30;
-			intr_base_id	= VIOC_INTR_VIN3;
-		}
-#elif defined(CONFIG_ARCH_TCC805X)
+
+#if defined(CONFIG_ARCH_TCC803X) || defined(CONFIG_ARCH_TCC805X)
 		if (vioc_path->vin >= VIOC_VIN40) {
 			vioc_base_id	= VIOC_VIN40;
 			intr_base_id	= VIOC_INTR_VIN4;
