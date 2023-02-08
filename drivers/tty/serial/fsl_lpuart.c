foundriesio/linux

commit b9ae52e89c61eed5d446a9693d8bf0f55a5323e7

serial: fsl_lpuart: fix merging issue
The commit [1] moves adding uart port after getting all options for
rs485. Merge commit mistakenly leaves both uart_add_one_port() calls
in place. Fix it.

[1]
commit b079d3775237 ("serial: Deassert Transmit Enable on probe in driver-specific way")

Fixes: 3ff5eb3ff57e ("Merge pull request #620 from angolini/5.15-2.2.x-imx-bump")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/tty/serial/fsl_lpuart.c
@@ -2962,10 +2962,6 @@ static int lpuart_probe(struct platform_device *pdev)
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
 
-	ret = uart_add_one_port(&lpuart_reg, &sport->port);
-	if (ret)
-		goto failed_attach_port;
-
 	ret = lpuart_global_reset(sport);
 	if (ret)
 		goto failed_reset;
@@ -2993,10 +2989,10 @@ static int lpuart_probe(struct platform_device *pdev)
 	return 0;
 
 failed_irq_request:
-failed_get_rs485:
-failed_reset:
 	uart_remove_one_port(&lpuart_reg, &sport->port);
 failed_attach_port:
+failed_get_rs485:
+failed_reset:
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_set_suspended(&pdev->dev);
 	pm_runtime_dont_use_autosuspend(&pdev->dev);

commit 645fce6a3bbd53ac8f1e8e5446de74edc2acf162

serial: fsl_lpuart: fix driver stuck
The calls reorder done in commit [1] works well in the upstream
kernel. The NXP version of the kernel needs a port to be attached to
the driver earlier, otherwise, the driver is stuck. Move back calls
order as a workaround.
It may break working rs485. The only board that uses this driver for
the RS-485 port is "vf610-zii-scu4-aib" which is quite legacy.

[1] commit b079d3775237 ("serial: Deassert Transmit Enable on probe in driver-specific way")

Fixes: b9ae52e89c61 ("serial: fsl_lpuart: fix merging issue")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

drivers/tty/serial/fsl_lpuart.c
@@ -2962,6 +2962,10 @@ static int lpuart_probe(struct platform_device *pdev)
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
 
+	ret = uart_add_one_port(&lpuart_reg, &sport->port);
+	if (ret)
+		goto failed_attach_port;
+
 	ret = lpuart_global_reset(sport);
 	if (ret)
 		goto failed_reset;
@@ -2977,9 +2981,7 @@ static int lpuart_probe(struct platform_device *pdev)
 	    sport->port.rs485.delay_rts_after_send)
 		dev_err(&pdev->dev, "driver doesn't support RTS delays\n");
 
-	ret = uart_add_one_port(&lpuart_reg, &sport->port);
-	if (ret)
-		goto failed_attach_port;
+	sport->port.rs485_config(&sport->port, &sport->port.rs485);
 
 	ret = devm_request_irq(&pdev->dev, sport->port.irq, handler, 0,
 				DRIVER_NAME, sport);
@@ -2989,10 +2991,10 @@ static int lpuart_probe(struct platform_device *pdev)
 	return 0;
 
 failed_irq_request:
-	uart_remove_one_port(&lpuart_reg, &sport->port);
-failed_attach_port:
 failed_get_rs485:
 failed_reset:
+	uart_remove_one_port(&lpuart_reg, &sport->port);
+failed_attach_port:
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_set_suspended(&pdev->dev);
 	pm_runtime_dont_use_autosuspend(&pdev->dev);